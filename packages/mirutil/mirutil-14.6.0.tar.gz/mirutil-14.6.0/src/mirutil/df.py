3028b9bf8db8bade2fb9eb7b48340f82d9c4b9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:49 +0000

    Add package mirutil/14.6.0/mirutil-14.6.0.tar.gz

---
+++ packages/mirutil/mirutil-14.6.0.tar.gz/mirutil-14.6.0/src/mirutil/df.py
@@ -0,0 +1,176 @@
+"""
+
+    """
+
+from pathlib import Path
+
+import openpyxl as pyxl
+import pandas as pd
+from itertools import product
+from multiprocess.pool import Pool
+
+
+def save_df_as_a_nice_xl(df ,
+                         fpn ,
+                         index: bool = False ,
+                         header: bool = True ,
+                         max_col_length: int = 40) -> None :
+    df.to_excel(fpn , index = False)
+
+    wb = pyxl.load_workbook(fpn)
+    ws = wb.active
+
+    panes = index * ws['A'] + header * ws[1]
+
+    for cell in panes :
+        cell.style = 'Pandas'
+
+    for column in ws.columns :
+        length = max(len(str(cell.value)) for cell in column) + 3
+        length = length if length <= max_col_length else max_col_length
+        ws.column_dimensions[column[0].column_letter].width = length
+
+    ws.freeze_panes = 'A2'
+
+    wb.save(fpn)
+    wb.close()
+    print(f"saved as {fpn}")
+
+def save_as_prq_wo_index(df , fp) -> None :
+    df.to_parquet(fp , index = False)
+    print(f'dataframe saved as {fp} without index')
+
+def read_data_according_to_type(fp) -> pd.DataFrame :
+    suf = Path(fp).suffix
+    if suf == '.xlsx' :
+        return pd.read_excel(fp)
+    elif suf == '.prq' :
+        return pd.read_parquet(fp)
+    elif suf == '.csv' :
+        return pd.read_csv(fp)
+
+def has_extra_data(df , df1) -> bool :
+    df = df.convert_dtypes()
+    df1 = df1.convert_dtypes()
+
+    df = df.astype('string')
+    df1 = df1.astype('string')
+
+    _df = pd.concat([df , df1])
+
+    _df = _df.drop_duplicates()
+
+    return not df.equals(_df)
+
+def drop_dup_and_sub_dfs(dfs_list: list) -> list :
+    """
+
+    assumption: len(dfs_list) >= 2
+    """
+
+    dfs1 = [(i , df) for i , df in enumerate(dfs_list)]
+
+    pr = product(dfs_list , dfs1)
+    pr = list(pr)
+
+    df = pd.DataFrame(pr , columns = [0 , 1])
+
+    df[2] = df[1].apply(lambda x : x[0])
+    df[1] = df[1].apply(lambda x : x[1])
+
+    ms = df.apply(lambda x : x[0].equals(x[1]) , axis = 1)
+    df = df[~ ms]
+
+    df[3] = df.apply(lambda x : has_extra_data(x[0] , x[1]) , axis = 1)
+
+    df[4] = df.groupby(2)[3].transform('all')
+
+    ms = df[4]
+    df = df[ms]
+
+    df = df.drop_duplicates(subset = 2)
+
+    return df[1].to_list()
+
+def find_all_df_locs_eq_val(df: pd.DataFrame , val) -> pd.MultiIndex :
+    msk = df.eq(val)
+    _df = df[msk]
+    s = _df.stack()
+    return s.index
+
+def ret_north_west_of_indices(mi: pd.MultiIndex) :
+    df = mi.to_frame()
+    if df.empty :
+        return
+    df = df.sort_values(by = [0 , 1] , ascending = False)
+    r = df.iloc[[0]]
+    return r.index[0]
+
+def ret_mask_indices(msk) :
+    return msk[msk].index
+
+def ret_indices(df , msk = None) :
+    if msk is not None :
+        return ret_mask_indices(msk)
+    else :
+        return df.index
+
+def make_inputs_4_apply_parallel(df , inds , inp_cols) :
+    if len(inp_cols) == 1 :
+        return df.loc[inds , inp_cols[0]]
+    else :
+        _df = df.loc[inds , inp_cols]
+        return zip(*[_df[x] for x in inp_cols])
+
+def run_parallel(func , inp , n_jobs = 30) :
+    pool = Pool(n_jobs)
+
+    if isinstance(inp , zip) :
+        fu = pool.starmap
+    else :
+        fu = pool.map
+
+    try :
+        return fu(func , inp)
+    except KeyboardInterrupt :
+        return
+
+def handle_parallel_output(o ,
+                           df ,
+                           inds ,
+                           out_map = None ,
+                           out_cols = None ,
+                           out_type_is_dict = True) :
+    if out_map and out_type_is_dict :
+        for k , v in out_map.items() :
+            df.loc[inds , k] = [x[v] for x in o]
+    elif out_map and not out_type_is_dict :
+        for k , v in out_map.items() :
+            df.loc[inds , k] = [x.__getattribute__(v) for x in o]
+    else :
+        for i , col in enumerate(out_cols) :
+            df.loc[inds , col] = [x[i] for x in o]
+
+    return df
+
+def df_apply_parallel(df ,
+                      func ,
+                      inp_cols: list ,
+                      out_map: dict = None ,
+                      out_cols: list = None ,
+                      out_type_is_dict = True ,
+                      msk = None ,
+                      test = False ,
+                      n_jobs = 30) :
+    inds = ret_indices(df , msk)
+    if test :
+        inds = inds[: min(100 , len(inds))]
+    inp = make_inputs_4_apply_parallel(df , inds , inp_cols)
+    o = run_parallel(func , inp , n_jobs)
+    df = handle_parallel_output(o ,
+                                df ,
+                                inds ,
+                                out_map ,
+                                out_cols ,
+                                out_type_is_dict)
+    return df
---
+++ packages/mirutil/mirutil-14.6.0.tar.gz/mirutil-14.6.0/src/mirutil/requests_htmll.py
@@ -0,0 +1,104 @@
+"""
+
+    """
+
+import asyncio
+from dataclasses import dataclass
+from functools import partial
+
+import nest_asyncio
+from lxml.etree import XMLSyntaxError
+from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
+from pyppeteer.errors import TimeoutError as tout
+from pyppeteer.errors import PageError
+from requests.exceptions import ConnectionError
+
+from .const import Const
+from .files import write_txt_to_file_async
+
+
+nest_asyncio.apply()
+
+cte = Const()
+
+def download_chromium_if_not_installed() :
+    """download chromium if not installed"""
+    url = 'https://google.com'
+
+    s = HTMLSession()
+    r = s.get(url , headers = cte.headers , timeout = 10)
+    s.close()
+
+    r.html.render(timeout = 30)
+
+@dataclass
+class RGetAndRender :
+    status: int
+    headers: dict
+    html: (str , None) = None
+    err: (str , None) = None
+
+async def get_and_render_by_requests_html_async(url ,
+                                                headers = cte.headers ,
+                                                params = None ,
+                                                verify = True ,
+                                                get_timeout = None ,
+                                                render_timeout = None) :
+    a = AsyncHTMLSession()
+    r = await a.get(url ,
+                    headers = headers ,
+                    params = params ,
+                    verify = verify ,
+                    timeout = get_timeout)
+    await a.close()
+
+    try :
+        await r.html.arender(timeout = render_timeout)
+        return RGetAndRender(status = r.status_code ,
+                             headers = r.headers ,
+                             html = r.html.html ,
+                             err = None)
+    except (XMLSyntaxError , tout , PageError , ConnectionError) as e :
+        print(e)
+        return RGetAndRender(status = r.status_code ,
+                             headers = r.headers ,
+                             html = None ,
+                             err = e)
+
+async def get_a_rendered_html_and_save_async(url ,
+                                             fp ,
+                                             headers = cte.headers ,
+                                             params = None ,
+                                             verify = True ,
+                                             get_timeout = None ,
+                                             render_timeout = None) :
+    fu = get_and_render_by_requests_html_async
+    o = await fu(url ,
+                 headers = headers ,
+                 params = params ,
+                 verify = verify ,
+                 get_timeout = get_timeout ,
+                 render_timeout = render_timeout)
+
+    outer_html = o.html
+    if outer_html :
+        await write_txt_to_file_async(outer_html , fp)
+
+    return o
+
+async def get_rendered_htmls_and_save_async(urls ,
+                                            fps ,
+                                            headers = cte.headers ,
+                                            params = None ,
+                                            verify = True ,
+                                            get_timeout = None ,
+                                            render_timeout = None) :
+    fu = partial(get_a_rendered_html_and_save_async ,
+                 headers = headers ,
+                 params = params ,
+                 verify = verify ,
+                 get_timeout = get_timeout ,
+                 render_timeout = render_timeout)
+    co_tasks = [fu(x , y) for x , y in zip(urls , fps)]
+    return await asyncio.gather(*co_tasks)
