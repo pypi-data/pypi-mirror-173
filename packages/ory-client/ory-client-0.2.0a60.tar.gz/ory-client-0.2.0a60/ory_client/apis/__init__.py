20cfe29238ca8687aab20995e88eba66a6b7b125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:20 +0000

    Add package ory-client/0.2.0a60/ory-client-0.2.0a60.tar.gz

---
+++ packages/ory-client/ory-client-0.2.0a60.tar.gz/ory-client-0.2.0a60/ory_client/apis/__init__.py
@@ -0,0 +1,23 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from ory_client.api.admin_api import AdminApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from ory_client.api.admin_api import AdminApi
+from ory_client.api.metadata_api import MetadataApi
+from ory_client.api.namespaces_api import NamespacesApi
+from ory_client.api.read_api import ReadApi
+from ory_client.api.syntax_api import SyntaxApi
+from ory_client.api.v0alpha2_api import V0alpha2Api
+from ory_client.api.write_api import WriteApi
