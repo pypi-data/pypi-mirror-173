19105ce9de1c1106f893177a8e9a8b5bad31010a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:13 +0000

    Add package TreeHole/1.1.0.post1/TreeHole-1.1.0.post1.tar.gz

---
+++ packages/TreeHole/TreeHole-1.1.0.post1.tar.gz/TreeHole-1.1.0.post1/src/treehole/__init__.py
@@ -0,0 +1,28 @@
+"""
+A simple interface to PKU Hole
+
+快速上手：
+
+```py
+from treehole import TreeHoleClient
+
+client = TreeHoleClient("your token")
+hole, timestamp = client.get_hole("hole ID")
+print(hole)
+```
+
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "TreeHole"
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+from .client import *
+from .models import *
