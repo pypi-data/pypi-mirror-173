824d8f287b66d552876530d15d4e88806694ad9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:46 +0000

    Add package CndIO/2.0.2/CndIO-2.0.2.tar.gz

---
+++ packages/CndIO/CndIO-2.0.2.tar.gz/CndIO-2.0.2/cnd_io/cnd_provider_localfile.py
@@ -0,0 +1,27 @@
+from os.path import exists
+from cnd_provider import CndProvider
+
+
+class CndProviderLocalfile(CndProvider):
+    def _full_path(selft, source_name, file_name):
+        return f"{source_name}/{file_name}"
+
+    def pull_file(self, source_name, file_name, **kwargs):
+        self._print_me("trace_c", f"Opening file {self._full_path(source_name, file_name)} now")
+        f = open(self._full_path(source_name, file_name), "r")
+        return f.read()
+
+    def push_file(self, source_name, file_name, content, **kwargs):
+        self._print_me("trace_c", f"Creation file {self._full_path(source_name, file_name)} now")
+        with open(self._full_path(source_name, file_name), 'w') as f:
+            f.write(content)
+        return True
+
+    def push_files(self, source_name, files, **kwargs):
+        for file_name in files:
+            self.push_file(source_name, file_name, files[file_name])
+        return True
+
+    def file_exist(self, source_name, file_name, **kwargs):
+        self._print_me("trace_c", f"Checking file {self._full_path(source_name, file_name)} now")
+        return exists(self._full_path(source_name, file_name))
