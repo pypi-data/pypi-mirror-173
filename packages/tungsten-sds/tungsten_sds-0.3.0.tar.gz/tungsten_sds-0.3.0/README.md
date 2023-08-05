bffb6a6c9f76afb940e53791f2514a31e25b5075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:59 +0000

    Add package tungsten-sds/0.3.0/tungsten_sds-0.3.0.tar.gz

---
+++ packages/tungsten-sds/tungsten_sds-0.3.0.tar.gz/tungsten_sds-0.3.0/README.md
@@ -0,0 +1,35 @@
+<div align="center">
+    <a align="center" href="https://pypi.org/project/tungsten-sds/">
+        <img src="https://raw.githubusercontent.com/Den4200/tungsten/main/assets/tungsten-wide-dark-bg-pad.png" align="center" alt="Tungsten" />
+    </a>
+    <h1 align="center">Tungsten</h1>
+    <p align="center">A material safety data sheet parser.</p>
+</div>
+
+## Installation
+Tungsten is available on PyPi via pip. To install, run the following command:
+```sh
+pip install tungsten-sds
+```
+
+## Usage Example
+```python
+from pathlib import Path
+
+from tungsten import SigmaAldrichSdsParser
+
+sds_parser = SigmaAldrichSdsParser()
+sds_path = Path("sigma_aldrich_w4502.pdf")
+
+# Convert PDF file to parsed data
+with open(sds_path, "rb") as f:
+    sds = sds_parser.parse_to_ghs_sds(f, sds_name=sds_path.stem)
+
+# Serialize parsed data to JSON and dump to a file
+with open(sds_path.stem + ".json", "w") as f:
+    sds.dump(f)
+```
+
+## License
+This work is licensed under MIT. Media assets in the `assets` directory are licensed under a
+Creative Commons Attribution-NoDerivatives 4.0 International Public License.
---
+++ packages/tungsten-sds/tungsten_sds-0.3.0.tar.gz/tungsten_sds-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tungsten',
+ 'tungsten.parsers',
+ 'tungsten.parsers.globally_harmonized_system',
+ 'tungsten.parsers.supplier',
+ 'tungsten.parsers.supplier.sigma_aldrich']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pdfminer.six>=20220524,<20220525']
+
+setup_kwargs = {
+    'name': 'tungsten-sds',
+    'version': '0.3.0',
+    'description': 'An MSDS parser.',
+    'long_description': '<div align="center">\n    <a align="center" href="https://pypi.org/project/tungsten-sds/">\n        <img src="https://raw.githubusercontent.com/Den4200/tungsten/main/assets/tungsten-wide-dark-bg-pad.png" align="center" alt="Tungsten" />\n    </a>\n    <h1 align="center">Tungsten</h1>\n    <p align="center">A material safety data sheet parser.</p>\n</div>\n\n## Installation\nTungsten is available on PyPi via pip. To install, run the following command:\n```sh\npip install tungsten-sds\n```\n\n## Usage Example\n```python\nfrom pathlib import Path\n\nfrom tungsten import SigmaAldrichSdsParser\n\nsds_parser = SigmaAldrichSdsParser()\nsds_path = Path("sigma_aldrich_w4502.pdf")\n\n# Convert PDF file to parsed data\nwith open(sds_path, "rb") as f:\n    sds = sds_parser.parse_to_ghs_sds(f, sds_name=sds_path.stem)\n\n# Serialize parsed data to JSON and dump to a file\nwith open(sds_path.stem + ".json", "w") as f:\n    sds.dump(f)\n```\n\n## License\nThis work is licensed under MIT. Media assets in the `assets` directory are licensed under a\nCreative Commons Attribution-NoDerivatives 4.0 International Public License.\n',
+    'author': 'Dennis Pham',
+    'author_email': 'dennis@dennispham.me',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Den4200/tungsten',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
