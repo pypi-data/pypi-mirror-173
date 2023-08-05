e8d4a7d535a892d3b6e7b714e01b27ed01130bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:52 +0000

    Add package pyhealth/1.0a0/pyhealth-1.0a0.tar.gz

---
+++ packages/pyhealth/pyhealth-1.0a0.tar.gz/pyhealth-1.0a0/pyhealth/__init__.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+__version__ = "1.0a"
+
+BASE_CACHE_PATH = os.path.join(str(Path.home()), ".cache/pyhealth/")
+
+if not os.path.exists(BASE_CACHE_PATH):
+    os.makedirs(BASE_CACHE_PATH)
