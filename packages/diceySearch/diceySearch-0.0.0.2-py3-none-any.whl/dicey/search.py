235347a4fbbd0170cf7691584b4d78d837e7df69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:24 +0000

    Add package diceySearch/0.0.0.2/diceySearch-0.0.0.2-py3-none-any.whl

---
+++ packages/diceySearch/diceySearch-0.0.0.2-py3-none-any.whl/dicey/search.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def embedding_func(id, query, images=None):
+    embedding_api = "http://43.200.239.247:8000/embedding/"
+    if images:
+      embed_data = {
+          "images": images,
+          "query": query,
+          "id": str(id)
+      }
+    else:
+      embed_data = {
+          "images": [],
+          "query": query,
+          "id": str(id)
+      }
+    response = requests.post(embedding_api, data=json.dumps(embed_data), headers={"Content-Type": "application/json"},)
+    res = tuple(response.json()["result"])
+    return res
+
+def data2vectordb(total_result=None):
+  upsert_api = "http://52.79.176.35:8000/upsert/"
+  if total_result is None:
+    response = requests.get(upsert_api)
+  else:
+    response = requests.post(upsert_api, data=json.dumps({"result": total_result}), headers={"Content-Type": "application/json"},)
+  return "Done"
+
+def search_func(query, top_k, debug=False):
+    search_api = "http://52.79.176.35:8000/search/"
+    search_data = {
+    "query": query,
+    "top_k": top_k,
+    'debug': debug
+    }
+    response = requests.post(search_api, data=json.dumps(search_data), headers={"Content-Type": "application/json"},)
+    res = response.json()
+    return res['result'], res['score'], res['meta']
