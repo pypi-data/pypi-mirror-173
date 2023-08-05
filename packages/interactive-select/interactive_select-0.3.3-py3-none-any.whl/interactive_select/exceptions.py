5db5490b704eeee356ed034fbc0aa25f0d180021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:27 +0000

    Add package interactive-select/0.3.3/interactive_select-0.3.3-py3-none-any.whl

---
+++ packages/interactive-select/interactive_select-0.3.3-py3-none-any.whl/interactive_select/exceptions.py
@@ -0,0 +1,14 @@
+class ShortcutNotGenerated(Exception):
+    pass
+
+
+class FailedToParseInput(Exception):
+    pass
+
+
+class TooManyItemsSelected(Exception):
+    pass
+
+
+class TooFewItemsSelected(Exception):
+    pass
