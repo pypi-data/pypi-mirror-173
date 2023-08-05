5c909de7b1550912db2ef03a4d80d82cf8be9a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package toolbox-runner/0.0.2/toolbox_runner-0.0.2.tar.gz

---
+++ packages/toolbox-runner/toolbox_runner-0.0.2.tar.gz/toolbox_runner-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='toolbox_runner',
+    author='Mirko Mälicke',
+    author_email='mirko@hydrocode.de',
+    description='Run data processing tools from docker containers from Python',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=requirements()
+)
