99ea4b3bfe5aedd72b37f122a7595c3440d71214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:52 +0000

    Add package ai-pill/0.0.0/ai-pill-0.0.0.tar.gz

---
+++ packages/ai-pill/ai-pill-0.0.0.tar.gz/ai-pill-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf8
+#
+"""
+This module contains the required libraries and softwares allowing to execute the software,
+and setup elements to configure and identify the software.
+"""
+
+from codecs import open as copen
+from setuptools import setup
+
+CMDCLASS = {}
+
+setup(
+    long_description="AI-PILL"
+)
