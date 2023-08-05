df47820df580626bc6b543e33cdd97dde4fb1fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:17 +0000

    Add package pysolid/0.2.3/pysolid-0.2.3.tar.gz

---
+++ packages/pysolid/pysolid-0.2.3.tar.gz/pysolid-0.2.3/src/pysolid/__init__.py
@@ -0,0 +1,14 @@
+# get version info
+from pysolid.version import version as __version__
+
+# top-level functions
+from pysolid.grid import (
+    calc_solid_earth_tides_grid,
+    plot_solid_earth_tides_grid,
+)
+from pysolid.point import (
+    TIDES,
+    calc_solid_earth_tides_point,
+    plot_solid_earth_tides_point,
+    plot_power_spectral_density4tides,
+)
