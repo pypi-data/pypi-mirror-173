852d5077f1ba3c56f187333376390db5d0e940fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:17 +0000

    Add package functions-by-sercantopcu/0.1.0/functions_by_sercantopcu-0.1.0.tar.gz

---
+++ packages/functions-by-sercantopcu/functions_by_sercantopcu-0.1.0.tar.gz/functions_by_sercantopcu-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_sercantopcu']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-sercantopcu',
+    'version': '0.1.0',
+    'description': 'This is our test project',
+    'long_description': '',
+    'author': 'somename',
+    'author_email': 'sercantopcu1@icloud.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
