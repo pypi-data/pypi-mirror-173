106104c5b4e9bb86dbcdf150acedee72299f0908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:37 +0000

    Add package beaker-pyteal/0.3.5/beaker-pyteal-0.3.5.tar.gz

---
+++ packages/beaker-pyteal/beaker-pyteal-0.3.5.tar.gz/beaker-pyteal-0.3.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name="beaker",
+    version="0.3.5",
+    python_requires=">=3.10",
+    packages=find_packages(),
+    license="MIT",
+    long_description=open("README.md").read(),
+    package_data={"beaker": ["py.typed"]},
+    install_requires=["pyteal>=0.19.0", "py-algorand-sdk>=1.16.1"],
+)
