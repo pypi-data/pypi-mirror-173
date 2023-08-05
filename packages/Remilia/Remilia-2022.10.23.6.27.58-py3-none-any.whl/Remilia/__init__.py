35dd5e00782fe0db8841d1fe91a775b6b4a528af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:20 +0000

    Add package Remilia/2022.10.23.6.27.58/Remilia-2022.10.23.6.27.58-py3-none-any.whl

---
+++ packages/Remilia/Remilia-2022.10.23.6.27.58-py3-none-any.whl/Remilia/__init__.py
@@ -0,0 +1,45 @@
+from . import (
+    LiteLog,
+    LiteTasks,
+    LiteMixin,
+    LitePGL,
+    LiteEvent,
+    LiteConfig,
+    LiteResource,
+    LiteI18n,
+    LiteThread,
+    base,
+    utils,
+    Logger,
+    )
+
+__all__=[
+    "LiteLog",
+    "LiteMixin",
+    "LiteThread",
+    "LitePGL",
+    "LiteResource",
+    "LiteTasks",
+    "LiteEvent",
+    "LiteConfig",
+    "LiteI18n",
+    "base",
+    "utils",
+    "Logger",
+    ]
+
+__version__="2022.10.23.06.27.58"
+
+def __REQUIREMENTS__():
+    '''
+## if you want to use some class in our lib,you must install the lib below
+### utils/cli
+- #### click >= 8.0
+- #### prompt_toolkit >= 3.0.31
+
+### utils/net/pixiv
+- #### pixivpy_async >= 1.2.14
+    '''
+    info="".join(__import__("inspect").getsourcelines(__REQUIREMENTS__)[0][2:-4])
+    print(info)
+    return info
