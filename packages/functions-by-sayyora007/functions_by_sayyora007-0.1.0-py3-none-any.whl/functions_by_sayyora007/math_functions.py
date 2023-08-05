dc8d3be05d4d5ce5ba61b850de51aa073d619d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package functions-by-sayyora007/0.1.0/functions_by_sayyora007-0.1.0-py3-none-any.whl

---
+++ packages/functions-by-sayyora007/functions_by_sayyora007-0.1.0-py3-none-any.whl/functions_by_sayyora007/math_functions.py
@@ -0,0 +1,14 @@
+def multiplication(NUMBER1, NUMBER2):
+    print(NUMBER1 * NUMBER2)
+
+
+def divison(NUMBER1, NUMBER2):
+    print(NUMBER1 / NUMBER2)
+
+
+def addition(NUMBER1, NUMBER2):
+    print(NUMBER1 + NUMBER2)
+
+
+def substraction(NUMBER1, NUMBER2):
+    print(NUMBER1 - NUMBER2)
