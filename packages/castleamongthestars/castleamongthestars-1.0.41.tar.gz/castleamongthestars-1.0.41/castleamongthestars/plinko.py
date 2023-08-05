2fa066ed5f9232ce768694ef9ea4ed028c7c13ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package castleamongthestars/1.0.41/castleamongthestars-1.0.41.tar.gz

---
+++ packages/castleamongthestars/castleamongthestars-1.0.41.tar.gz/castleamongthestars-1.0.41/castleamongthestars/plinko.py
@@ -0,0 +1,15 @@
+import cloudscraper
+
+scraper = cloudscraper.create_scraper(delay=10,interpreter='nodejs',
+  captcha={'provider': 'return_response'})
+
+def autoplinko(auth, bet_amount, risk, rows):
+    request = scraper.post("https://rest-bf.blox.land/games/plinko/roll", headers={"x-auth-token": auth},
+                           json={"amount": bet_amount, "risk": risk, "rows": rows})
+    checker = request['success']
+    if checker == True:
+        return request
+    else:
+        errorr = request['error']
+        error = {'error': errorr, 'status': False}
+        return error
---
+++ packages/castleamongthestars/castleamongthestars-1.0.41.tar.gz/castleamongthestars-1.0.41/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="castleamongthestars",
+	author = "Xtev",
+	version='1.0.41',
+	description = "A system",
+	packages=["castleamongthestars"]
+)
