19700a35ca506811b9581f849306a196a4ac97a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:25 +0000

    Add package maryam-published/1.1/maryam_published-1.1.tar.gz

---
+++ packages/maryam-published/maryam_published-1.1.tar.gz/maryam_published-1.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="maryam_published",
+    # version=1.0,
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
