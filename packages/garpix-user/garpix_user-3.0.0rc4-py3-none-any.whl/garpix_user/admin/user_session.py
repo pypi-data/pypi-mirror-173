2745796df8c930db8a0045cd62e8f2cc62027a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:45 +0000

    Add package garpix-user/3.0.0rc4/garpix_user-3.0.0rc4-py3-none-any.whl

---
+++ packages/garpix-user/garpix_user-3.0.0rc4-py3-none-any.whl/garpix_user/admin/user_session.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from ..models import UserSession
+
+
+@admin.register(UserSession)
+class UserSessionAdmin(admin.ModelAdmin):
+    fields = ('user', 'token_number', 'recognized', 'last_access', 'email_confirmation_code')
