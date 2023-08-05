b987c441b1e31d97c11298db35c501fb478150ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:43 +0000

    Add package fairdetect-muo/0.5/fairdetect_muo-0.5.tar.gz

---
+++ packages/fairdetect-muo/fairdetect_muo-0.5.tar.gz/fairdetect_muo-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+install_requires = [
+    'shap>=0.41.0',
+    'dalex>=1.4.1'
+]
+
+setup(name='fairdetect_muo',
+      version='0.5',  # Development release
+      description='Library to identify bias in pre-trained models!',
+      url='https://github.com/manoelgadi/fairdetect',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+          packages=['fairdetect'],
+      zip_safe=False,
+      install_requires=install_requires)
