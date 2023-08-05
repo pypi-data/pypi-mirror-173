318fdebba96d979b4b3cb43a6d18d4e7e6cc9120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:13 +0000

    Add package pyfyre/0.6.1/pyfyre-0.6.1-py3-none-any.whl

---
+++ packages/pyfyre/pyfyre-0.6.1-py3-none-any.whl/pyfyre/nodes/buttons.py
@@ -0,0 +1,21 @@
+from browser import DOMEvent
+from pyfyre.styles import Style
+from pyfyre.states import State
+from pyfyre.nodes.base import Node, Element
+from typing import Callable, Optional, Dict, List, Any
+
+
+class Button(Element):
+	def __init__(
+		self,
+		onclick: Callable[[DOMEvent], None],
+		children: Optional[Callable[[], List[Node]]] = None,
+		*,
+		styles: Optional[List[Style]] = None,
+		states: Optional[List[State[Any]]] = None,
+		attrs: Optional[Dict[str, str]] = None
+	) -> None:
+		super().__init__(
+			"button", children, styles=styles, states=states, attrs=attrs
+		)
+		self.add_event_listener("click", onclick)
