db2f00661eed9439c090fa5f7d82c80fe3c58a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:12 +0000

    Add package jddo-probability/0.1/jddo_probability-0.1.tar.gz

---
+++ packages/jddo-probability/jddo_probability-0.1.tar.gz/jddo_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jddo_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jddo_probability'],
+      author = 'Jola Olatunji',
+      author_mail = 'joladavid0@gmail.com',
+      zip_safe=False)
