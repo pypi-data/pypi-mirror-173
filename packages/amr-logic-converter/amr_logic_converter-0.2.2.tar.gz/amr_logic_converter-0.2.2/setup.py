e8713824ec4abe579c044759cb9263d5e5f82404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:11 +0000

    Add package amr-logic-converter/0.2.2/amr_logic_converter-0.2.2.tar.gz

---
+++ packages/amr-logic-converter/amr_logic_converter-0.2.2.tar.gz/amr_logic_converter-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['amr_logic_converter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Penman>=1.2.2,<2.0.0', 'typing-extensions>=3.7.4']
+
+setup_kwargs = {
+    'name': 'amr-logic-converter',
+    'version': '0.2.2',
+    'description': 'Convert Abstract Meaning Representation (AMR) into first-order logic',
+    'long_description': 'None',
+    'author': 'David Chanin',
+    'author_email': 'chanindav@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
