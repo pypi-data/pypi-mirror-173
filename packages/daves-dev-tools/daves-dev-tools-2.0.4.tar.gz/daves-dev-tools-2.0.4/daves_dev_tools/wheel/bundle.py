fe1721a1c5d59b4b31775c9300b0665cb5ce8780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:20 +0000

    Add package daves-dev-tools/2.0.4/daves-dev-tools-2.0.4.tar.gz

---
+++ packages/daves-dev-tools/daves-dev-tools-2.0.4.tar.gz/daves-dev-tools-2.0.4/daves_dev_tools/wheel/bundle.py
@@ -0,0 +1,15 @@
+# from ..utilities import run
+
+# TODO: python -m pip wheel . -w $TMPDIR
+
+
+def bundle() -> str:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == " __main__":
+    main()
