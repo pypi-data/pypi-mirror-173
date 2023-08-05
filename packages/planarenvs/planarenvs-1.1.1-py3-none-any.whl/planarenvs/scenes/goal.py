72de4f99f4164d843f65140870662584af49ca17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:17 +0000

    Add package planarenvs/1.1.1/planarenvs-1.1.1-py3-none-any.whl

---
+++ packages/planarenvs/planarenvs-1.1.1-py3-none-any.whl/planarenvs/scenes/goal.py
@@ -0,0 +1,56 @@
+from MotionPlanningEnv.sphereObstacle import SphereObstacle
+from MotionPlanningGoal.staticSubGoal import StaticSubGoal
+from MotionPlanningGoal.dynamicSubGoal import DynamicSubGoal
+
+staticGoalDict = {
+    "weight": 1.0,
+    "is_primary_goal": True,
+    "indices": [0, 1],
+    "parent_link": 0,
+    "child_link": 3,
+    "desired_position": [1, 2],
+    "epsilon": 0.2,
+    "type": "staticSubGoal",
+}
+
+staticGoal = StaticSubGoal(name="goal1", content_dict=staticGoalDict)
+lineGoalDict = {
+    "weight": 1.0,
+    "is_primary_goal": False,
+    "indices": [1],
+    "parent_link": 0,
+    "child_link": 3,
+    "desired_position": [0.5],
+    "angle": 0.5,
+    "epsilon": 0.2,
+    "type": "staticSubGoal",
+}
+
+lineGoal = StaticSubGoal(name="goal2", content_dict=lineGoalDict)
+analyticGoalDict = {
+    "weight": 1.0,
+    "is_primary_goal": True,
+    "indices": [0, 1],
+    "parent_link": 0,
+    "child_link": 3,
+    "trajectory": ["1 + ca.cos(0.3 * t)", "2"],
+    "epsilon": 0.2,
+    "type": "analyticSubGoal",
+}
+analyticGoal = DynamicSubGoal(name="goal2", content_dict=analyticGoalDict)
+splineDict = {
+    "degree": 2,
+    "controlPoints": [[-2.0, 1.0], [2.0, 0.0], [4.0, 2.0], [3.0, 2.0]],
+    "duration": 10,
+}
+splineGoalDict = {
+    "weight": 1.0,
+    "is_primary_goal": True,
+    "indices": [0, 1],
+    "parent_link": 0,
+    "child_link": 3,
+    "trajectory": splineDict,
+    "epsilon": 0.2,
+    "type": "splineSubGoal",
+}
+splineGoal = DynamicSubGoal(name="goal2", content_dict=splineGoalDict)
