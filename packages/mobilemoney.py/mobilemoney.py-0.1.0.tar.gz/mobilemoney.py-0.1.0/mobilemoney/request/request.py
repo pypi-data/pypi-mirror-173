cd838dec9b062b284cdf2fbafa47e2cfb75af566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:11 +0000

    Add package mobilemoney.py/0.1.0/mobilemoney.py-0.1.0.tar.gz

---
+++ packages/mobilemoney.py/mobilemoney.py-0.1.0.tar.gz/mobilemoney.py-0.1.0/mobilemoney/request/request.py
@@ -0,0 +1,50 @@
+
+
+from .http import HTTPClient
+from .collection import Collection
+from .disbursements import Disbursements
+
+
+class Request:
+    def __init__(self):
+        self.__http = HTTPClient()
+        self.__collection = None
+        self.__disbursement = None
+
+    def http(self) -> HTTPClient:
+        """
+        Method to get the HTTP client
+
+        Arguments:
+            None
+        
+        Returns:
+            HTTP client : HTTPClient
+        """
+
+        return self.__http
+    def collection(self, subscription_key: str,  http : HTTPClient) -> Collection:
+        """
+        Method to get the collection client
+
+        Arguments:
+            None
+        
+        Returns:
+            Collection client : Collection
+        """
+        self.__collection = Collection(http, subscription_key)
+        return self.__collection
+    def disbursements(self, subscription_key: str, http: HTTPClient) -> Disbursements:
+        """
+        Method to get the Disbursements client
+
+        Arguments:
+            None
+        
+        Returns:
+            Disbursements client : Disbursements
+        """
+
+        self.__disbursement = Disbursements(http, subscription_key)
+        return self.__disbursement
