f60858bb070ba1b9e85a78d7ae4a340b7dee5d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:24 +0000

    Add package example-package-nagore-ainhoa/0.0.1/example_package_nagore_ainhoa-0.0.1-py3-none-any.whl

---
+++ packages/example-package-nagore-ainhoa/example_package_nagore_ainhoa-0.0.1-py3-none-any.whl/example_package_nagore_ainhoa/__init__.py
@@ -0,0 +1,9 @@
+#from .functions import EDA_class
+
+# eda = EDA_class(df)
+
+# eda.Nulls_Zeros()
+# eda.Duplicates_UniqueValues()
+# eda.Numeric_Variables()
+# eda.Categorical_Variables()
+# eda.Descriptives()
