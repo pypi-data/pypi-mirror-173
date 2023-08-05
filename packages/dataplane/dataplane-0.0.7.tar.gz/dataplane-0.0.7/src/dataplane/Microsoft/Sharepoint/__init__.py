474faa294749e58b26042c8bf612225bd06560aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:06 +0000

    Add package dataplane/0.0.7/dataplane-0.0.7.tar.gz

---
+++ packages/dataplane/dataplane-0.0.7.tar.gz/dataplane-0.0.7/src/dataplane/Microsoft/Sharepoint/__init__.py
@@ -0,0 +1,9 @@
+from dataplane.Microsoft.Sharepoint.sharepoint_download import (
+    sharepoint_download,
+)
+
+from dataplane.Microsoft.Sharepoint.sharepoint_upload import (
+    sharepoint_upload,
+)
+
+__all__ = ["sharepoint_download", "sharepoint_upload"]
