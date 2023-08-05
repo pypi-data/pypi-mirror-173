9771507d2a4f90bd5f96e3201459415f17559a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package gui-api-tkinter/0.0.7/gui-api-tkinter-0.0.7.tar.gz

---
+++ packages/gui-api-tkinter/gui-api-tkinter-0.0.7.tar.gz/gui-api-tkinter-0.0.7/gui_api_tkinter/lib/services.py
@@ -0,0 +1,13 @@
+from .logger_null import LoggerNull
+
+## holds reference to Cfg
+cfg = None
+
+## holds reference to logger
+logger = LoggerNull()
+
+## holds GUI API instance
+guiapi = None
+
+## holds reference to GUI API server
+server = None
