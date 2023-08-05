cbc02273d04ee00a1c6559917f653634360c037e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:46 +0000

    Add package ais-dom/2022.10.5/ais_dom-2022.10.5-py3-none-any.whl

---
+++ packages/ais-dom/ais_dom-2022.10.5-py3-none-any.whl/homeassistant/components/ais_updater/scripts/fix_script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "2022-10-24" > ~/AIS/.ais_fix
---
+++ packages/ais-dom/ais_dom-2022.10.5-py3-none-any.whl/homeassistant/components/braviatv/const.py
@@ -0,0 +1,16 @@
+"""Constants for Bravia TV integration."""
+from __future__ import annotations
+
+from typing import Final
+
+ATTR_CID: Final = "cid"
+ATTR_MAC: Final = "macAddr"
+ATTR_MANUFACTURER: Final = "Sony"
+ATTR_MODEL: Final = "model"
+
+CONF_IGNORED_SOURCES: Final = "ignored_sources"
+CONF_USE_PSK: Final = "use_psk"
+
+CLIENTID_PREFIX: Final = "HomeAssistant"
+DOMAIN: Final = "braviatv"
+NICKNAME: Final = "Home Assistant"
