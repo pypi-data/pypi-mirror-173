201b09534c01c734959805e3530c94655ff19062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:47 +0000

    Add package funcsforspo-l/1.0.0/FuncsForSPO_l-1.0.0.tar.gz

---
+++ packages/funcsforspo-l/FuncsForSPO_l-1.0.0.tar.gz/FuncsForSPO_l-1.0.0/FuncsForSPO_l/__init__.py
@@ -0,0 +1,11 @@
+"""
+Funções criadas por Gabriel Lopes de Souza
+
+
+
+Porque, assim como relâmpago sai do oriente e se mostra até o ocidente,
+assim será também a vinda do Filho do homem - Mateus 24:27
+
+Conheça a nossa empresa!
+https://paycon.com.br/
+"""
