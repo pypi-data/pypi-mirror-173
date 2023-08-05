a756338fe8a838ea221196150c964b6177b4aa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package richvalues/1.0.0/richvalues-1.0.0.tar.gz

---
+++ packages/richvalues/richvalues-1.0.0.tar.gz/richvalues-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name = 'richvalues',
+    version = '1.0.0',
+    license = 'BSD-3-Clause',
+    author = 'Andrés Megías Toledano',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    url = 'https://github.com/andresmegias/richvalues',
+    install_requires = ['numpy', 'pandas']
+)
