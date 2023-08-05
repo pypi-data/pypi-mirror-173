02cd1a55d9f0f435ee7f3811d8c739f620c68151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:33 +0000

    Add package pyDE1/1.6.0a20221023/pyDE1-1.6.0a20221023.tar.gz

---
+++ packages/pyDE1/pyDE1-1.6.0a20221023.tar.gz/pyDE1-1.6.0a20221023/src/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "f206188",
+    "subject": "scale: Don't release on DE1 sleep unless captured",
+    "ref_names": "HEAD -> devel-2022-10",
+    "author_date": "Sun Oct 23 08:54:50 2022 -0700",
+    "commit_date": "Sun Oct 23 08:54:50 2022 -0700",
+}
+source_data = {
+    "git": git_data,
+}
