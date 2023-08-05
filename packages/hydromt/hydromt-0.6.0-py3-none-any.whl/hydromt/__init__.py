88b039493cd8ad0ed41acb8dc8462c0e0a39d690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:58 +0000

    Add package hydromt/0.6.0/hydromt-0.6.0-py3-none-any.whl

---
+++ packages/hydromt/hydromt-0.6.0-py3-none-any.whl/hydromt/__init__.py
@@ -0,0 +1,17 @@
+"""HydroMT: Automated and reproducible model building and analysis"""
+
+# version number without 'v' at start
+__version__ = "0.6.0"
+
+import warnings
+
+# required for accessor style documentation
+from xarray import DataArray, Dataset
+
+# submoduls
+from . import cli, workflows, stats, flw, raster, vector
+
+# high-level methods
+from .models import *
+from .io import *
+from .data_catalog import *
