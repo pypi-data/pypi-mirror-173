169f9186505839a82e3491df9c111786a8322e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:29 +0000

    Add package bqemulatormanager/0.1.1/bqemulatormanager-0.1.1.tar.gz

---
+++ packages/bqemulatormanager/bqemulatormanager-0.1.1.tar.gz/bqemulatormanager-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bqemulatormanager']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['db-dtypes>=1.0.4,<2.0.0',
+ 'google-cloud-bigquery==2.34.4',
+ 'pandas>=1.5.1,<2.0.0',
+ 'pyyaml>=6.0,<7.0']
+
+setup_kwargs = {
+    'name': 'bqemulatormanager',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'gyuta',
+    'author_email': 'kuroshiba0408@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
