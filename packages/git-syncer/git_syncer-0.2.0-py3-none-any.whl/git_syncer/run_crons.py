2661447cc53f6a2490c0b97caa0791ecdd2c5bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package git-syncer/0.2.0/git_syncer-0.2.0-py3-none-any.whl

---
+++ packages/git-syncer/git_syncer-0.2.0-py3-none-any.whl/git_syncer/run_crons.py
@@ -0,0 +1,42 @@
+import logging
+from typing import List
+
+import pycron
+
+from git_syncer.models import CronJob
+from git_syncer.utils.logger import wrap
+
+log = logging.getLogger(__name__)
+
+CRON_JOBS: List[CronJob] = []
+
+
+def run_crons():
+    jobs_to_run = _get_jobs_to_run()
+    # First checking and then running because things might take longer then 1 minute.
+    _run_jobs(jobs_to_run=jobs_to_run)
+
+
+def _get_jobs_to_run() -> List[CronJob]:
+    log.info(f"Checking {wrap(len(CRON_JOBS))} cron jobs.")
+    jobs_to_run = []
+    for job in CRON_JOBS:
+        if pycron.is_now(job.expression):
+            jobs_to_run.append(job)
+            log.debug(f"Cron {wrap(job.verbose_name)} will run.")
+    return jobs_to_run
+
+
+def _run_jobs(jobs_to_run: List[CronJob]):
+    log.debug(f"Total {wrap(len(jobs_to_run))} crons will run.")
+    # TODO: Maybe run async in parallel?
+    for job in jobs_to_run:
+        try:
+            log.debug(f"Running cron {wrap(job.verbose_name)}")
+            job.run()
+        except:  # noqa
+            log.exception(f"Job {wrap(job.verbose_name)} execution failed")
+
+
+def add_cron_jobs(*jobs: CronJob):
+    CRON_JOBS.extend(jobs)
