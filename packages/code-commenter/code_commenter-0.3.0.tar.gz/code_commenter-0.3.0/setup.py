c9bebd73ac9566623c506d38a59e4769bc62e086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:06 +0000

    Add package code-commenter/0.3.0/code_commenter-0.3.0.tar.gz

---
+++ packages/code-commenter/code_commenter-0.3.0.tar.gz/code_commenter-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['code_commenter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyperclip>=1.8.2,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['generate-comment = code_commenter.__main__:run_cli']}
+
+setup_kwargs = {
+    'name': 'code-commenter',
+    'version': '0.3.0',
+    'description': 'Lightweight tool to generate decorative code comments to enhance readability',
+    'long_description': '# Code Commenter\nGenerate stylized code comment headers to help make your code more readable\n\n## Basic Usage\nTo print a comment header to the console run\n```bash\ngenerate-comment -t My_Script_Heading\n```\nand if you wish to have it copied to your clipboard, add the `-c` flag\n```bash\ngenerate-comment -t My_Script_Heading -c\n```\nCode commenter supports multiple language comments. The default is Python but if you wish to generate a comment for example, Golang, set the `-lang` flag.\n```bash\ngenerate-comment -t My_Script_Heading -lang GO\n```\n',
+    'author': 'Tristan Naidoo',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
