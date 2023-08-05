ef70062ad7188234b1df93fbcd45cf51162ec85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:15 +0000

    Add package speedyfit/0.2.3/speedyfit-0.2.3.tar.gz

---
+++ packages/speedyfit/speedyfit-0.2.3.tar.gz/speedyfit-0.2.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    "numpy",
+    "emcee",
+    "astropy>=5.1",
+    "astroquery",
+    "pyvo>=1.4",
+    "matplotlib",
+    "pyaml",
+    "corner",
+    "tqdm",
+    "scipy",
+    "h5py",
+    "pandas",
+    "gaiadr3-zeropoint",
+]
+setup(
+    name="speedyfit",
+    version="0.2.3",
+    author="Joris Vos",
+    author_email="joris.vos@uv.cl",
+    description="MC approach to fit photometric SEDs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vosjo/speedyfit",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    test_suite='pytest.collector',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['speedyfit=speedyfit.main:main', 'speedyfit-batch=speedyfit.speedyfit_batch:main'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    python_requires='>=3.7',
+)
