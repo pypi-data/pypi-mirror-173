8f0deeb6bea51f01c60184e42fc116de25c0aa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:29 +0000

    Add package dice-prob-distribution/1.2/dice_prob_distribution-1.2.tar.gz

---
+++ packages/dice-prob-distribution/dice_prob_distribution-1.2.tar.gz/dice_prob_distribution-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dice_prob_distribution',
+      version='1.2',
+      description='Gaussian and Binomial distributions calculations',
+      packages=['dice_prob_distribution'],
+      author='Desmond Odera',
+      zip_safe=False)
