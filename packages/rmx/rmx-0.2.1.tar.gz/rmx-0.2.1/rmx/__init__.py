8ece003f305d04d4fd16a4f81a10774e6630dcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:50 +0000

    Add package rmx/0.2.1/rmx-0.2.1.tar.gz

---
+++ packages/rmx/rmx-0.2.1.tar.gz/rmx-0.2.1/rmx/__init__.py
@@ -0,0 +1,9 @@
+from ._version import __version__  # NOQA
+import colorlog
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(name)s: %(message)s'))
+
+logger = colorlog.getLogger('rmx-logger')
+logger.addHandler(handler)
