314afc3932852ae612489f9bdfe9732445c0f4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package Data-Generator-LRT/0.0.2/Data_Generator_LRT-0.0.2.tar.gz

---
+++ packages/Data-Generator-LRT/Data_Generator_LRT-0.0.2.tar.gz/Data_Generator_LRT-0.0.2/src/Data_Generator_LRT.py
@@ -0,0 +1,24 @@
+#this code sets up a restricted data set generator for use in a project focused on machine learning methods centered
+#centered around linear regression.
+
+import numpy as np
+
+class Data_Generator_LRT:
+    x_1 = np.random.randint(low=1, high=20, size=100)
+    x_2 = np.random.randint(low=1, high=20, size=100)
+    x_3 = np.random.randint(low=1, high=20, size=100)
+
+    def __init__(self):
+        self.intercept = np.random.randint(low=1, high=20, size=1)
+        self.X_0 = np.column_stack([self.x_1, self.x_2, self.x_3])
+
+    def calculate_y(self, weight_1, weight_2, weight_3):
+        linear_y = [weight_1 * x for x in x_1]
+        linear_y_2 = [linear_y[i] + weight_2 * x_2[i] for i in range(0, len(x_2))]
+        linear_y_3 = [linear_y_2[i] - weight_3 * x_3[i] for i in range(0, len(x_3))]
+        self.linear_y = np.array(linear_y_3) + self.intercept
+        noise = noise = np.random.normal(10, 3, 100)
+        self.linear_y_noise = [linear_y_3[i] + noise[i] for i in range(0, len(noise))]
+
+    def for_josh():
+        print('Fuck You')
