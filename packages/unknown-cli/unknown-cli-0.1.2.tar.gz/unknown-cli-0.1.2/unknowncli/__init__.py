8506ce1e2f7550c66782c6fd47322bdb0cfc021c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package unknown-cli/0.1.2/unknown-cli-0.1.2.tar.gz

---
+++ packages/unknown-cli/unknown-cli-0.1.2.tar.gz/unknown-cli-0.1.2/unknowncli/__init__.py
@@ -0,0 +1,18 @@
+import os
+from typer import Typer, Context, Argument, Option, echo, secho, confirm
+
+app = Typer()
+
+
+def read_version():
+    directory = os.path.dirname(__file__)
+    with open(os.path.join(directory, "VERSION"), "r") as version_file:
+        version = version_file.readline().strip()
+        return version
+
+
+__version__ = read_version()
+
+
+def get_version_string():
+    return f"unknown-cli/{__version__}"
