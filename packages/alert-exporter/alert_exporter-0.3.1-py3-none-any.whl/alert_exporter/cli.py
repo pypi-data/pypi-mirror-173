67cd05ec1c28024803ccdd6ab7184518effc3d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:18 +0000

    Add package alert-exporter/0.3.1/alert_exporter-0.3.1-py3-none-any.whl

---
+++ packages/alert-exporter/alert_exporter-0.3.1-py3-none-any.whl/alert_exporter/cli.py
@@ -0,0 +1,157 @@
+"""Console script for alert_exporter."""
+import json
+import logging
+import os
+import sys
+from argparse import SUPPRESS, ArgumentParser, Namespace
+from pathlib import Path
+
+from jinja2 import (
+    Environment,
+    FileSystemLoader,
+    PackageLoader,
+    Template,
+    select_autoescape,
+)
+
+from alert_exporter.sources.cloudwatch import Cloudwatch
+from alert_exporter.sources.kubernetes import Kubernetes
+
+AVAILABLE_FORMATS = {
+    "csv": "csv",
+    "html": "html",
+    "md": "markdown",
+    "yaml": "yaml",
+    "yml": "yaml",
+}
+
+
+def init_args() -> Namespace:
+    parser = ArgumentParser(
+        description="Extract alerts configured in different sources"
+        " (eg: Prometheus Rules, CloudWatch Alarms, etc.)",
+        usage=SUPPRESS,
+    )
+    parser.add_argument("-v", "--version", action="version", version="Version 0.3.1")
+    parser.add_argument(
+        "--log-level",
+        default="WARNING",
+        choices=[
+            "DEBUG",
+            "INFO",
+            "WARNING",
+            "ERROR",
+        ],
+    )
+    parser.add_argument("-o", "--output-file", required=True)
+    parser.add_argument("--jinja-template", nargs="?")
+    parser.add_argument("-f", "--format", choices=list(set(AVAILABLE_FORMATS.values())))
+    parser.add_argument("--prometheus", default=False, action="store_true")
+    parser.add_argument("--prometheus-filters", default={}, type=json.loads)
+    parser.add_argument("--context", nargs="?")
+    parser.add_argument("--cloudwatch", default=False, action="store_true")
+    parser.add_argument("--aws-profile", default=os.getenv("AWS_PROFILE", None))
+    parser.add_argument(
+        "--aws-region",
+        help="Specific region to target. Default: Iterate over all regions available.",
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+def get_template_file(
+    output_file: str, output_format: str, jinja2_template: str
+) -> str:
+    if output_format:
+        if output_format == "markdown":
+            return "alerts.md.jinja"
+        else:
+            return f"alerts.{output_format}.jinja"
+    else:
+        if jinja2_template:
+            return jinja2_template
+    file_extension = output_file.split(".")[-1]
+    if not file_extension:
+        logging.error(
+            "The output format couldn't be automatically guessed from the"
+            " file extension. Please use one of the available formats"
+            " or provide a Jinja2 template."
+        )
+        sys.exit(1)
+    if file_extension in AVAILABLE_FORMATS:
+        if AVAILABLE_FORMATS[file_extension] == "markdown":
+            return "alerts.md.jinja"
+        return f"alerts.{AVAILABLE_FORMATS[file_extension]}.jinja"
+    logging.error(
+        f"No output format detected for the output file extension '{file_extension}'."
+        " If you want to export in a custom format,"
+        " you need to provide a Jinja2 file with the --jinja-template flag."
+    )
+    sys.exit(1)
+
+
+def render_template(template: Template, rules: list, output_file: str) -> None:
+    rendered = template.render(rules=rules)
+    if "/" in output_file:
+        absolute_file = output_file
+    else:
+        absolute_file = f"{os.getcwd()}/{output_file}"
+    with open(absolute_file, "w+") as f:
+        f.write(rendered)
+
+
+def main():
+    args = init_args()
+    logging.basicConfig(
+        level=args.log_level,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    rules = []
+    if args.prometheus:
+        k = Kubernetes(context=args.context, filters=args.prometheus_filters)
+        k.get_prometheus_rules()
+        rules += k.rules
+    if args.cloudwatch:
+        c = Cloudwatch(
+            profile=args.aws_profile,
+            region=args.aws_region,
+            debug=bool(args.log_level == "DEBUG"),
+        )
+        c.get_alarms(profile=args.aws_profile, debug=bool(args.log_level == "DEBUG"))
+        rules += c.rules
+    if len(rules) == 0:
+        logging.warning("No alert rule found.")
+        return 1
+
+    if args.jinja_template:
+        path = Path(args.jinja_template)
+        if not path.exists():
+            logging.error(f"The template file {str(path)} does not exist.")
+            return 1
+        template_file = path.name
+        env = Environment(loader=FileSystemLoader(path.parent.absolute()))
+    else:
+        template_file = get_template_file(
+            output_file=args.output_file,
+            output_format=args.format,
+            jinja2_template=args.jinja_template,
+        )
+        env = Environment(
+            loader=PackageLoader("alert_exporter"), autoescape=select_autoescape()
+        )
+    template = env.get_template(template_file)
+    render_template(
+        template=template,
+        rules={
+            "prometheus": k.rules if args.prometheus else [],
+            "cloudwatch": c.rules if args.cloudwatch else [],
+        },
+        output_file=args.output_file,
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/alert-exporter/alert_exporter-0.3.1-py3-none-any.whl/alert_exporter/sources/cloudwatch.py
@@ -0,0 +1,94 @@
+"""Cloudwatch class for Alert Exporter."""
+
+import logging
+from datetime import timedelta
+from time import sleep
+
+import boto3
+import botocore
+from humanize import naturaldelta
+
+COMPARISON_OPERATORS = {
+    "GreaterThanOrEqualToThreshold": ">=",
+    "GreaterThanUpperThreshold": ">",
+    "LessThanLowerThreshold": "<",
+    "GreaterThanThreshold": ">",
+    "LessThanOrEqualToThreshold": "<=",
+    "LessThanThreshold": "<",
+}
+
+
+class Cloudwatch:
+    """
+    A wrapper for the Cloudwatch boto client
+    """
+
+    def __init__(self, profile: str, region: str, debug: bool) -> None:
+        if not debug:
+            logging.getLogger("boto3").setLevel(logging.ERROR)
+            logging.getLogger("botocore").setLevel(logging.ERROR)
+        try:
+            self.session = boto3.session.Session(profile_name=profile)
+        except botocore.exceptions.ClientError as e:
+            logging.warning(f"Error creating a session with region {region}:")
+            if debug:
+                print(e)
+        if region:
+            self.regions = [region]
+        else:
+            self.regions = self.session.get_available_regions(service_name="sts")
+
+    def init_client(self, profile: str, region: str) -> None:
+        self.session = boto3.session.Session(profile_name=profile)
+        self.client = self.session.client("cloudwatch", region_name=region)
+
+    def build_rule_expression(self, rule: dict) -> str:
+        if rule.get("ComparisonOperator") in [
+            "GreaterThanUpperThreshold",
+            "LessThanLowerThreshold",
+        ]:
+            metrics = {m["Id"]: m for m in rule["Metrics"]}
+            c2 = metrics.pop(rule["ThresholdMetricId"])
+            c1 = metrics.pop(next(iter(metrics)))
+            if metrics:
+                print("DEBUG: Hmmmm weird I didn't expect anything left here.")
+            metric_name = c1["MetricStat"]["Metric"]["MetricName"]
+            threshold = c2["Expression"]
+            period = c1["MetricStat"]["Period"]
+        else:
+            metric_name = rule.get("MetricName")
+            threshold = rule.get("Threshold")
+            period = rule.get("Period")
+        expression = (
+            f"{metric_name}"
+            f' {COMPARISON_OPERATORS[rule.get("ComparisonOperator")]}'
+            f" {threshold}"
+            f' for {rule.get("EvaluationPeriods")} datapoints within'
+            f" {naturaldelta(timedelta(seconds=period))}"
+        )
+        return expression
+
+    def get_alarms(self, profile: str, debug: bool) -> None:
+        self.rules = []
+        for region in self.regions:
+            logging.info(f"Getting alarms from region {region}")
+            try:
+                self.init_client(profile=profile, region=region)
+                alarms = self.client.describe_alarms()
+            except botocore.exceptions.ClientError as e:
+                logging.warning(f"Error while describing alarms in region {region}:")
+                if debug:
+                    print(e)
+                continue
+            for alarm_type in ["CompositeAlarms", "MetricAlarms"]:
+                self.rules += [
+                    {
+                        "region": region,
+                        "type": alarm_type,
+                        "name": r.get("AlarmName", ""),
+                        "description": r.get("AlarmDescription", ""),
+                        "rule": self.build_rule_expression(rule=r),
+                    }
+                    for r in alarms[alarm_type]
+                ]
+            sleep(0.5)
