9ac3b70db3197dba7f65137c0dfbdca4268c4d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:16 +0000

    Add package hopconn/0.1/hopconn-0.1.tar.gz

---
+++ packages/hopconn/hopconn-0.1.tar.gz/hopconn-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'hopconn',
+      version= '0.1',
+      description='Hop connection test',
+      author='Muhammad Hamza',
+      email='peacfulsoul18@gmail.com',
+      packages=['hopconn'])
