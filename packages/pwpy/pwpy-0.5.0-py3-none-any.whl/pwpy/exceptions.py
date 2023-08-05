b9cfb4b41debe773ecf53c1bd88876172fd67e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:52 +0000

    Add package pwpy/0.5.0/pwpy-0.5.0-py3-none-any.whl

---
+++ packages/pwpy/pwpy-0.5.0-py3-none-any.whl/pwpy/exceptions.py
@@ -0,0 +1,73 @@
+# MIT License
+#
+# Copyright (c) 2021 God Empress Verin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import typing
+
+
+__all__: typing.List[str] = [
+    "InvalidToken",
+    "NoTokenProvided",
+    "InvalidQuery",
+    "UnexpectedResponse",
+    "CloudflareInterrupt",
+    "LoginFailure"
+]
+
+
+class InvalidToken(Exception):
+    """
+    Exception raised when the provided API key is invalid.
+    """
+
+
+class NoTokenProvided(Exception):
+    """
+    Exception raised when no global token is set and no local token is provided.
+    """
+
+
+class InvalidQuery(Exception):
+    """
+    Exception raised when the GraphQL query is invalid.
+    """
+
+
+class UnexpectedResponse(Exception):
+    """
+    Exception raised when the GraphQL response is unexpected.
+    """
+
+    def __init__(self, response: str) -> None:
+        self.response = response
+
+
+class CloudflareInterrupt(Exception):
+    """
+    Exception raised when response status not OK, likely due to cloudflare.
+    """
+
+
+class LoginFailure(Exception):
+    """
+    Exception raised when the provided login credentials are invalid.
+    """
---
+++ packages/pwpy/pwpy-0.5.0-py3-none-any.whl/pwpy/scrape.py
@@ -0,0 +1,97 @@
+# MIT License
+#
+# Copyright (c) 2021 God Empress Verin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from pwpy import exceptions, links
+from bs4 import BeautifulSoup
+
+import aiohttp
+import typing
+
+
+__all__: typing.List[str] = [
+    "login",
+    "send_message",
+    "discord"
+]
+
+
+async def login(email: str, password: str, session: aiohttp.ClientSession) -> None:
+    """
+    Login to Politics and War.
+
+    :param email: A valid email address for logging in with.
+    :param password: A valid password for logging in with.
+    :param session: A client session to login on.
+    """
+    login_data = {"email": email, "password": password, "loginform": "Login"}
+
+    async with session.post(links.LOGIN, data=login_data) as response:
+        if "Login Successful" not in str(await response.read()):
+            raise exceptions.LoginFailure("The provided login credentials were invalid!")
+
+
+async def send_message(
+    email: str, password: str, target: str, subject: str, message: str
+) -> None:
+    """
+    Sends a message in Politics and War using provided account information.
+
+    :param email: A valid email address for logging in with.
+    :param password: A valid password for logging in with.
+    :param target: A valid target leader name to message.
+    :param subject: A subject for the message.
+    :param message: The message content to be sent.
+    :return: None
+    """
+    message_data = {
+        "newconversation": "true",
+        "receiver": target,
+        "carboncopy": "",
+        "subject": subject,
+        "body": message,
+        "sndmsg": "Send Message",
+    }
+
+    async with aiohttp.ClientSession() as session:
+        await login(email, password, session)
+
+        async with session.post(links.MESSAGE, data=message_data):
+            pass
+
+
+async def discord(nation: int) -> str or None:
+    """
+    Fetch a discord username from a given nation page.
+
+    :param nation: A valid nation id.
+    :return: The username listed on the specified nation page or None.
+    """
+    selector = 'tr > td > a[alt="Official PW Discord Server"]'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(links.NATION + str(nation)) as response:
+            content = await response.read()
+
+    soup = BeautifulSoup(content, features="html.parser")
+    element = soup.select_one(selector)
+    return element.text if element else None
