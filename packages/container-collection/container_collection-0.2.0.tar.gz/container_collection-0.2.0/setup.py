f12a5adefb0284d9914e5671514610c643b632c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:11 +0000

    Add package container-collection/0.2.0/container_collection-0.2.0.tar.gz

---
+++ packages/container-collection/container_collection-0.2.0.tar.gz/container_collection-0.2.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['container_collection',
+ 'container_collection.batch',
+ 'container_collection.docker',
+ 'container_collection.jobs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.24.92,<2.0.0',
+ 'deepdiff>=5.8.1,<6.0.0',
+ 'docker>=6.0.0,<7.0.0',
+ 'pandas>=1.5.0,<2.0.0',
+ 'prefect>=2.5.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'container-collection',
+    'version': '0.2.0',
+    'description': 'Collection of tasks for running containerized models.',
+    'long_description': '[![Build Status](https://github.com/bagherilab/container-collection/workflows/build/badge.svg)](https://github.com/bagherilab/container-collection/actions?query=workflow%3Abuild)\n[![Codecov](https://img.shields.io/codecov/c/gh/bagherilab/container-collection?token=OH8080ZFCU)](https://codecov.io/gh/bagherilab/container-collection)\n[![Lint Status](https://github.com/bagherilab/container-collection/workflows/lint/badge.svg)](https://github.com/bagherilab/container-collection/actions?query=workflow%3Alint)\n[![Documentation](https://github.com/bagherilab/container-collection/workflows/documentation/badge.svg)](https://bagherilab.github.io/container-collection/)\n[![Code style](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n',
+    'author': 'Jessica S. Yu',
+    'author_email': 'jesyu@uw.edu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
