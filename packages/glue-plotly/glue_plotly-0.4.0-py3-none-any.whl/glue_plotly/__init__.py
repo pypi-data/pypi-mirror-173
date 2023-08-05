7d55cbad3c64d620d64254d8e608dadbf5ccbf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:12 +0000

    Add package glue-plotly/0.4.0/glue_plotly-0.4.0-py3-none-any.whl

---
+++ packages/glue-plotly/glue_plotly-0.4.0-py3-none-any.whl/glue_plotly/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
+
+PLOTLY_LOGO = os.path.abspath(os.path.join(os.path.dirname(__file__), 'logo.png'))
+
+
+def setup():
+
+    from . import html_exporters  # noqa
+
+    from glue.viewers.scatter.qt import ScatterViewer
+    ScatterViewer.subtools = {
+        **ScatterViewer.subtools,
+        "save": ScatterViewer.subtools["save"] + ['save:plotly2d']
+    }
+
+    from glue.viewers.image.qt import ImageViewer
+    ImageViewer.subtools = {
+        **ImageViewer.subtools,
+        "save": ImageViewer.subtools["save"] + ['save:plotlyimage2d']
+    }
+
+    from glue.viewers.histogram.qt import HistogramViewer
+    HistogramViewer.subtools['save'] = HistogramViewer.subtools['save'] + ['save:plotlyhist']
+
+    try:
+        from glue_vispy_viewers.scatter.scatter_viewer import VispyScatterViewer
+    except ImportError:
+        pass
+    else:
+        VispyScatterViewer.subtools['save'] = VispyScatterViewer.subtools['save'] + ['save:plotly3d']
