2cae4c694eae4a7df845f058b7f98eed825889e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:37 +0000

    Add package cornflakes/2.6.8/cornflakes-2.6.8.tar.gz

---
+++ packages/cornflakes/cornflakes-2.6.8.tar.gz/cornflakes-2.6.8/cornflakes/click/_rich_group.py
@@ -0,0 +1,69 @@
+import sys
+from typing import Any, Optional, Union
+
+import click
+
+from cornflakes.click._rich_config import Config as RichConfig
+
+from ._rich_click import rich_abort_error, rich_format_error, rich_format_help
+from ._rich_command import RichCommand
+
+
+class RichGroup(click.Group):
+    """Richly formatted click Group.
+
+    Inherits click.Group and overrides help and error methods
+    to print richly formatted output.
+    """
+
+    command_class = RichCommand
+    group_class = type
+    params = []
+    name = ""
+    context_settings = {}
+    commands = []
+
+    def callback(self):
+        """Callback method with is wrapped over the command group."""
+        pass
+
+    def add_command(self, cmd: Union[RichCommand, Any], name: Optional[str] = None) -> None:
+        """Wrapper of click.core.Groud.add_command to pass configs.
+
+        Registers another :class:`Command` with this group.  If the name
+        is not provided, the name of the command is used.
+        """
+        if not cmd.config:
+            cmd.config = self.config
+        if cmd.config.GLOBAL_OPTIONS:
+            for option_obj in cmd.config.GLOBAL_OPTIONS:
+                cmd.params.extend(option_obj.params)
+        click.core.Group.add_command(self, cmd, name)
+
+        # click.core.Group.add_command(self, cmd_wrapper, name)
+
+    def __init__(self, config: RichConfig = None, *args, **kwargs):
+        """Init function of RichGroup with extra config argument."""
+        super().__init__(*args, **kwargs)
+        self.config = config or None
+
+    def main(self, *args, standalone_mode: bool = True, **kwargs) -> Any:
+        """Main function of RichGroup."""
+        try:
+            rv = super().main(*args, standalone_mode=False, **kwargs)  # type: ignore
+            if not standalone_mode:
+                return rv
+        except click.ClickException as e:
+            if not standalone_mode:
+                raise
+            rich_format_error(e, config=self.config)
+            sys.exit(e.exit_code)
+        except click.exceptions.Abort:
+            if not standalone_mode:
+                raise
+            rich_abort_error(config=self.config)
+            sys.exit(1)
+
+    def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
+        """Format function of RichGroup."""
+        rich_format_help(self, ctx, formatter, config=self.config)
---
+++ packages/cornflakes/cornflakes-2.6.8.tar.gz/cornflakes-2.6.8/cornflakes/decorator/_click_cli.py
@@ -0,0 +1,43 @@
+from inspect import getfile
+from typing import Callable
+
+from click import Group
+import pkg_resources
+
+from cornflakes.click import RichConfig, group, style, version_option
+
+
+def click_cli(*args, callback: Callable = None, **kwargs):
+    """Function that creates generic click CLI Object."""
+
+    def cli_wrapper(w_callback: Callable) -> Callable[..., Group]:
+        if not callable(w_callback):
+            return w_callback
+
+        name = w_callback.__qualname__
+        module = getfile(w_callback)
+        version = "0.0.1"
+
+        if hasattr(w_callback, "__module__"):
+            module = w_callback.__module__.split(".", 1)[0]
+            if module != "__main__":
+                version = pkg_resources.get_distribution(module).version
+
+        config = RichConfig(*args, **kwargs)
+
+        cli_group = version_option(
+            prog_name=name,
+            version=version,
+            message=style(f"\033[95m{module}\033" f"[0m \033[95mVersion\033[0m: \033[1m" f"{version}\033[0m"),
+        )(group(module.split(".", 1)[0], config=config)(w_callback))
+
+        if cli_group.config.GLOBAL_OPTIONS:
+            for option_obj in cli_group.config.GLOBAL_OPTIONS:
+                cli_group.params.extend(option_obj.params)
+        if config.CONTEXT_SETTINGS:
+            cli_group.context_settings = config.CONTEXT_SETTINGS
+        return cli_group
+
+    if callback:
+        return cli_wrapper(callback, *args, **kwargs)
+    return cli_wrapper
