c8cc4352deb11d0a78d881385d48ed67d68dcbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:34 +0000

    Add package pigdetection/0.0.1.5/pigDetection-0.0.1.5.tar.gz

---
+++ packages/pigdetection/pigDetection-0.0.1.5.tar.gz/pigDetection-0.0.1.5/pigDetection/__init__.py
@@ -0,0 +1,1 @@
+from pigDetection.detection import PigDetection
---
+++ packages/pigdetection/pigDetection-0.0.1.5.tar.gz/pigDetection-0.0.1.5/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os.path
+
+def get_file(*paths):
+    path = os.path.join(*paths)
+    try:
+        with open(path, 'rb') as f:
+            return f.read().decode('utf8')
+    except IOError:
+        pass
+
+def get_readme():
+    return get_file(os.path.dirname(__file__), 'README.rst')
+
+
+
+setup(
+  name = 'pigDetection',         # How you named your package folder (MyLib)
+  packages = ['pigDetection'],   # Chose the same as "name"
+  version = '0.0.1.5',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Update',   # Give a short description about your library
+  long_description='plese read in: https://github.com/TNPKpenguin',
+  author = 'Bas Tatsana',                   # Type in your name
+  author_email = 'tatsanasrisawang2@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/UncleEngineer/loongpom',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/TNPKpenguin',    # I explain this later on
+  keywords = ['PigDetection', 'Pig', 'Detection', 'Pig classification'],   # Keywords that define your package best
+  install_requires=[],
+
+)
