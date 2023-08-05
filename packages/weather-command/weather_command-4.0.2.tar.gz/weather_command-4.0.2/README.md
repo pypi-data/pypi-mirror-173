476322f91945d9f7d01fefadee0ec00778785b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:11 +0000

    Add package weather-command/4.0.2/weather_command-4.0.2.tar.gz

---
+++ packages/weather-command/weather_command-4.0.2.tar.gz/weather_command-4.0.2/README.md
@@ -0,0 +1,84 @@
+# Weather Command
+
+[![Tests Status](https://github.com/sanders41/weather-command/workflows/Testing/badge.svg?branch=main&event=push)](https://github.com/sanders41/weather-command/actions?query=workflow%3ATesting+branch%3Amain+event%3Apush)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/sanders41/weather-command/main.svg)](https://results.pre-commit.ci/latest/github/sanders41/weather-command/main)
+[![Coverage](https://codecov.io/github/sanders41/weather-command/coverage.svg?branch=main)](https://codecov.io/gh/sanders41/weather-command)
+[![PyPI version](https://badge.fury.io/py/weather-command.svg)](https://badge.fury.io/py/weather-command)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/weather-command?color=5cc141)](https://github.com/sanders41/weather-command)
+
+A command line weather app
+
+## Installation
+
+Installation with [pipx](https://github.com/pypa/pipx) is recommended.
+
+If you are running on a non-Windows OS you can install with uvloop for more speed.
+
+```sh
+pipx install "weather-command[uvloop]"
+```
+
+uvloop is currently not supported on Windows on this platform install without uvloop.
+
+```sh
+pipx install weather-command
+```
+
+Alternatively Weather Command can be installed with pip.
+
+```sh
+# Non Windows
+pip install weather-command[uvloop]
+
+# Windows
+pip install weather-command
+```
+
+## Usage
+
+First an API key is needed from [OpenWeather](https://openweathermap.org/), A free account is all that
+is needed. Once you have your API key create an environment variable named `OPEN_WEATHER_API_KEY` that
+constains your API key.
+
+```sh
+export OPEN_WEATHER_API_KEY=your-api-key
+```
+
+Each time the shell is restarted this variable will be cleared. To avoid this it can be added to your
+profile. For example if your shell is zsh the API key can be added to the `~/.zshenv` file. Doing this
+will prevent the need to re-add the key each time the shell is started.
+
+To get the weather for a city:
+
+```sh
+weather-command city seattle
+```
+
+Once installed you can also add aliases to your shell to make it quick to get a forecast. For example
+if your shell is zsh you can add something like the following to your `~/.zshrc` file:
+
+```sh
+alias we="weather-command zip 98109 -i --am-pm"
+alias wed="weather-command zip 98109 -i --am-pm -f daily"
+alias weh="weather-command zip 98109 -i --am-pm -f hourly"
+alias wet="weather-command zip 98109 -i --am-pm --tui"
+```
+
+After adding this to the `~/.zshrc` you will need to restart your terminal. After that typing `we`
+will get the current forecast, `wed` will get the daily forecast and `weh` will get the hourly forecast.
+
+### Modes
+
+Weather Command can be run in either CLI (command line interface) mode or TUI (text user interface)
+mode. CLI mode will print the weather to your terminal, while TUI mode will open the weather in an
+interactive terminal app. The default mode is CLI
+
+To run in TUI mode use the `tui` flag
+
+```py
+weather-command zip 98109 -i --am-pm --tui
+```
+
+## Contributing
+
+Contributions to this project are welcome. If you are interesting in contributing please see our [contributing guide](CONTRIBUTING.md)
---
+++ packages/weather-command/weather_command-4.0.2.tar.gz/weather_command-4.0.2/weather_command/main.py
@@ -0,0 +1,247 @@
+import asyncio
+from enum import Enum
+from sys import platform
+from typing import Union
+
+from rich.traceback import install
+from typer import Argument, Exit, Option, Typer, echo
+
+from weather_command._builder import show_current, show_daily, show_hourly
+from weather_command._cache import Cache
+from weather_command._tui import WeatherApp
+
+
+def _is_uvloop_platform() -> bool:
+    if platform != "win32":
+        return True  # pragma: no cover
+
+    return False  # pragma: no cover
+
+
+__version__ = "4.0.2"
+
+install()
+app = Typer()
+
+
+class ForecastType(str, Enum):
+    CURRENT = "current"
+    DAILY = "daily"
+    HOURLY = "hourly"
+
+
+class How(str, Enum):
+    CITY = "city"
+    ZIP = "zip"
+
+
+def _runner(
+    how: str,
+    city_zip: str,
+    state_code: Union[str, None],
+    country_code: Union[str, None],
+    imperial: bool,
+    am_pm: bool,
+    forecast_type: ForecastType,
+    temp_only: bool,
+    clear_cache: bool,
+    tui: bool,
+    terminal_width: Union[int, None],
+) -> None:
+    if clear_cache:
+        cache = Cache()
+        cache.clear()
+
+    units = "imperial" if imperial else "metric"
+
+    if _is_uvloop_platform():
+        try:
+            import uvloop
+
+            uvloop.install()
+        except ImportError:  # pragma: no cover
+            pass
+        except NameError:  # pragma: no cover
+            pass
+
+    if tui:
+        WeatherApp.how = how
+        WeatherApp.city_zip = city_zip
+        WeatherApp.state = state_code
+        WeatherApp.country = country_code
+        WeatherApp.forecast_type = forecast_type
+        WeatherApp.units = units
+        WeatherApp.am_pm = am_pm
+        WeatherApp.run()
+    elif forecast_type == "current":
+        asyncio.run(
+            show_current(
+                how=how,
+                city_zip=city_zip,
+                units=units,
+                state_code=state_code,
+                country_code=country_code,
+                am_pm=am_pm,
+                temp_only=temp_only,
+                terminal_width=terminal_width,
+            )
+        )
+    elif forecast_type == "daily":
+        asyncio.run(
+            show_daily(
+                how=how,
+                city_zip=city_zip,
+                units=units,
+                state_code=state_code,
+                country_code=country_code,
+                am_pm=am_pm,
+                temp_only=temp_only,
+                terminal_width=terminal_width,
+            )
+        )
+    elif forecast_type == "hourly":
+        asyncio.run(
+            show_hourly(
+                how=how,
+                city_zip=city_zip,
+                units=units,
+                state_code=state_code,
+                country_code=country_code,
+                am_pm=am_pm,
+                temp_only=temp_only,
+                terminal_width=terminal_width,
+            )
+        )
+
+
+@app.command()
+def city(
+    city: str = Argument(
+        ...,
+        help="The city for which the weather should be retrieved.",
+    ),
+    state_code: Union[str, None] = Option(
+        None, "--state-code", "-s", help="The name of the state where the city is located."
+    ),
+    country_code: Union[str, None] = Option(
+        None,
+        "--country-code",
+        "-c",
+        help="The country code where the city is located.",
+    ),
+    imperial: bool = Option(
+        False,
+        "--imperial",
+        "-i",
+        help="If this flag is used the units will be imperial, otherwise units will be metric.",
+    ),
+    am_pm: bool = Option(
+        False,
+        "--am-pm",
+        help="If this flag is set the times will be displayed in 12 hour format, otherwise times will be 24 hour format.",
+    ),
+    forecast_type: ForecastType = Option(
+        ForecastType.CURRENT,
+        "--forecast-type",
+        "-f",
+        help="The type of forecast to display.",
+    ),
+    temp_only: bool = Option(
+        False, "--temp-only", "-t", help="If this flag is set only tempatures will be displayed."
+    ),
+    clear_cache: bool = Option(False, help="Clear the cache data before running."),
+    tui: bool = Option(False, help="Run in TUI mode."),
+    terminal_width: Union[int, None] = Option(
+        None, help="Allows for overriding the default terminal width."
+    ),
+) -> None:
+    """Get the weather by city."""
+    _runner(
+        how="city",
+        city_zip=city,
+        state_code=state_code,
+        country_code=country_code,
+        imperial=imperial,
+        am_pm=am_pm,
+        forecast_type=forecast_type,
+        temp_only=temp_only,
+        clear_cache=clear_cache,
+        tui=tui,
+        terminal_width=terminal_width,
+    )
+
+
+@app.command()
+def zip(
+    zip_code: str = Argument(
+        ...,
+        help="The zip code for which the weather should be retrieved.",
+    ),
+    state_code: Union[str, None] = Option(
+        None, "--state-code", "-s", help="The name of the state where the city is located."
+    ),
+    country_code: Union[str, None] = Option(
+        None,
+        "--country-code",
+        "-c",
+        help="The country code where the city is located.",
+    ),
+    imperial: bool = Option(
+        False,
+        "--imperial",
+        "-i",
+        help="If this flag is used the units will be imperial, otherwise units will be metric.",
+    ),
+    am_pm: bool = Option(
+        False,
+        "--am-pm",
+        help="If this flag is set the times will be displayed in 12 hour format, otherwise times will be 24 hour format.",
+    ),
+    forecast_type: ForecastType = Option(
+        ForecastType.CURRENT,
+        "--forecast-type",
+        "-f",
+        help="The type of forecast to display.",
+    ),
+    temp_only: bool = Option(
+        False, "--temp-only", "-t", help="If this flag is set only tempatures will be displayed."
+    ),
+    clear_cache: bool = Option(False, help="Clear the cache data before running."),
+    tui: bool = Option(False, help="Run in TUI mode."),
+    terminal_width: Union[int, None] = Option(
+        None, help="Allows for overriding the default terminal width."
+    ),
+) -> None:
+    """Get the weather by zip code."""
+    _runner(
+        how="zip",
+        city_zip=zip_code,
+        state_code=state_code,
+        country_code=country_code,
+        imperial=imperial,
+        am_pm=am_pm,
+        forecast_type=forecast_type,
+        temp_only=temp_only,
+        clear_cache=clear_cache,
+        tui=tui,
+        terminal_width=terminal_width,
+    )
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    version: Union[bool, None] = Option(
+        None,
+        "--version",
+        "-v",
+        is_eager=True,
+        help="Show the installed version",
+    ),
+) -> None:
+    if version:
+        echo(__version__)
+        raise Exit()
+
+
+if __name__ == "__main__":
+    app()
