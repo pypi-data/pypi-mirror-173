0922ba0a9c1432874e818ee8c726b424e05a9c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package audio-data-pytorch/0.0.17/audio-data-pytorch-0.0.17.tar.gz

---
+++ packages/audio-data-pytorch/audio-data-pytorch-0.0.17.tar.gz/audio-data-pytorch-0.0.17/audio_data_pytorch/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .audio_web_dataset import AudioWebDataset, AudioWebDatasetPreprocess
+from .clotho_dataset import ClothoDataset
+from .common_voice_dataset import CommonVoiceDataset
+from .libri_speech_dataset import LibriSpeechDataset
+from .lj_speech_dataset import LJSpeechDataset
+from .wav_dataset import WAVDataset
+from .youtube_dataset import YoutubeDataset
