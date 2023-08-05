d3cd0ddd3c83ee58be75a2fd809c4afc0d091e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package lgtm-art/1.0.3/lgtm-art-1.0.3.tar.gz

---
+++ packages/lgtm-art/lgtm-art-1.0.3.tar.gz/lgtm-art-1.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name='lgtm-art',
+    version='1.0.3',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['opencv-python>=4.6.0.66'],
+    entry_points={
+        'console_scripts': "lgtm-art=lgtm:cli"
+    },
+    download_url='https://github.com/Steeeephen/LGTM-Art/archive/refs/tags/1.0.3.tar.gz'
+)
