480a5596ef5a55884380ac5378b215d6c913bcd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:21 +0000

    Add package nsj-rest-lib/0.0.4/nsj_rest_lib-0.0.4-py3-none-any.whl

---
+++ packages/nsj-rest-lib/nsj_rest_lib-0.0.4-py3-none-any.whl/nsj_rest_lib/exception.py
@@ -0,0 +1,18 @@
+class DTOConfigException(Exception):
+    pass
+
+
+class DTOListFieldConfigException(Exception):
+    pass
+
+
+class MissingParameterException(Exception):
+    _parameter_name: str
+
+    def __init__(self, parameter_name: str):
+        super().__init__(f"Missing parameter: {parameter_name}")
+        self._parameter_name = parameter_name
+
+
+class NotFoundException(Exception):
+    pass
