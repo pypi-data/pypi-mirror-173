6a9f2e26ea6bfb92127443f20789db15c60069e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:05 +0000

    Add package castleamongthestars/1.0.40/castleamongthestars-1.0.40.tar.gz

---
+++ packages/castleamongthestars/castleamongthestars-1.0.40.tar.gz/castleamongthestars-1.0.40/castleamongthestars/accounts.py
@@ -0,0 +1,17 @@
+
+import cloudscraper
+
+scraper = cloudscraper.create_scraper(delay=10)
+
+
+def account(auth):
+    acc = scraper.get(f'https://rest-bf.blox.land/user', headers={'x-auth-token': auth}).json()
+    if acc['success'] == True:
+        user = acc['user']
+        id = user['robloxId']
+        subacc = scraper.get(f'https://rest-bf.blox.land/user/lookup/{id}').json()
+        accountsuccess = {'success': True, 'acc': acc , 'subacc' : subacc}
+        return accountsuccess
+    else:
+        error = acc['error']
+        accountsuccess = {'success': False, 'error': error}
---
+++ packages/castleamongthestars/castleamongthestars-1.0.40.tar.gz/castleamongthestars-1.0.40/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="castleamongthestars",
+	author = "Xtev",
+	version='1.0.40',
+	description = "A system",
+	packages=["castleamongthestars"]
+)
