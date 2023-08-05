e5cc8f1be0a6bde74ba81bc58d051e632743798b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package atc-dataplatform/1.1.3/atc-dataplatform-1.1.3.tar.gz

---
+++ packages/atc-dataplatform/atc-dataplatform-1.1.3.tar.gz/atc-dataplatform-1.1.3/src/atc/atc_exceptions.py
@@ -0,0 +1,16 @@
+class AtcException(Exception):
+    pass
+
+
+class NoTableException(AtcException):
+    value = "No table found!"
+    pass
+
+
+class UnkownPathException(AtcException):
+    value = "Something went wrong during reading of path!"
+    pass
+
+
+class ColumnDoesNotExistException(AtcException):
+    pass
