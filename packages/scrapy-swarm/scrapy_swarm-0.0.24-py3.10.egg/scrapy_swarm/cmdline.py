8c49b630ac6775531c4fa8fed069a66473e3196a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package scrapy-swarm/0.0.24/scrapy_swarm-0.0.24-py3.10.egg

---
+++ packages/scrapy-swarm/scrapy_swarm-0.0.24-py3.10.egg/scrapy_swarm/cmdline.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from scrapy.utils.project import get_project_settings
+
+
+def execute(argv=None, settings=None):
+    if argv is None:
+        argv = sys.argv
+
+    if settings is None:
+        settings = get_project_settings()
+        # set EDITOR from environment if available
+        try:
+            editor = os.environ['EDITOR']
+        except KeyError:
+            pass
+        else:
+            settings['EDITOR'] = editor
+    print("======haha===========")
