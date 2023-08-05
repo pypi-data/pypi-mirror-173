2f2c39142d0e5dac99f6019d30630a82ecb70298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package ElMD/0.5.11/ElMD-0.5.11.tar.gz

---
+++ packages/ElMD/ElMD-0.5.11.tar.gz/ElMD-0.5.11/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# UPGRADE VERSION NUMBER IN version, download_url, ElMD.py, and __init__.py
+
+setup(
+  name = 'ElMD',        
+  packages = ['ElMD'],  
+  version = '0.5.11',
+  license='GPL3',       
+  description = 'An implementation of the Element movers distance for chemical similarity of ionic compositions',  
+  author = 'Cameron Hagreaves',            
+  author_email = 'cameron.h@rgreaves.me.uk', 
+  url = 'https://github.com/lrcfmd/ElMD/',   
+  download_url = 'https://github.com/lrcfmd/ElMD/archive/v0.5.11.tar.gz',    
+  keywords = ['ChemInformatics', 'Materials Science', 'Machine Learning', 'Materials Representation'],  
+  package_data={"elementFeatures": ["el_lookup/*.json"]}, 
+  include_package_data=True,
+  install_requires=[ 
+          'setuptools',
+          'numba',
+          'numpy',
+          'scipy',
+          'flit',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',  
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3) ',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
