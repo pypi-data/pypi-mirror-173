2e7c047cff3efe79ec368749c1dd6c09337e13d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:18 +0000

    Add package flet-like-streamlit/0.0.8/flet_like_streamlit-0.0.8.tar.gz

---
+++ packages/flet-like-streamlit/flet_like_streamlit-0.0.8.tar.gz/flet_like_streamlit-0.0.8/flet_like_streamlit/__init__.py
@@ -0,0 +1,4 @@
+from curses.ascii import ETB
+from flet_like_streamlit.main import *
+from flet_like_streamlit.control import *
+name = "flet_like_streamlit"
---
+++ packages/flet-like-streamlit/flet_like_streamlit-0.0.8.tar.gz/flet_like_streamlit-0.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flet_like_streamlit",
+    version="0.0.8",
+    author="Omochi",
+    author_email="soffionam@yahoo.co.jp",
+    description="Flet working like streamlit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Shogo0607/flet-like-streamlit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires= ["flet","numpy","pandas"],
+    entry_points = {
+        "console_scripts": ["flet_like_streamlit = flet_like_streamlit.main:main"]
+    },
+)
