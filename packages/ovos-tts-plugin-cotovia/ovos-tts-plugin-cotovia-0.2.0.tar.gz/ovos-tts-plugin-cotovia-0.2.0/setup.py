c17fe557c9af4dc1c2981ee54f92b048f93679cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:07 +0000

    Add package ovos-tts-plugin-cotovia/0.2.0/ovos-tts-plugin-cotovia-0.2.0.tar.gz

---
+++ packages/ovos-tts-plugin-cotovia/ovos-tts-plugin-cotovia-0.2.0.tar.gz/ovos-tts-plugin-cotovia-0.2.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-cotovia = ' \
+                     'ovos_tts_plugin_cotovia:CotoviaTTSPlugin'
+CONFIG_ENTRY_POINT = 'ovos-tts-plugin-cotovia.config = ovos_tts_plugin_cotovia:CotoviaTTSPluginConfig'
+
+
+setup(
+    name='ovos-tts-plugin-cotovia',
+    version='0.2.0',
+    description='A galician/spanish tts plugin for mycroft',
+    url='https://github.com/OpenVoiceOS/ovos-tts-plugin-cotovia',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_tts_plugin_cotovia'],
+    install_requires=["ovos-plugin-manager>=0.0.1a12"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='mycroft ovos plugin tts',
+    entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT,
+                  'mycroft.plugin.tts.config': CONFIG_ENTRY_POINT}
+)
