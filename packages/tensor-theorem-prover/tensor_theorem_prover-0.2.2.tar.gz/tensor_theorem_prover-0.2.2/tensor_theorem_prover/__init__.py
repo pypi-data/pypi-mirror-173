2c6f3336be9a0ea9006f626293ab700e7d0c081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package tensor-theorem-prover/0.2.2/tensor_theorem_prover-0.2.2.tar.gz

---
+++ packages/tensor-theorem-prover/tensor_theorem_prover-0.2.2.tar.gz/tensor_theorem_prover-0.2.2/tensor_theorem_prover/__init__.py
@@ -0,0 +1,38 @@
+__version__ = "0.2.2"
+
+from .prover.ResolutionProver import ResolutionProver
+
+from .types import (
+    Atom,
+    Constant,
+    Predicate,
+    Variable,
+    And,
+    Or,
+    Not,
+    Implies,
+    Exists,
+    All,
+    Function,
+    Clause,
+)
+
+from .similarity import cosine_similarity, symbol_compare
+
+__all__ = (
+    "ResolutionProver",
+    "Atom",
+    "Constant",
+    "Predicate",
+    "Variable",
+    "And",
+    "Or",
+    "Not",
+    "Implies",
+    "Exists",
+    "All",
+    "Clause",
+    "Function",
+    "cosine_similarity",
+    "symbol_compare",
+)
