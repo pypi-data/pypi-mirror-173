a77bb6cce676448466515fd1f2ca684707a1f5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package py-book-util/0.1.7/py_book_util-0.1.7.tar.gz

---
+++ packages/py-book-util/py_book_util-0.1.7.tar.gz/py_book_util-0.1.7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['py_book_util']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'py-book-util',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': '',
+    'author': '"Richard.Tang"',
+    'author_email': 'tang_can@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
