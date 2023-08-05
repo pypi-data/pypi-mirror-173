008309d8404d6f4cecc7eb02dde756af45c2235f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package cniptest/2.0.0/cniptest-2.0.0.tar.gz

---
+++ packages/cniptest/cniptest-2.0.0.tar.gz/cniptest-2.0.0/cniptest/__init__.py
@@ -0,0 +1,10 @@
+import requests
+def ip():
+    ip = requests.get("https://4.ipw.cn").text
+    return ip
+def ipv6():
+    ip = requests.get("https://6.ipw.cn").text
+    return ip
+def test():
+    ip = requests.get("https://test.ipw.cn").text
+    return ip
