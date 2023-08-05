33c9831ee4a1b9c0b971276812bd8c8baa291ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:17 +0000

    Add package pypim/0.0.1/pypim-0.0.1-py3-none-any.whl

---
+++ packages/pypim/pypim-0.0.1-py3-none-any.whl/mad-math/maths.py
@@ -0,0 +1,21 @@
+def i(e):
+    if e == 2+2:
+        print(5)
+    if e == 1+1:
+        print(11)
+    if e == 3+3:
+        print(33)
+    if e == 4+4:
+        print(44)
+    if e == 5+5:
+        print(55)
+    if e == 6+6:
+        print(66)
+    if e == 7+7:
+        print(8)
+    if e == 8+8:
+        print(88)
+    if e == 9+9:
+        print(99)
+    if e == 10+10:
+        print(1010)
