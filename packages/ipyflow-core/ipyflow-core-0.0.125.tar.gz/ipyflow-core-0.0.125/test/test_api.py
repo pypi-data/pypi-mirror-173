8889dec9cebc9b3d5b0f3cd986804b84f03bc8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package ipyflow-core/0.0.125/ipyflow-core-0.0.125.tar.gz

---
+++ packages/ipyflow-core/ipyflow-core-0.0.125.tar.gz/ipyflow-core-0.0.125/test/test_api.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import logging
+from test.utils import make_flow_fixture
+
+from ipyflow.api import lift
+
+logging.basicConfig(level=logging.ERROR)
+
+# Reset dependency graph before each test
+# _flow_fixture, run_cell_ = make_flow_fixture(trace_messages_enabled=True)
+_flow_fixture, run_cell_ = make_flow_fixture(
+    setup_stmts=[
+        "from ipyflow.api import code, deps, lift, rdeps, rusers, timestamp, users",
+        "import pyccolo as pyc",
+    ]
+)
+run_cell = run_cell_
+
+
+def test_lookup_symbol_simple():
+    run_cell("x = y = 42")
+    run_cell("assert lift(x).readable_name == 'x'")
+    run_cell("assert lift(y).readable_name == 'y'")
+
+
+def test_code_simple():
+    run_cell("x = 0")
+    run_cell("y = x + 1")
+    run_cell("env = pyc.exec(lift(y).code())")
+    run_cell("assert env['x'] == 0")
+    run_cell("assert env['y'] == 1")
+    run_cell("env = pyc.exec(code(y))")
+    run_cell("assert env['x'] == 0")
+    run_cell("assert env['y'] == 1")
+
+
+def test_timestamp():
+    run_cell("x = 0")
+    run_cell("y = x + 1")
+    # start at 1 b/c of setup cell
+    run_cell("assert timestamp(x).cell_num == 1")
+    run_cell("assert timestamp(y).cell_num == 2")
+
+
+def test_deps_and_users():
+    run_cell("x = 0")
+    run_cell("y = x + 0")
+    run_cell("assert deps(x) == []")
+    run_cell("assert deps(y) == [lift(x)]")
+    run_cell("assert users(x) == [lift(y)]")
+    run_cell("assert users(y) == []")
+
+
+def test_rdeps_and_rusers():
+    run_cell("x = 0")
+    run_cell("y = x + 0")
+    run_cell("z = y + 0")
+    run_cell("assert rdeps(x) == []")
+    run_cell("assert rdeps(y) == [lift(x)]")
+    run_cell("assert set(rdeps(z)) == {lift(x), lift(y)}")
+    run_cell("assert set(rusers(x)) == {lift(y), lift(z)}")
+    run_cell("assert rusers(y) == [lift(z)]")
+    run_cell("assert rusers(z) == []")
