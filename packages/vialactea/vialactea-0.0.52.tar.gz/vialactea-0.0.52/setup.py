c1ddd34feda2ec9d85d1b353ad63ab1e6f4e6f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:03 +0000

    Add package vialactea/0.0.52/vialactea-0.0.52.tar.gz

---
+++ packages/vialactea/vialactea-0.0.52.tar.gz/vialactea-0.0.52/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+
+long_description = (this_dir / 'README.md').read_text()
+
+setup(
+    version='0.0.52',
+    name='vialactea',
+    packages=['vialactea'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
+
