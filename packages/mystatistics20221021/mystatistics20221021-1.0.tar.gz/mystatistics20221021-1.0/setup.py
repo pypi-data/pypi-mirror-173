27e5961954fbc4b4b3d08e41b56a2a0863f3f863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:16 +0000

    Add package mystatistics20221021/1.0/mystatistics20221021-1.0.tar.gz

---
+++ packages/mystatistics20221021/mystatistics20221021-1.0.tar.gz/mystatistics20221021-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mystatistics20221021',
+      version='1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['mystatistics20221021'],
+      author_email='billyli1991@gmail.com',
+      zip_safe=False)
