63f3eeadcbc3bf5e8b1f687f783cd5b4f641fa47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:24 +0000

    Add package fastapi-helper/0.0.8/fastapi_helper-0.0.8-py3-none-any.whl

---
+++ packages/fastapi-helper/fastapi_helper-0.0.8-py3-none-any.whl/fastapi_helper/exceptions/validation_exceptions.py
@@ -0,0 +1,24 @@
+from fastapi.exceptions import RequestValidationError
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+
+def base_validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = []
+    for count, error in enumerate(exc.errors()):
+        loc, message, types = error["loc"], error["msg"], error['type']
+        try:
+            field = error["loc"][1]
+        except IndexError:
+            field = None
+        errors.append({
+            "code": "validation-error",
+            "type": types,
+            "message": message,
+            "field": None if types == 'value_error.jsondecode' else field
+        })
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=errors
+    )
