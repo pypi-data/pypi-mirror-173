828cbd403bffc174b13343940c9518418ff42758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:00 +0000

    Add package robyn/0.18.1/robyn-0.18.1.tar.gz

---
+++ packages/robyn/robyn-0.18.1.tar.gz/robyn-0.18.1/src/shared_socket.rs
@@ -0,0 +1,53 @@
+use pyo3::prelude::*;
+
+use log::debug;
+use socket2::{Domain, Protocol, Socket, Type};
+use std::net::SocketAddr;
+
+#[pyclass]
+#[derive(Debug)]
+pub struct SocketHeld {
+    pub socket: Socket,
+}
+
+#[pymethods]
+impl SocketHeld {
+    #[new]
+    #[cfg(not(target_os = "windows"))]
+    pub fn new(address: String, port: i32) -> PyResult<SocketHeld> {
+        let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?;
+        let address: SocketAddr = format!("{}:{}", address, port).parse()?;
+        debug!("{}", address);
+        socket.set_reuse_port(true)?;
+        socket.set_reuse_address(true)?;
+        socket.bind(&address.into())?;
+        socket.listen(1024)?;
+
+        Ok(SocketHeld { socket })
+    }
+
+    #[new]
+    #[cfg(target_os = "windows")]
+    pub fn new(address: String, port: i32) -> PyResult<SocketHeld> {
+        let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?;
+        let address: SocketAddr = format!("{}:{}", address, port).parse()?;
+        debug!("{}", address);
+        // reuse port is not available on windows
+        socket.set_reuse_address(true)?;
+        socket.bind(&address.into())?;
+        socket.listen(1024)?;
+
+        Ok(SocketHeld { socket })
+    }
+
+    pub fn try_clone(&self) -> PyResult<SocketHeld> {
+        let copied = self.socket.try_clone()?;
+        Ok(SocketHeld { socket: copied })
+    }
+}
+
+impl SocketHeld {
+    pub fn get_socket(&self) -> Socket {
+        self.socket.try_clone().unwrap()
+    }
+}
