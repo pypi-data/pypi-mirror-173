69590b903ac8e0acda5e6b9ec60b859efc95c68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:41 +0000

    Add package ckanext-saml/0.1.5/ckanext_saml-0.1.5-py3-none-any.whl

---
+++ packages/ckanext-saml/ckanext_saml-0.1.5-py3-none-any.whl/ckanext/saml/tests/test_helpers.py
@@ -0,0 +1,44 @@
+import ckan.model as model
+import ckan.plugins.toolkit as tk
+import pytest
+
+import ckanext.saml.helpers as h
+from ckanext.saml.model.user import User
+
+
+@pytest.mark.usefixtures("with_plugins", "clean_db")
+def test_is_saml_user(user):
+    assert not tk.h.saml_is_saml_user(user["name"])
+
+    model.Session.add(User(id=user["id"], name_id="test"))
+    model.Session.commit()
+
+    assert tk.h.saml_is_saml_user(user["name"])
+
+
+@pytest.mark.usefixtures("with_plugins")
+def test_login_button_text(ckan_config, monkeypatch, faker):
+    assert tk.h.saml_login_button_text() == h.DEFAULT_LOGIN_TEXT
+
+    label = faker.sentence()
+    monkeypatch.setitem(ckan_config, h.CONFIG_LOGIN_TEXT, label)
+    assert tk.h.saml_login_button_text() == label
+
+
+@pytest.mark.usefixtures("with_plugins")
+def test_folder_path(ckan_config, monkeypatch, faker):
+    assert tk.h.saml_folder_path() == h.DEFAULT_FOLDER_PATH
+
+    path = faker.file_path()
+    monkeypatch.setitem(ckan_config, h.CONFIG_FOLDER_PATH, path)
+    assert tk.h.saml_folder_path() == path
+
+
+@pytest.mark.usefixtures("with_plugins")
+def test_attr_mapper(ckan_config, monkeypatch, faker):
+    assert tk.h.saml_attr_mapper() is None
+
+
+@pytest.mark.usefixtures("with_plugins")
+def test_settings(ckan_config, monkeypatch, faker):
+    assert tk.h.saml_settings() == {}
