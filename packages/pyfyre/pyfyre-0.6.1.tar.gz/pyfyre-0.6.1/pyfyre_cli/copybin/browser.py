dcb31679016be138cd98933d33fe6e305e99f1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:13 +0000

    Add package pyfyre/0.6.1/pyfyre-0.6.1.tar.gz

---
+++ packages/pyfyre/pyfyre-0.6.1.tar.gz/pyfyre-0.6.1/pyfyre_cli/copybin/browser.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+
+class EmptyObject:
+	def __getattr__(self, attr: str) -> Any:
+		return EmptyObject()
+	
+	def __call__(self, *args: Any, **kwargs: Any) -> Any:
+		return EmptyObject()
+	
+	def __getitem__(self, key: str) -> Any:
+		return EmptyObject()
+
+
+alert = EmptyObject()
+bind = EmptyObject()
+confirm = EmptyObject()
+console = EmptyObject()
+document = EmptyObject()
+DOMEvent = EmptyObject()
+DOMNode = EmptyObject()
+load = EmptyObject()
+prompt = EmptyObject()
+run_script = EmptyObject()
+window = EmptyObject()
+aio = EmptyObject()
+ajax = EmptyObject()
+html = EmptyObject()
+local_storage = EmptyObject()
+markdown = EmptyObject()
+object_storage = EmptyObject()
+session_storage = EmptyObject()
+svg = EmptyObject()
+template = EmptyObject()
+timer = EmptyObject()
+webcomponent = EmptyObject()
+websocket = EmptyObject()
+worker = EmptyObject()
+widgets = EmptyObject()
