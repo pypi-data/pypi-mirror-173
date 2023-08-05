b6ccb9f92b94ea8d9b4cd050bf3d6ab77d3eb587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:51 +0000

    Add package cluster-experiments/0.2.4/cluster_experiments-0.2.4-py2.py3-none-any.whl

---
+++ packages/cluster-experiments/cluster_experiments-0.2.4-py2.py3-none-any.whl/cluster_experiments/power_config.py
@@ -0,0 +1,117 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+from cluster_experiments.cupac import EmptyRegressor, TargetAggregation
+from cluster_experiments.experiment_analysis import (
+    GeeExperimentAnalysis,
+    OLSAnalysis,
+    ClusteredOLSAnalysis,
+    TTestClusteredAnalysis,
+)
+from cluster_experiments.perturbator import BinaryPerturbator, UniformPerturbator
+from cluster_experiments.random_splitter import (
+    BalancedClusteredSplitter,
+    ClusteredSplitter,
+    NonClusteredSplitter,
+    StratifiedClusteredSplitter,
+)
+
+
+@dataclass
+class PowerConfig:
+    """
+    Dataclass to create a power analysis from.
+
+    Arguments:
+        splitter: Splitter object to use
+        perturbator: Perturbator object to use
+        analysis: ExperimentAnalysis object to use
+        cupac_model: CUPAC model to use
+        n_simulations: number of simulations to run
+        cluster_cols: list of columns to use as clusters
+        target_col: column to use as target
+        treatment_col: column to use as treatment
+        treatment: what value of treatment_col should be considered as treatment
+        strata_cols: columns to stratify with
+        covariates: list of columns to use as covariates
+        average_effect: average effect to use in the perturbator
+        treatments: list of treatments to use
+        alpha: alpha value to use in the power analysis
+        agg_col: column to use for aggregation in the CUPAC model
+        smoothing_factor: smoothing value to use in the CUPAC model
+        features_cupac_model: list of features to use in the CUPAC model
+
+    Usage:
+
+    ```python
+    from cluster_experiments.power_config import PowerConfig
+    from cluster_experiments.power_analysis import PowerAnalysis
+
+    p = PowerConfig(
+        analysis="gee",
+        splitter="clustered_balance",
+        perturbator="uniform",
+        cluster_cols=["city"],
+        n_simulations=100,
+        alpha=0.05,
+    )
+    power_analysis = PowerAnalysis.from_config(p)
+    ```
+    """
+
+    # mappings
+    perturbator: str
+    splitter: str
+    analysis: str
+
+    # Needed
+    cluster_cols: Optional[List[str]] = None
+
+    # optional mappings
+    cupac_model: str = ""
+
+    # Shared
+    target_col: str = "target"
+    treatment_col: str = "treatment"
+    treatment: str = "B"
+
+    # Perturbator
+    average_effect: float = 0.0
+
+    # Splitter
+    treatments: Optional[List[str]] = None
+    strata_cols: Optional[List[str]] = None
+
+    # Analysis
+    covariates: Optional[List[str]] = None
+
+    # Power analysis
+    n_simulations: int = 100
+    alpha: float = 0.05
+
+    # Cupac
+    agg_col: str = ""
+    smoothing_factor: float = 20
+    features_cupac_model: Optional[List[str]] = None
+
+
+perturbator_mapping = {
+    "binary": BinaryPerturbator,
+    "uniform": UniformPerturbator,
+}
+
+splitter_mapping = {
+    "clustered": ClusteredSplitter,
+    "clustered_balance": BalancedClusteredSplitter,
+    "non_clustered": NonClusteredSplitter,
+    "clustered_stratified": StratifiedClusteredSplitter,
+}
+
+analysis_mapping = {
+    "gee": GeeExperimentAnalysis,
+    "ols_non_clustered": OLSAnalysis,
+    "ols_clustered": ClusteredOLSAnalysis,
+    "ttest_clustered": TTestClusteredAnalysis,
+}
+
+cupac_model_mapping = {"": EmptyRegressor, "mean_cupac_model": TargetAggregation}
