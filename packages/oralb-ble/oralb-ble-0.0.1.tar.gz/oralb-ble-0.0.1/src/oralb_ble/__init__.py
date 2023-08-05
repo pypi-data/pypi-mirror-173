32066b89be1a977768707b0b305b0f6c5a79cf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:01 +0000

    Add package oralb-ble/0.0.1/oralb-ble-0.0.1.tar.gz

---
+++ packages/oralb-ble/oralb-ble-0.0.1.tar.gz/oralb-ble-0.0.1/src/oralb_ble/__init__.py
@@ -0,0 +1,34 @@
+"""Parser for OralB BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/oral_b.py
+
+MIT License applies.
+"""
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import OralBBluetoothDeviceData
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "OralBBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
