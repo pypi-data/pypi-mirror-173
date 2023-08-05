fe5d203002a47ce0908cf29f934317f77328e85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:52 +0000

    Add package gpu-tracking/0.2.3/gpu_tracking-0.2.3-py3-none-win_amd64.whl

---
+++ packages/gpu-tracking/gpu_tracking-0.2.3-py3-none-win_amd64.whl/gpu_tracking/lib.py
@@ -0,0 +1,106 @@
+from .gpu_tracking import batch_rust 
+from .gpu_tracking import batch_file_rust
+from .gpu_tracking import link_rust
+import pandas as pd
+
+def batch(
+    video,
+    diameter,
+    minmass = None,
+    maxsize = None,
+    separation = None,
+    noise_size = None,
+    smoothing_size = None,
+    threshold = None,
+    invert = None,
+    percentile = None,
+    topn = None,
+    preprocess = None,
+    max_iterations = None,
+    characterize = None,
+    filter_close = None,
+    search_range = None,
+    memory = None,
+    cpu_processed = None,
+    sig_radius = None,
+    bg_radius = None,
+    gap_radius = None,
+    ):
+
+    arr, columns = batch_rust(
+        video,
+        diameter,
+        minmass,
+        maxsize,
+        separation,
+        noise_size,
+        smoothing_size,
+        threshold,
+        invert,
+        percentile,
+        topn,
+        preprocess,
+        max_iterations,
+        characterize,
+        filter_close,
+        search_range,
+        memory,
+        cpu_processed,
+        sig_radius,
+        bg_radius,
+        gap_radius,
+    )
+    columns = {name: typ for name, typ in columns}
+    return pd.DataFrame(arr, columns = columns).astype(columns)
+
+def batch_file(
+    path,
+    diameter,
+    channel = None,
+    minmass = None,
+    maxsize = None,
+    separation = None,
+    noise_size = None,
+    smoothing_size = None,
+    threshold = None,
+    invert = None,
+    percentile = None,
+    topn = None,
+    preprocess = None,
+    max_iterations = None,
+    characterize = None,
+    filter_close = None,
+    search_range = None,
+    memory = None,
+    cpu_processed = None,
+    sig_radius = None,
+    bg_radius = None,
+    gap_radius = None,
+    ):
+
+    arr, columns = batch_file_rust(
+        path,
+        diameter,
+        channel,
+        minmass,
+        maxsize,
+        separation,
+        noise_size,
+        smoothing_size,
+        threshold,
+        invert,
+        percentile,
+        topn,
+        preprocess,
+        max_iterations,
+        characterize,
+        filter_close,
+        search_range,
+        memory,
+        cpu_processed,
+        sig_radius,
+        bg_radius,
+        gap_radius,
+    )
+    columns = {name: typ for name, typ in columns}
+    return pd.DataFrame(arr, columns = columns).astype(columns)
