8b4896354f84641e8c7ed4ba2fcd9618ba47f6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:24 +0000

    Add package drf-operation-log/0.5.1/drf_operation_log-0.5.1-py3-none-any.whl

---
+++ packages/drf-operation-log/drf_operation_log-0.5.1-py3-none-any.whl/drf_operation_log/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from .models import OperationLogEntry
+
+
+class OperationLogEntrySerializer(serializers.ModelSerializer):
+    operator = serializers.CharField(source="user.username", default="", label="操作人")
+    change_message = serializers.ListField(
+        source="get_change_message", child=serializers.CharField(), label="操作内容"
+    )
+    content_type_name = serializers.CharField(source="content_type.name", label="对象名称")
+    domain_content_type_name = serializers.CharField(
+        source="domain_content_type.name", default="", label="域对象名称"
+    )
+    change_message_raw = serializers.JSONField(source="change_message")
+
+    class Meta:
+        model = OperationLogEntry
+        fields = "__all__"
