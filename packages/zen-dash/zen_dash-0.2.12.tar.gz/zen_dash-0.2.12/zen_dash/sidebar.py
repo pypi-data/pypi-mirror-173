fc57c3eacd2601149ef349b35e0fb2a57eccccce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package zen-dash/0.2.12/zen_dash-0.2.12.tar.gz

---
+++ packages/zen-dash/zen_dash-0.2.12.tar.gz/zen_dash-0.2.12/zen_dash/sidebar.py
@@ -0,0 +1,17 @@
+from typing import Optional, List
+
+from zen_dash.support import BaseUpdate
+
+
+class SidebarTab(BaseUpdate):
+    label: str
+    icon: Optional[str] = 'Home'
+    fragment: str
+
+class FilterInfo(BaseUpdate):
+    url: str
+    
+class Sidebar(BaseUpdate):
+    tabs: List[SidebarTab]
+    filters: List[FilterInfo]
+    size: str = '300px'
