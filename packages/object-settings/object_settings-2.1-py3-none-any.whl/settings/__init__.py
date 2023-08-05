0cf1c1c2ea4dba95d51a2602ca33c0ceae13b715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:11 +0000

    Add package object-settings/2.1/object_settings-2.1-py3-none-any.whl

---
+++ packages/object-settings/object_settings-2.1-py3-none-any.whl/settings/__init__.py
@@ -0,0 +1,13 @@
+"""
+Simple-to-use object-oriented config library.
+
+The values have automatic validation and get saved to a file that's
+seamlessly written and read in the background, so you don't have to worry about any of it.
+"""
+
+from . import base, dir, parser, types
+
+from .dir import setup
+from .base import Section, all_sections
+from .types import *
+
