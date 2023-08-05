314b65a03972a0a1009c5b5997fdda06e3553d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:34 +0000

    Add package ParUtils/1.0.5/ParUtils-1.0.5-py3-none-any.whl

---
+++ packages/ParUtils/ParUtils-1.0.5-py3-none-any.whl/parutils/changelog.py
@@ -0,0 +1,18 @@
+__VERSION__ = '1.0.5'
+# CWD log at init
+
+# __VERSION__ = '1.0.4'
+# # Improved ttry
+
+# __VERSION__ = '1.0.3'
+# # loging.close_logger()
+# # test -> testing
+
+# __VERSION__ = '1.0.2'
+# # Removed tests from packaging
+
+# __VERSION__ = '1.0.1'
+# # Removed extract_doc
+
+# __VERSION__ = '1.0.0'
+# # Init
---
+++ packages/ParUtils/ParUtils-1.0.5-py3-none-any.whl/parutils/logging/__init__.py
@@ -0,0 +1,15 @@
+from .main import log
+from .main import log_dict
+from .main import log_print
+from .main import log_array
+from .main import log_input
+from .main import log_example
+
+from .sl import step_log
+from .sl import init_sl_timer
+
+from . import const
+from .cl import check_log
+from .logger import Logger
+from .core import get_logger
+from .core import close_logger
