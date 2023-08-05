332bdecc0d65c843978e0847605ae350cec7e94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:17 +0000

    Add package pypim/0.0.1/pypim-0.0.1.tar.gz

---
+++ packages/pypim/pypim-0.0.1.tar.gz/pypim-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'a random thing'
+
+# Setting up
+setup(
+    name="pypim",
+    version=VERSION,
+    author="Mr Cottonball",
+    author_email="<64mrnoah@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['none'],
+    keywords=['python', 'math'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
