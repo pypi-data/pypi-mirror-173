50931f2cc9e88523eae96d51f14a07b11a12095d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:48 +0000

    Add package cooptools/0.48/cooptools-0.48.tar.gz

---
+++ packages/cooptools/cooptools-0.48.tar.gz/cooptools-0.48/cooptools/plotting.py
@@ -0,0 +1,31 @@
+from typing import List, Tuple
+
+def plot_series(points: List[Tuple[int, float]],
+                 ax,
+                 color=None,
+                 type=None,
+                 label=None,
+                 line_width: int = None,
+                 line_style: str = None,
+                 point_size = None):
+    """
+
+    :param points:
+    :param ax:
+    :param color:
+    :param type:
+    :param label:
+    :param line_width:
+    :param line_style: [‘solid’, ‘dashed’, ‘dashdot’, ‘dotted’, (offset, on-off-dash-seq), '-', '--', '-.', ':', 'None', ' ', '']
+    :return:
+    """
+
+    if points is None or len(points) == 0:
+        return
+
+    res = list(zip(*points))
+
+    if type is None:
+        ax.plot(res[0], res[1], color=color, linewidth=line_width, label=label, linestyle=line_style)
+    elif type == 'scatter':
+        ax.scatter(res[0], res[1], color=color, label=label, s=point_size)
