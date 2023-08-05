1f00e800bc18afc71bf6da5180ffc49cac326a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:23 +0000

    Add package aurora-mvc/0.8.18/aurora-mvc-0.8.18.tar.gz

---
+++ packages/aurora-mvc/aurora-mvc-0.8.18.tar.gz/aurora-mvc-0.8.18/src/aurora/__init__.py
@@ -0,0 +1,17 @@
+# Dependencies
+from .Aurora import Aurora
+from .Controller import Controller
+from .View import View as Template
+
+try:
+    from .Forms import Forms
+    from .Model import Model
+except:
+    pass
+
+View = Template().render
+
+
+# Descriptions
+__version__ = '0.8.18'
+__author__ = '<https://github.com/heminsatya>'
