1ccce4c3805bda4977b1e8e03ee643d9a8e367d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:22 +0000

    Add package elimity-insights-client/9.0.0/elimity_insights_client-9.0.0-py3-none-any.whl

---
+++ packages/elimity-insights-client/elimity_insights_client-9.0.0-py3-none-any.whl/elimity_insights_client/api/entities/__init__.py
@@ -0,0 +1,4 @@
+from elimity_insights_client.api.entities._entities import entities
+from elimity_insights_client.api.entities._entity import Entity, Link
+
+__all__ = ["Entity", "Link", "entities"]
