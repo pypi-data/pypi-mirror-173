3f04d4f0e6d1097eaec1231577d6614b8c3374b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package casarecipes/0.3.0/casarecipes-0.3.0.tar.gz

---
+++ packages/casarecipes/casarecipes-0.3.0.tar.gz/casarecipes-0.3.0/README.rst
@@ -0,0 +1,6 @@
+casarecipes - accessories for CASA
+==================================
+
+Introduction
+------------
+:code:`casarecipes` contains useful contributed additions to `CASA <https://casadocs.readthedocs.io/en/latest/>`_ .
---
+++ packages/casarecipes/casarecipes-0.3.0.tar.gz/casarecipes-0.3.0/casarecipes/cl2skycat.py
@@ -0,0 +1,56 @@
+import casatools
+
+import numpy as np
+def cl2skycat(componentlist='', skycat=''):
+    """
+    Converts a componentlist dictionary or componnent list file on disk
+    to  a skycatalog to overlay on image in the viewer
+    
+    """
+    qa=casatools.quanta()
+    cl=casatools.componentlist()
+    tb=casatools.table()
+    if(type(componentlist)==str):
+        cl.open(componentlist)
+    elif(type(componentlist)==dict):
+        cl.purge()
+        cl.fromrecord(componentlist)
+    if(cl.length()==0):
+            print("no components found")
+            return
+
+    des={}
+    des['Type']={'valueType':'string'}
+    des['Long']={'valueType':'double'}
+    des['Lat']={'valueType':'double'}
+    des['COMP_ID']={'valueType':'string'}
+    des['RA']={'valueType':'string'}
+    des['DEC']={'valueType':'string'}
+    des['FluxValue']={'valueType':'double'}
+    tb.create(tablename=skycat, tabledesc=des, nrow=cl.length())
+    eltype=[]
+    nam=[]
+    RA=[]
+    DEC=[]
+    lati=np.zeros((cl.length(),))
+    longi=np.zeros((cl.length(),))
+    fluxval=np.zeros((cl.length(),))
+    for k in range(cl.length()):
+        longi[k]=qa.convert(cl.getrefdir(k)['m0'],'deg')['value']
+        lati[k]=qa.convert(cl.getrefdir(k)['m1'],'deg')['value']
+        fluxval[k]=cl.getfluxvalue(k)[0]
+        RA.append(qa.time(cl.getrefdir(k)['m0'], prec=10))
+        DEC.append(qa.angle(cl.getrefdir(k)['m1'], prec=10))
+        eltype.append(cl.getrefdir(k)['refer'])
+        nam.append(str(k))
+    tb.putcol('Type', eltype)
+    tb.putcol('RA', RA)
+    tb.putcol('DEC', DEC)
+    tb.putcol('COMP_ID', nam)
+    tb.putcol('Long', longi)
+    tb.putcol('Lat', lati)
+    tb.putcol('FluxValue', fluxval)
+    tb.putcolkeyword(columnname='Long', keyword='UNIT', value='deg')
+    tb.putcolkeyword(columnname='Lat', keyword='UNIT', value='deg')                     
+    tb.putinfo({'type':'Skycatalog'})
+    tb.done()
