18c0d38ab3416f2c8ca95cc176a08b3670138275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package inputer/0.0.1/inputer-0.0.1.tar.gz

---
+++ packages/inputer/inputer-0.0.1.tar.gz/inputer-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+file = open("README.md")
+long_description = file.read()
+file.close()
+
+setuptools.setup(
+	name="inputer",
+	version="0.0.1",
+	author="Time-Coder",
+	author_email="binghui.wang@foxmail.com",
+	description="Print before input",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Time-Coder/inputer",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		"click"
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	],
+)
