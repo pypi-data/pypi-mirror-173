e95e245d6116292feba904e1171f9f147a46c350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:16 +0000

    Add package statsig/0.15.0/statsig-0.15.0.tar.gz

---
+++ packages/statsig/statsig-0.15.0.tar.gz/statsig-0.15.0/statsig/statsig.py
@@ -0,0 +1,74 @@
+from typing import Optional
+from statsig.statsig_event import StatsigEvent
+from statsig.statsig_user import StatsigUser
+from .statsig_server import StatsigServer
+
+__instance = StatsigServer()
+
+
+def initialize(secret_key: str, options=None):
+    __instance.initialize(secret_key, options)
+
+
+def check_gate(user: StatsigUser, gate: str):
+    return __instance.check_gate(user, gate)
+
+
+def get_config(user: StatsigUser, config: str):
+    return __instance.get_config(user, config)
+
+
+def get_experiment(user: StatsigUser, experiment: str):
+    return get_config(user, experiment)
+
+
+def get_layer(user: StatsigUser, layer: str):
+    return __instance.get_layer(user, layer)
+
+
+def log_event(event: StatsigEvent):
+    __instance.log_event(event)
+
+
+def override_gate(gate: str, value: bool, user_id: Optional[str] = None):
+    __instance.override_gate(gate, value, user_id)
+
+
+def override_config(config: str, value: object, user_id: Optional[str] = None):
+    __instance.override_config(config, value, user_id)
+
+
+def override_experiment(experiment: str, value: object, user_id: Optional[str] = None):
+    __instance.override_experiment(experiment, value, user_id)
+
+
+def remove_gate_override(gate: str, user_id: Optional[str] = None):
+    __instance.remove_gate_override(gate, user_id)
+
+
+def remove_config_override(config: str, user_id: Optional[str] = None):
+    __instance.remove_config_override(config, user_id)
+
+
+def remove_experiment_override(experiment: str, user_id: Optional[str] = None):
+    __instance.remove_experiment_override(experiment, user_id)
+
+
+def remove_all_overrides():
+    __instance.remove_all_overrides()
+
+
+def get_client_initialize_response(user: StatsigUser):
+    return __instance.get_client_initialize_response(user)
+
+
+def evaluate_all(user: StatsigUser):
+    return __instance.evaluate_all(user)
+
+
+def shutdown():
+    __instance.shutdown()
+
+
+def get_instance():
+    return __instance
---
+++ packages/statsig/statsig-0.15.0.tar.gz/statsig-0.15.0/statsig/statsig_user.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from statsig import statsig_environment_tier
+from statsig.statsig_errors import StatsigValueError
+
+
+def _str_or_none(field):
+    return str(field) if field is not None else None
+
+
+@dataclass
+class StatsigUser:
+    """An object of properties relating to the current user
+    user_id or at least a custom ID is required: learn more https://docs.statsig.com/messages/serverRequiredUserID
+    Provide as many as possible to take advantage of advanced conditions in the statsig console
+    A dictionary of additional fields can be provided under the custom field
+    Set private_attributes for any user property you need for gate evaluation but prefer stripped from logs/metrics
+    """
+    user_id: Optional[str] = None
+    email: Optional[str] = None
+    ip: Optional[str] = None
+    user_agent: Optional[str] = None
+    country: Optional[str] = None
+    locale: Optional[str] = None
+    app_version: Optional[str] = None
+    custom: Optional[dict] = None
+    private_attributes: Optional[dict] = None
+    custom_ids: Optional[dict] = None
+    _statsig_environment: Optional[dict] = None
+
+    def __post_init__(self):
+        # ensure there is a user id or at least a custom ID, empty dict evaluates to false in python so we can use "not" operator to check
+        if not self.user_id and not self.custom_ids:
+            raise StatsigValueError(
+                'user_id or at least a custom ID is required: learn more https://docs.statsig.com/messages/serverRequiredUserID')
+
+    def to_dict(self, forEvaluation=False):
+        user_nullable = {
+            'userID': _str_or_none(self.user_id),
+            'email': _str_or_none(self.email),
+            'ip': _str_or_none(self.ip),
+            'userAgent': _str_or_none(self.user_agent),
+            'country': _str_or_none(self.country),
+            'locale': _str_or_none(self.locale),
+            'appVersion': _str_or_none(self.app_version),
+            'custom': self.custom,
+            'statsigEnvironment': self._get_environment(),
+            'customIDs': self.custom_ids,
+        }
+
+        if forEvaluation and self.private_attributes is not None:
+            user_nullable["privateAttributes"] = self.private_attributes
+
+        return {k: v for k, v in user_nullable.items() if v is not None}
+
+    def _get_environment(self):
+        if self._statsig_environment is None or not isinstance(self._statsig_environment, dict) or self._statsig_environment['tier'] is None:
+            return None
+
+        tier = self._statsig_environment['tier']
+        if isinstance(tier, str):
+            return {'tier': tier}
+
+        if isinstance(tier, statsig_environment_tier.StatsigEnvironmentTier):
+            return {'tier': tier.value}
+
+        return None
