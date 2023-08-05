b06c46dfcba44687af102064515668e3193d1717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:51 +0000

    Add package eo-grow/1.3.2/eo-grow-1.3.2.tar.gz

---
+++ packages/eo-grow/eo-grow-1.3.2.tar.gz/eo-grow-1.3.2/eogrow/core/storage.py
@@ -0,0 +1,131 @@
+"""
+This module handles everything regarding storage of the data
+"""
+from io import StringIO
+from typing import Dict, List, Optional
+
+import fs
+from fs.base import FS
+from pydantic import BaseSettings, Field
+
+from eolearn.core.utils.fs import get_aws_credentials, get_filesystem, is_s3_path
+from sentinelhub import SHConfig
+
+from ..utils.types import AwsAclType
+from .base import EOGrowObject
+from .schemas import ManagerSchema
+
+
+class StorageManager(EOGrowObject):
+    PRESET_FOLDERS: Dict[str, str] = {"logs": "logs", "input_data": "input-data", "cache": "cache"}
+
+    class Schema(ManagerSchema, BaseSettings):
+        project_folder: str = Field(
+            description=(
+                "The root project folder. Can be either local or on AWS S3 Bucket."
+                "If on AWS, the path must be prefixed with s3://."
+            ),
+        )
+        aws_profile: Optional[str] = Field(
+            env="AWS_PROFILE",
+            description=(
+                "The AWS profile with credentials needed to access the S3 buckets. In case the profile isn't specified"
+                " with a parameter it can be read from an environmental variable."
+            ),
+        )
+        aws_acl: Optional[AwsAclType] = Field(
+            description=(
+                "An optional parameter to specify under what kind of access control list (ACL) objects should be saved"
+                " to an AWS S3 bucket."
+            )
+        )
+        structure: Dict[str, str] = Field(
+            default_factory=dict,
+            description="A flat key: value store mapping each key to a path in the project.",
+        )
+
+        class Config(ManagerSchema.Config):
+            case_sensitive = True
+            env_prefix = "eogrow_"
+
+    config: Schema
+
+    def __init__(self, config: Schema):
+        super().__init__(config)
+
+        for folder_key, folder_path in self.PRESET_FOLDERS.items():
+            if folder_key not in self.config.structure:
+                self.config.structure[folder_key] = folder_path
+
+        self.sh_config = self._prepare_sh_config()
+        self.filesystem = self._prepare_filesystem()
+
+    def _prepare_sh_config(self) -> SHConfig:
+        """Prepares an instance of `SHConfig` containing AWS credentials. In case given AWS profile doesn't exist it
+        will show a warning and return a config without AWS credentials."""
+        sh_config = SHConfig(hide_credentials=True)
+
+        if self.is_on_aws() and self.config.aws_profile:
+            sh_config = get_aws_credentials(aws_profile=self.config.aws_profile, config=sh_config)
+
+        return sh_config
+
+    def _prepare_filesystem(self) -> FS:
+        """Prepares the main instance of filesystem object which contains all additional configuration parameters."""
+        fs_kwargs: Dict[str, str] = {}
+        if is_s3_path(self.config.project_folder) and self.config.aws_acl:
+            fs_kwargs["acl"] = self.config.aws_acl
+
+        return get_filesystem(self.config.project_folder, create=True, config=self.sh_config, **fs_kwargs)
+
+    def get_folder(self, key: str, full_path: bool = False) -> str:
+        """Returns the path  associated with a key in the structure config."""
+        folder_path = self.config.structure[key]
+        self.filesystem.makedirs(folder_path, recreate=True)
+
+        if full_path:
+            return fs.path.combine(self.config.project_folder, folder_path)
+        return folder_path
+
+    def get_logs_folder(self, full_path: bool = False) -> str:
+        """Method for obtaining the logs folder. Will store logs to the current folder.
+        Temporary solution until the logging to AWS is handled properly
+        """
+        return self.get_folder("logs", full_path=full_path)
+
+    def get_cache_folder(self, full_path: bool = False) -> str:
+        """Returns the path associated with the cache key."""
+        return self.get_folder("cache", full_path=full_path)
+
+    def get_input_data_folder(self, full_path: bool = False) -> str:
+        """Returns the path associated with the input_data key."""
+        return self.get_folder("input_data", full_path=full_path)
+
+    def is_on_aws(self) -> bool:
+        """Returns True if the project_folder is on S3, False  otherwise."""
+        return is_s3_path(self.config.project_folder)
+
+    def show_folder_structure(
+        self, show_files: bool = False, return_str: bool = False, exclude: Optional[List[str]] = None
+    ) -> Optional[str]:
+        """Shows how folder structure looks like at the moment. It will show all folders except EOPatch folders and
+        EOExecution report folders
+
+        :param show_files: If  `True` it will show also files inside the folders. Note that the number of files may be
+            huge. By default, this is set to `False`.
+        :param return_str: If `True` it will return folder structure as a string. If `False` it will just print the
+            visualization to stdout.
+        :param exclude: A list of grep folder paths  to exclude from the structure return.
+            Defaults to ['eopatch*', 'eoexecution-report*']
+        :return: Depending on return_str it will either return a string or None
+        """
+        if exclude is None:
+            exclude = ["eopatch*", "eoexecution-report*"]
+
+        file_filter = None if show_files else [""]
+        io_object = StringIO() if return_str else None
+        self.filesystem.tree(max_levels=10, with_color=True, exclude=exclude, filter=file_filter, file=io_object)
+        if io_object:
+            io_object.seek(0)
+            return io_object.getvalue()
+        return None
---
+++ packages/eo-grow/eo-grow-1.3.2.tar.gz/eo-grow-1.3.2/eogrow/utils/meta.py
@@ -0,0 +1,98 @@
+"""
+Utilities for solving different problems in `eo-grow` package structure, which are mostly a pure Python magic.
+"""
+from __future__ import annotations
+
+import importlib
+import inspect
+from typing import TYPE_CHECKING, Any, Dict, Type
+
+if TYPE_CHECKING:
+    from ..core.pipeline import Pipeline
+    from ..core.schemas import BaseSchema
+
+_PIPELINE_PARAM_NAME = "pipeline"
+
+
+def load_pipeline_class(config: dict) -> Type[Pipeline]:
+    """Given a config object it loads the pipeline class referenced in the config"""
+    pipeline_class_name = config.get(_PIPELINE_PARAM_NAME)
+    if pipeline_class_name is None:
+        raise ValueError(f"Config file is missing '{_PIPELINE_PARAM_NAME}' parameter, don't know which pipeline to use")
+
+    pipeline_class = import_object(pipeline_class_name)
+    return pipeline_class
+
+
+def collect_schema(object_with_schema: Any) -> Type[BaseSchema]:
+    """A utility that collects a schema from the given object.
+
+    The object is expected to hold a unique internal class which inherits from `BaseSchema`. Example:
+
+    class MyObject:
+        class Schema(BaseSchema):
+            ...
+
+    This utility would provide `MySchema`. It works also if `MyObject` inherits from a class that holds the schema.
+    """
+    class_with_schema = object_with_schema if inspect.isclass(object_with_schema) else object_with_schema.__class__
+
+    try:
+        return class_with_schema.Schema
+    except AttributeError as exception:
+        raise SyntaxError(
+            f"Class {class_with_schema} is missing a schema. Each EOGrowObject class needs to contain a pydantic "
+            "model named `Schema`."
+        ) from exception
+
+
+def import_object(import_path: str) -> Any:
+    """Imports an object from a given import path"""
+    if "." not in import_path:
+        raise ValueError(f"Import path {import_path} doesn't reference an object in a module.")
+    module_name, object_name = import_path.rsplit(".", 1)
+
+    try:
+        module = importlib.import_module(module_name)
+    except ModuleNotFoundError as exception:
+        raise ModuleNotFoundError(f"{exception}. Given import path '{import_path}' is invalid.") from exception
+
+    if hasattr(module, object_name):
+        return getattr(module, object_name)
+
+    raise ImportError(
+        f"Cannot import name '{object_name}' from {module_name} ({module.__file__}). Given import path "
+        f"'{import_path}' is invalid."
+    )
+
+
+def get_os_import_path(import_path: str) -> str:
+    """For a Python import path it provides OS import path.
+
+    E.g. `eogrow.utils.meta` -> `/home/ubuntu/.../eogrow/utils/meta.py`
+    """
+    module_spec = importlib.util.find_spec(import_path)
+    if module_spec is not None and module_spec.origin is not None:
+        return module_spec.origin
+    raise ValueError(f"Given import path {import_path} not found")
+
+
+def get_package_versions() -> Dict[str, str]:
+    """A utility function that provides dependency package versions
+
+    At the moment it is and experimental utility. Everything is under try-catch in case something goes wrong
+
+    :return: A dictionary with versions
+    """
+    try:
+        import pkg_resources
+
+        dependency_packages = ["eo-grow"] + [
+            requirement.name
+            for requirement in pkg_resources.working_set.by_key["eo-grow"].requires()  # type: ignore[attr-defined]
+        ]
+
+        return {name: pkg_resources.get_distribution(name).version for name in dependency_packages}
+
+    except BaseException as ex:
+        return {"error": repr(ex)}
