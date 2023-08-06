ad775d3c0692a08ba9c28f108de8cdacd8921100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:39 +0000

    Add package jax-fdm/0.2.3/jax_fdm-0.2.3.tar.gz

---
+++ packages/jax-fdm/jax_fdm-0.2.3.tar.gz/jax_fdm-0.2.3/src/jax_fdm/equilibrium/state.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple
+
+import jax.numpy as jnp
+
+
+# ==========================================================================
+# Equilibrium state
+# ==========================================================================
+
+class EquilibriumState(NamedTuple):
+    xyz: jnp.ndarray
+    residuals: jnp.ndarray
+    lengths: jnp.ndarray
+    forces: jnp.ndarray
+    force_densities: jnp.ndarray
+    vectors: jnp.ndarray
