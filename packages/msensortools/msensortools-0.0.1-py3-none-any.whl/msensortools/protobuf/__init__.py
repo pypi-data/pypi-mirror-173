90f3bb0d82f2e00e3d1771354efb2229b04d4490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:50 +0000

    Add package msensortools/0.0.1/msensortools-0.0.1-py3-none-any.whl

---
+++ packages/msensortools/msensortools-0.0.1-py3-none-any.whl/msensortools/protobuf/__init__.py
@@ -0,0 +1,7 @@
+from .morai_actor_pb2_grpc import morai_actor_pb2_grpc
+from .morai_actor_pb2 import morai_actor_pb2
+from .morai_type_pb2 import morai_type_pb2
+from .morai_mgeo_pb2 import morai_mgeo_pb2
+from .morai_sensor_pb2 import morai_sensor_pb2
+from .morai_sim_info_pb2 import morai_sim_info_pb2
+from .morai_simulation_pb2 import morai_simulation_pb2
