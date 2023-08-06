f47f6d7e6c1b8f154ce189a7cc30023442fb6628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:30 +0000

    Add package pys5p/2.1.3/pys5p-2.1.3-py3-none-any.whl

---
+++ packages/pys5p/pys5p-2.1.3-py3-none-any.whl/pys5p/__init__.py
@@ -0,0 +1,19 @@
+#
+# This file is part of pys5p
+#
+# https://github.com/rmvanhees/pys5p.git
+#
+# Copyright (c) 2017-2021 SRON - Netherlands Institute for Space Research
+#   All Rights Reserved
+#
+# License:  BSD-3-Clause
+
+""" The pys5p package contains software to read S5p Tropomi L1B products.
+    And contains plotting routines to display your data beautifully."""
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass
