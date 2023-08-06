0538d5a2e62876753e108d2acdb77225fc7333ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:54 +0000

    Add package xpycommon/0.0.14/xpycommon-0.0.14.tar.gz

---
+++ packages/xpycommon/xpycommon-0.0.14.tar.gz/xpycommon-0.0.14/test/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python.10
+
+from xpycommon import libbluetooth, libc
+
+def main():
+    print(libbluetooth)
+    print(libbluetooth.str2ba)
+    
+    print(libc.writev)
+
+if __name__ == '__main__':
+    main()
