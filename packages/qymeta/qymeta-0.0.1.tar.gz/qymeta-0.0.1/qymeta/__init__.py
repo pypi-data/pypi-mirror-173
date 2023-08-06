60a235ac9b22c891ee04f560ef89f9356f1fb577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:55 +0000

    Add package qymeta/0.0.1/qymeta-0.0.1.tar.gz

---
+++ packages/qymeta/qymeta-0.0.1.tar.gz/qymeta-0.0.1/qymeta/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2022/10/27 17:01
+# @Author : justin.郑 3907721@qq.com
+# @File : __init__.py.py
+# @desc : 清元多链 SDK
+
+
+from qymeta.qy_nft.qy import (
+    qy_test
+)
+
