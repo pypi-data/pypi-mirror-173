8bbe81cb47d63978598756e9dd53fb3c8f8d91b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:08 +0000

    Add package aie-sdk/2.0.2/aie_sdk-2.0.2-py3-none-any.whl

---
+++ packages/aie-sdk/aie_sdk-2.0.2-py3-none-any.whl/aie/client/maps.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+from .endpoints import *
+from .client import Client
+from aie.serialize import serializer
+import aie
+
+
+class Maps(object):
+    class AddLayerType:
+        Image = "Image"
+        Feature = "Feature"
+
+    @staticmethod
+    def getMapId(obj, vis_params=None):
+        request_id = newUUID()
+
+        add_layer_type = None
+        if isinstance(obj, aie.Image):
+            add_layer_type = Maps.AddLayerType.Image
+        elif isinstance(obj, aie.ImageCollection):
+            obj = obj.mosaic()
+            add_layer_type = Maps.AddLayerType.Image
+        elif isinstance(obj, aie.Geometry):
+            add_layer_type = Maps.AddLayerType.Feature
+        elif isinstance(obj, aie.Feature):
+            add_layer_type = Maps.AddLayerType.Feature
+        elif isinstance(obj, aie.FeatureCollection):
+            add_layer_type = Maps.AddLayerType.Feature
+
+        expr = serializer.encode(obj)
+
+        options = {
+            "mapId": newUUID(),
+            "taskType": "addLayer",
+            "addLayerType": add_layer_type,
+            "visParams": vis_params
+        }
+
+        data = {"requestId": request_id,
+                "expr": expr,
+                "options": json.dumps(options)}
+
+        url = Endpoints.SDK_GATEWAY + SdkGatewayResource.Maps.GET_MAP_ID
+        return Client.post(url, data)
