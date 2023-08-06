2ab5777945a7ab9d5546ff78c1043ca65c7950b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:45 +0000

    Add package qclight/0.0.2/qclight-0.0.2-py3-none-any.whl

---
+++ packages/qclight/qclight-0.0.2-py3-none-any.whl/qclight/circuit/bell.py
@@ -0,0 +1,31 @@
+"""BellCircuit class."""
+from .circuit import QCLCircuit
+
+
+class BellCircuit(QCLCircuit):
+    """Bell states are the four states that can be created when two qubits are maximally entangled.
+    In this state, the behavior of one qubit affects the other one, and vice versa.
+
+    .. math::
+
+        \\begin{align}
+        \\ket{\\Phi^+} = \\frac{\\ket{00} + \\ket{11}}{\\sqrt{2}} \\\\
+        \\ket{\\Phi^-} = \\frac{\\ket{00} - \\ket{11}}{\\sqrt{2}} \\\\
+        \\ket{\\Psi^+} = \\frac{\\ket{01} + \\ket{10}}{\\sqrt{2}} \\\\
+        \\ket{\\Psi^-} = \\frac{\\ket{01} - \\ket{10}}{\\sqrt{2}}
+        \\end{align}
+    """
+
+    def correlate(self, q1: "int", q2: "int") -> "None":
+        """Correlates the two qubits provided.
+        The bell state is created by
+
+        - applying a :class:`~qclight.gate.h_gate.HGate` gate to the first qubit
+        - applying a :attr:`~qclight.circuit.circuit.QCLCircuit.cx` gate between the two qubits
+
+        Args:
+            q1: first qubit to correlate
+            q2: second qubit to correlate
+        """
+        self.h(q1)
+        self.cx(q1, q2)
