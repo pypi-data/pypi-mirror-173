8709180dc67a019b352f3a1732d218380fcac98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:49 +0000

    Add package aws-pcluster-slurm-spawner/3.2.6/aws_pcluster_slurm_spawner-3.2.6.tar.gz

---
+++ packages/aws-pcluster-slurm-spawner/aws_pcluster_slurm_spawner-3.2.6.tar.gz/aws_pcluster_slurm_spawner-3.2.6/aws_pcluster_slurm_spawner/__init__.py
@@ -0,0 +1,17 @@
+"""Top-level package for AwsPClusterSlurmSpawner."""
+
+__author__ = """ Jillian Rowe"""
+__email__ = "jillian@dabbleofdevops.com"
+__version__ = "0.1.0"
+
+import os
+
+from aws_pcluster_slurm_spawner.aws_pcluster_slurm_spawner import (
+    PClusterSlurmSpawner,
+)
+
+pcluster_spawner_template_paths = os.path.join(os.path.dirname(__file__), "templates")
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
