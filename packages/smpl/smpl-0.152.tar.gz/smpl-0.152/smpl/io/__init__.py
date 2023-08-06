4e7b58eeecb36c5f84a06ba16326fd31038df33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:35 +0000

    Add package smpl/0.152/smpl-0.152.tar.gz

---
+++ packages/smpl/smpl-0.152.tar.gz/smpl-0.152/smpl/io/__init__.py
@@ -0,0 +1,5 @@
+"""Simplified input and output."""
+
+from .io import pwd, mkdirs, pr, files, find_file, gf, write, read,append
+
+__all__ = ['pwd', 'mkdirs', 'pr', 'files', 'find_file', 'gf' , 'write','read','append']
---
+++ packages/smpl/smpl-0.152.tar.gz/smpl-0.152/smpl/wrap/__init__.py
@@ -0,0 +1,5 @@
+"""Simplified wrapping."""
+
+from .wrap import get_varnames, get_lambda, get_latex, get_lambda_argd
+
+__all__ = ['get_varnames', 'get_lambda', 'get_latex', 'get_lambda_argd']
