08454aee977e3d06e0eba5f1e44909d5b2d4ecf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package nester_xzx521/1.0.0/nester_xzx521-1.0.0.tar.gz

---
+++ packages/nester_xzx521/nester_xzx521-1.0.0.tar.gz/nester_xzx521-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester_xzx521',
+        version      = '1.0.0',
+        py_modules   = ['nester_xzx521'],
+        author       = 'Robert',
+        author_email = '',
+        url          = '',
+        description  = 'A simple printer of nested lists',
+     )
