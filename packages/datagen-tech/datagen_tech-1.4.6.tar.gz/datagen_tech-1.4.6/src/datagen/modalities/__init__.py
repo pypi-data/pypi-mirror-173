ea42de4b6e9dbdfe7fa901085568528b40cf1f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:32 +0000

    Add package datagen-tech/1.4.6/datagen_tech-1.4.6.tar.gz

---
+++ packages/datagen-tech/datagen_tech-1.4.6.tar.gz/datagen_tech-1.4.6/src/datagen/modalities/__init__.py
@@ -0,0 +1,9 @@
+from datagen.modalities.descriptors import (
+    VisualModalityDescriptor,
+    VisualModality,
+    TextualModalityDescriptor,
+    TextualModality,
+)
+
+visual_modality = VisualModalityDescriptor
+textual_modality = TextualModalityDescriptor
