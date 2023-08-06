120fe6f5a1b6e2596211d4ebca0336dfac36131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:36 +0000

    Add package binary-formatter-468/0.0.2/binary_formatter_468-0.0.2.tar.gz

---
+++ packages/binary-formatter-468/binary_formatter_468-0.0.2.tar.gz/binary_formatter_468-0.0.2/binary_formatter_468/binary_formatter_468.py
@@ -0,0 +1,9 @@
+def binify(num_list, length=None):
+    if length is None:
+        length = len(bin(max(num_list))) - 2
+    result = []
+    for num in list(map(bin, num_list)):
+        leading_zeros = "0" * (length + 2 - len(num))
+        num = leading_zeros + num[2:]
+        result.append(num)
+    return "\n".join(result)
