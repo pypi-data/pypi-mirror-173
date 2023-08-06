12d44a9e67ca325ab0e6643786dbbafa61f4022d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package eventsinksdkapi-swagger-client/1.0.3/eventsinksdkapi-swagger-client-1.0.3.tar.gz

---
+++ packages/eventsinksdkapi-swagger-client/eventsinksdkapi-swagger-client-1.0.3.tar.gz/eventsinksdkapi-swagger-client-1.0.3/swagger_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.basic_error_controller_api import BasicErrorControllerApi
+from swagger_client.api.event_sink_controller_api import EventSinkControllerApi
+from swagger_client.api.health_controller_api import HealthControllerApi
