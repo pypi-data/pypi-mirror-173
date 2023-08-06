71ccca5def173f7983b921dc986b71f4084d26f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:18 +0000

    Add package collective.wfadaptations/1.2/collective.wfadaptations-1.2.tar.gz

---
+++ packages/collective.wfadaptations/collective.wfadaptations-1.2.tar.gz/collective.wfadaptations-1.2/CHANGES.rst
@@ -0,0 +1,37 @@
+Changelog
+=========
+
+
+1.2 (2022-10-28)
+----------------
+
+- Added `name` parameter on `apply_from_registry` to apply given adaptation.
+  [sgeulette]
+
+1.1 (2021-04-20)
+----------------
+
+- Added reapply adaptation parameter to differentiate a reapply after a workflow reset.
+  [sgeulette]
+- Kept keys as unicode in `get_applied_adaptations`
+  [sgeulette]
+
+1.0 (2020-10-07)
+----------------
+
+- Added multiplicity adaptation parameter to authorize an adaptation to be applied multiple times.
+  [sgeulette]
+
+0.2 (2019-09-20)
+----------------
+
+- Set default value to None.
+  [sgeulette]
+
+0.1 (2017-05-31)
+----------------
+
+- Add base class for workflow adaptation.
+  [sgeulette]
+- Initial release.
+  [cedricmessiant]
---
+++ packages/collective.wfadaptations/collective.wfadaptations-1.2.tar.gz/collective.wfadaptations-1.2/src/collective/wfadaptations/tests/base.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from zope import schema
+from zope.interface import Interface
+from zope.interface import implements
+
+from collective.wfadaptations.interfaces import IWorkflowAdaptation
+
+
+class IDummySchema(Interface):
+
+    param = schema.TextLine(
+        title=u"Dummy parameter",
+        required=True)
+
+
+class DummyWorkflowAdaptation(object):
+
+    implements(IWorkflowAdaptation)
+
+    schema = IDummySchema
+    multiplicity = False
+    reapply = False
+
+    def patch_workflow(self, workflow_name, **parameters):
+        self.patched = "{};{};{}".format(workflow_name, parameters['param'], self.reapply)
+        return True, ''
+
+
+class AnotherWorkflowAdaptation(object):
+
+    implements(IWorkflowAdaptation)
+
+    schema = None
+
+    def patch_workflow(self, workflow_name, **parameters):
+        return True, ''
