85fbd6c06f51cad6f0d6f4de75be4ef97bf3aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:48 +0000

    Add package TempConverter/1.0/TempConverter-1.0-py3-none-any.whl

---
+++ packages/TempConverter/TempConverter-1.0-py3-none-any.whl/converter/converts.py
@@ -0,0 +1,31 @@
+### https://realpython.com/pypi-publish-python-package/
+## if you need a package that should be executable like "python converter" then there should be a __main__.py file. read the above article.
+
+def c2f(n):
+    """
+    celsius to fahrenheit
+    """
+    try:
+        f = (n * 1.8) + 32
+        return f
+    except TypeError:
+        n = "Enter a number"
+        return n
+
+def f2c(n):
+    """
+    fahrenheit to celsius
+    """
+    try:
+        c = (n - 32) / 1.8
+        return c
+    except TypeError:
+        n = "Enter a number"
+        return n
+
+def add_temp(n):
+    a = f2c(n)
+    b = c2f(n)
+    r = a+b
+    return r
+
