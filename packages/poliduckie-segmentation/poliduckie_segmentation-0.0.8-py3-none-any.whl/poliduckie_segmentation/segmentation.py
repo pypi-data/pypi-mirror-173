1659f8069ac9aa6eb35bce09051a69b54665e42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:27 +0000

    Add package poliduckie-segmentation/0.0.8/poliduckie_segmentation-0.0.8-py3-none-any.whl

---
+++ packages/poliduckie-segmentation/poliduckie_segmentation-0.0.8-py3-none-any.whl/poliduckie_segmentation/segmentation.py
@@ -0,0 +1,31 @@
+import os
+
+import tensorflow as tf
+# try:
+#     import importlib.resources as pkg_resources
+# except ImportError:
+#     import importlib_resources as pkg_resources
+
+IMG_WIDTH = 320
+IMG_HEIGHT = 240
+IMG_CHANNELS = 3
+
+class Segmentation:
+    def __init__(self):
+        # can be done better with importlib.resources
+        pkg_path = os.path.abspath(__file__)
+        model_path = os.path.join(os.path.dirname(pkg_path), 'models', 'multiclass_segmentation_model')
+        self.model = tf.keras.models.load_model(model_path)
+        self.model.summary()
+
+    def predict(self, image):
+        return self.model.predict(image.reshape((1,IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)))
+
+    def predict_batch(self, images):
+        return self.model.predict(images.reshape((1,IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)))
+
+    def get_model(self):
+        return self.model
+
+    def get_model_summary(self):
+        return self.model.summary()
