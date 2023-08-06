02137a958198748b21b130c51585d99d32f9d679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:27 +0000

    Add package ob-dj-store/0.0.10.7/ob-dj-store-0.0.10.7.tar.gz

---
+++ packages/ob-dj-store/ob-dj-store-0.0.10.7.tar.gz/ob-dj-store-0.0.10.7/ob_dj_store/core/stores/receivers.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from ob_dj_store.core.stores.models import Cart, Order, OrderHistory, Wallet
+
+
+@receiver(
+    post_save,
+    sender=settings.AUTH_USER_MODEL,
+    dispatch_uid="create_customer_cart_and_wallet_handler",
+)
+def create_customer_cart_and_wallet_handler(sender, instance, created, **kwargs):
+    if not created:
+        return
+    cart = Cart(customer=instance)
+    cart.save()
+    Wallet.objects.create(user=instance)
+
+
+# add receiver to ProductVariant to create inventory
+
+
+@receiver(
+    post_save,
+    sender=Order,
+    dispatch_uid="create_order_history_handler",
+)
+def create_order_history_handler(sender, instance, created, **kwargs):
+    OrderHistory.objects.create(
+        order=instance,
+        status=instance.status,
+    )
