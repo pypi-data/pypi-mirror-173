8a466d24fc404e895b4a2d9a9fe1d53864eadb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package hackebds/0.1.5/hackebds-0.1.5.tar.gz

---
+++ packages/hackebds/hackebds-0.1.5.tar.gz/hackebds-0.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(name='hackebds',
+      version='0.1.5',
+      description='This tool is used for backdoor and shellcode generation for various architecture devices',
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url='https://github.com/doudoudedi/hackEmbedded',
+      author='doudoudedi',
+      author_email='doudoudedi233@gmail.com',
+      license='MIT',
+      py_modules=['hackebds.arm','hackebds.mips',"hackebds.aarch64","hackebds.extract_shellcode"],
+      data_files=["README.md"]
+)
+
