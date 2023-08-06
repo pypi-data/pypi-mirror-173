d5512a58bb6d46168b5b30d45803da445789a758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:48 +0000

    Add package pyinaturalist-convert/0.6.0.dev0/pyinaturalist_convert-0.6.0.dev0-py3-none-any.whl

---
+++ packages/pyinaturalist-convert/pyinaturalist_convert-0.6.0.dev0-py3-none-any.whl/pyinaturalist_convert/db.py
@@ -0,0 +1,188 @@
+"""ORM data models, which can be used to create and access tables in
+`any database supported by SQLAlchemy <https://docs.sqlalchemy.org/en/14/dialects/>`_.
+These contain a relevant subset of columns common to most iNat data sources,
+suitable for combining data from API results, CSV export, DwC-A, and/or inaturalist-open-data.
+
+Requirements for a relational database are highly variable, so this won't suit all use cases, but
+at least provides a starting point.
+
+**Extra dependencies**: ``sqlalchemy``
+
+**Example**::
+
+    >>> from pyinaturalist import iNatClient
+    >>> from pyinaturalist_convert import create_tables, read_observations, save_observations
+
+    >>> # Fetch all of your own observations
+    >>> client = iNatClient()
+    >>> observations = client.observations.search(user_id='my_username').all()
+
+    >>> # Save to a SQLite database
+    >>> create_tables('observations.db')
+    >>> save_observations(observations, 'observations.db')
+
+    >>> # Read them back from the database
+    >>> for observation in get_db_observations('observations.db'):
+    ...    print(observation)
+
+**Main functions:**
+
+.. autosummary::
+    :nosignatures:
+
+    create_tables
+    get_db_observations
+    get_db_taxa
+    save_observations
+    save_taxa
+
+**Models:**
+
+.. currentmodule:: pyinaturalist_convert._models
+
+.. autosummary::
+    :nosignatures:
+
+    DbObservation
+    DbPhoto
+    DbTaxon
+    DbUser
+"""
+# TODO: Abstraction for converting between DB models and attrs models
+# TODO: Annotations and observation field values
+# TODO: If needed, this could be done with just the stdlib sqlite3 and no SQLAlchemy
+from itertools import chain
+from logging import getLogger
+from typing import TYPE_CHECKING, Iterable, Iterator, List
+
+from pyinaturalist import Observation, Taxon
+
+from .constants import DB_PATH, PathOrStr
+
+# If SQLAlchemy isn't installed, don't raise ImportErrors at import time.
+# DB Model classes require SA imports in module scope, so they're wrapped here.
+try:
+    from ._models import Base, DbObservation, DbPhoto, DbTaxon, DbUser
+except ImportError:
+    pass
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+logger = getLogger(__name__)
+
+
+def create_table(model, db_path: PathOrStr = DB_PATH):
+    """Create a single table for the specified model, if it doesn't already exist"""
+    from sqlalchemy import inspect
+
+    engine = _get_engine(db_path)
+    table = model.__tablename__
+    if inspect(engine).has_table(table):
+        logger.info(f'Table {table} already exists')
+    else:
+        model.__table__.create(engine)
+        logger.info(f'Table {table} created')
+
+
+def create_tables(db_path: PathOrStr = DB_PATH):
+    """Create all tables in a SQLite database"""
+    engine = _get_engine(db_path)
+    Base.metadata.create_all(engine)
+
+
+def get_session(db_path: PathOrStr = DB_PATH) -> 'Session':
+    """Get a SQLAlchemy session for a SQLite database"""
+    from sqlalchemy.orm import Session
+
+    return Session(_get_engine(db_path), future=True)
+
+
+def _get_engine(db_path):
+    from sqlalchemy import create_engine
+
+    return create_engine(f'sqlite:///{db_path}')
+
+
+def get_db_observations(
+    db_path: PathOrStr = DB_PATH,
+    ids: List[int] = None,
+    limit: int = 200,
+) -> Iterator[Observation]:
+    """Load observation records (and associated taxa and photos) from SQLite"""
+    from sqlalchemy import select
+
+    stmt = (
+        select(DbObservation)
+        .join(DbObservation.photos, isouter=True)  # type: ignore  # created at runtime
+        .join(DbObservation.taxon, isouter=True)
+        .join(DbObservation.user, isouter=True)
+    )
+    if ids:
+        stmt = stmt.where(DbObservation.id.in_(ids))  # type: ignore
+    if limit:
+        stmt = stmt.limit(limit)
+
+    with get_session(db_path) as session:
+        for obs in session.execute(stmt):
+            yield obs[0].to_model()
+
+
+def get_db_taxa(
+    db_path: PathOrStr = DB_PATH,
+    ids: List[int] = None,
+    accept_partial: bool = True,
+    limit: int = 200,
+) -> Iterator[Taxon]:
+    """Load taxon records from SQLite"""
+    from sqlalchemy import select
+
+    stmt = select(DbTaxon)
+    if ids:
+        stmt = stmt.where(DbTaxon.id.in_(ids))  # type: ignore
+    if not accept_partial:
+        stmt = stmt.where(DbTaxon.partial == False)
+    if limit:
+        stmt = stmt.limit(limit)
+
+    with get_session(db_path) as session:
+        for taxon in session.execute(stmt):
+            yield taxon[0].to_model()
+
+
+def save_observations(observations: Iterable[Observation], db_path: PathOrStr = DB_PATH):
+    """Save Observation objects (and associated taxa and photos) to SQLite"""
+    with get_session(db_path) as session:
+        for observation in observations:
+            session.merge(DbObservation.from_model(observation))
+            session.merge(DbTaxon.from_model(observation.taxon))
+            session.merge(DbUser.from_model(observation.user))
+            for photo in observation.photos:
+                session.merge(
+                    DbPhoto.from_model(
+                        photo, observation_id=observation.id, user_id=observation.user.id
+                    )
+                )
+
+        session.commit()
+
+
+def save_taxa(taxa: Iterable[Taxon], db_path: PathOrStr = DB_PATH):
+    """Save Taxon objects (plus ancestors and children, if available) to SQLite"""
+    from sqlalchemy import select
+
+    with get_session(db_path) as session:
+        for taxon in taxa:
+            session.merge(DbTaxon.from_model(taxon))
+
+        # Save ancestors and children (partial records), but don't overwrite any full records
+        taxonomy = {t.id: t for t in chain.from_iterable([t.ancestors + t.children for t in taxa])}
+        unique_taxon_ids = list(taxonomy.keys())
+        stmt = select(DbTaxon).where(DbTaxon.id.in_(unique_taxon_ids))  # type: ignore
+        stmt = stmt.where(DbTaxon.partial == False)
+        saved_ids = [t[0].id for t in session.execute(stmt)]
+        for taxon in taxonomy.values():
+            if taxon.id not in saved_ids:
+                session.merge(DbTaxon.from_model(taxon))
+
+        session.commit()
---
+++ packages/pyinaturalist-convert/pyinaturalist_convert-0.6.0.dev0-py3-none-any.whl/pyinaturalist_convert/fts.py
@@ -0,0 +1,283 @@
+"""Build and search a taxonomy full text search database using
+`FTS5 <https://www.sqlite.org/fts5.html>`_. This works similarly to the API endpoint
+:py:func:`~pyinaturalist.v1.taxa.get_taxa_autocomplete`, which powers the taxon autocomplete feature
+on inaturalist.org:
+
+.. image::
+    ../images/inat-taxon-autocomplete.png
+
+**Extra dependencies**: ``sqlalchemy`` (only for building the database; not required for searches)
+
+**Build Example**::
+
+    >>> from pyinaturalist_convert import (
+    ...     aggregate_taxon_db, enable_logging, load_dwca_tables, load_fts_taxa
+    ... )
+
+    >>> # Optional, but recommended:
+    >>> enable_logging()
+    >>> load_dwca_tables()
+    >>> aggregate_taxon_db()
+
+    >>> # Load FTS table for all languages (Defaults to English names only):
+    >>> load_fts_taxa(language='all')
+
+.. note::
+    Running :py:func:`.aggregate_taxon_db` will result in more accurate search rankings based
+    on taxon counts, but will take a couple hours to complete.
+
+**Search example**::
+
+    >>> from pyinaturalist_convert import TaxonAutocompleter
+
+    >>> ta = TaxonAutocompleter()
+
+    >>> # Search by scientific name
+    >>> ta.search('aves')
+    [
+        Taxon(id=3, name='Aves'),
+        Taxon(id=1043988, name='Avesicaria'),
+        ...,
+    ]
+
+    >>> # Or by common name
+    >>> ta.search('frill')
+    [
+        Taxon(id=56447, name='Acid Frillwort'),
+        Taxon(id=614339, name='Antilles Frillfin'),
+        ...,
+    ]
+
+    >>> # Or by common name in a specific language
+    >>> ta.search('flughund', language='german')
+
+**Main classes & functions:**
+
+.. autosummary::
+    :nosignatures:
+
+    TaxonAutocompleter
+    load_fts_taxa
+
+"""
+import sqlite3
+from functools import partial
+from logging import getLogger
+from pathlib import Path
+from typing import Dict, Iterable, List, Union
+
+from pyinaturalist.models import Taxon
+
+from .constants import DB_PATH, DWCA_TAXON_CSV_DIR, TAXON_COUNTS, PathOrStr
+from .download import CSVProgress, get_progress_spinner
+from .sqlite import load_table, vacuum_analyze
+
+# Add extra text search prefix indexes to speed up searches for these prefix lengths
+PREFIX_INDEXES = [2, 3, 4]
+
+# Columns to use for text search table, and which should be indexed
+TAXON_FTS_TABLE = 'taxon_fts'
+TAXON_FTS_COLUMNS = {
+    'name': True,
+    'taxon_id': False,
+    'taxon_rank': False,
+    'count_rank': False,
+    'language': False,
+}
+
+TAXON_COUNT_RANK_FACTOR = 2.5
+TAXON_NAME_MAP = {
+    'scientificName': 'name',
+    'id': 'taxon_id',
+    'taxonRank': 'taxon_rank',
+    'count_rank': 'count_rank',
+}
+COMMON_TAXON_NAME_MAP = {
+    'vernacularName': 'name',
+    'id': 'taxon_id',
+    'language': 'language_code',
+    'count_rank': 'count_rank',
+}
+# Other available fields:
+# 'language',
+# 'locality',
+# 'countryCode',
+# 'source',
+# 'lexicon',
+# 'contributor',
+# 'created',
+
+logger = getLogger(__name__)
+
+
+# TODO: Deduplicate results (if both common and scientific names are present)
+class TaxonAutocompleter:
+    """Taxon autocomplete search.
+
+    Args:
+        db_path: Path to SQLite database; uses platform-specific data directory by default
+        limit: Maximum number of results to return per query
+    """
+
+    def __init__(self, db_path: PathOrStr = DB_PATH, limit: int = 10):
+        self.limit = limit
+        self.connection = sqlite3.connect(db_path)
+        self.connection.row_factory = sqlite3.Row
+
+    def search(self, q: str, language: str = 'en') -> List[Taxon]:
+        """Search for taxa by scientific and/or common name.
+
+        Args:
+            q: Search query
+            language: Language code for common names
+
+        Returns:
+            Taxon objects (with ID and name only)
+        """
+        if not q:
+            return []
+
+        language = (language or '').lower().replace('-', '_')
+        query = f'SELECT *, rank, (rank - count_rank) AS combined_rank FROM {TAXON_FTS_TABLE} '
+        query += f"WHERE name MATCH '{q}*' AND (language_code IS NULL "
+        query += f"OR language_code = '{language}') " if language else ' '
+        query += f'ORDER BY combined_rank LIMIT {self.limit}'
+
+        with self.connection as conn:
+            results = sorted(conn.execute(query).fetchall(), key=lambda row: row['combined_rank'])
+            return [
+                Taxon(id=int(row['taxon_id']), name=row['name'], rank=row['taxon_rank'])
+                for row in results
+            ]
+
+
+def load_fts_taxa(
+    csv_dir: PathOrStr = DWCA_TAXON_CSV_DIR,
+    db_path: PathOrStr = DB_PATH,
+    counts_path: PathOrStr = TAXON_COUNTS,
+    languages: Iterable[str] = ('english',),
+):
+    """Create full text search tables for taxonomic names.
+    Requires SQLite FTS5 extension and the iNat taxonomy DwC-A archive.
+
+    Args:
+        csv_dir: Directory containing extracted CSV files
+        db_path: Path to SQLite database
+        counts_path: Path to previously calculated taxon counts
+            (from :py:func:`.aggregate_taxon_db`)
+        lanugages: List of common name languages to load, or 'all' to load everything
+    """
+    csv_dir = Path(csv_dir).expanduser()
+    main_csv = csv_dir / 'taxa.csv'
+    common_name_csvs = get_common_name_csvs(csv_dir, languages)
+    progress = CSVProgress(main_csv, *common_name_csvs.values())
+
+    taxon_counts = normalize_taxon_counts(counts_path)
+    transform = partial(add_taxon_counts, taxon_counts=taxon_counts)
+
+    def load_fts_table(csv_path, column_map):
+        load_table(
+            csv_path,
+            db_path,
+            TAXON_FTS_TABLE,
+            column_map,
+            progress=progress,
+            transform=transform,
+        )
+
+    with progress:
+        logger.info(
+            f'Loading taxon scientific names + common names for {len(common_name_csvs)} languages:'
+            + ', '.join(common_name_csvs.keys())
+        )
+        create_fts5_table(db_path)
+
+        for lang, csv_file in common_name_csvs.items():
+            lang = lang.lower().replace('-', '_')
+            load_fts_table(csv_file, COMMON_TAXON_NAME_MAP)
+        load_fts_table(main_csv, TAXON_NAME_MAP)
+
+    optimize_fts_table(db_path)
+
+
+def get_common_name_csvs(csv_dir: Path, languages: Iterable[str] = None) -> Dict[str, Path]:
+    """Get common name CSVs, for either all or some languages, with a separate table per language"""
+    if languages and languages != 'all':
+        common_name_csvs = {lang: csv_dir / f'VernacularNames-{lang}.csv' for lang in languages}
+        return {lang: csv_path for lang, csv_path in common_name_csvs.items() if csv_path.exists()}
+    else:
+        return {
+            path.stem.replace('VernacularNames-', ''): path
+            for path in csv_dir.glob('VernacularNames-*.csv')
+        }
+
+
+def create_fts5_table(db_path: PathOrStr = DB_PATH):
+    prefix_idxs = ', '.join([f'prefix={i}' for i in PREFIX_INDEXES])
+
+    with sqlite3.connect(db_path) as conn:
+        conn.execute(
+            f'CREATE VIRTUAL TABLE IF NOT EXISTS {TAXON_FTS_TABLE} USING fts5( '
+            '   name, taxon_id, taxon_rank UNINDEXED, count_rank UNINDEXED, language_code,'
+            f'  {prefix_idxs})'
+        )
+
+
+def optimize_fts_table(db_path: PathOrStr = DB_PATH):
+    """Some final cleanup after loading text search tables"""
+    logger.info('Optimizing FTS table')
+    progress = get_progress_spinner('Optimizing table')
+    with progress, sqlite3.connect(db_path) as conn:
+        _load_taxon_ranks(conn)
+        conn.execute(f"INSERT INTO {TAXON_FTS_TABLE}({TAXON_FTS_TABLE}) VALUES('optimize')")
+        conn.commit()
+    vacuum_analyze([TAXON_FTS_TABLE], db_path, show_spinner=True)
+
+
+def _load_taxon_ranks(conn):
+    """Set taxon ranks for common name results. Attempt to get from full taxa table, which
+    will be much faster than using text search table.
+    """
+    try:
+        conn.execute(
+            f'UPDATE {TAXON_FTS_TABLE} SET taxon_rank = '
+            f'(SELECT t2.rank from taxon t2 WHERE t2.id = {TAXON_FTS_TABLE}.taxon_id) '
+            'WHERE taxon_rank IS NULL'
+        )
+    except sqlite3.OperationalError:
+        logger.warning('Full taxon table not found; ranks not loaded for common names')
+
+
+def add_taxon_counts(row: Dict[str, Union[int, str]], taxon_counts: Dict[int, int]):
+    """Add taxon counts to a chunk of taxon records read from CSV"""
+    taxon_id = int(row['id'])
+    row['count_rank'] = taxon_counts.get(taxon_id, -1)
+    if row.get('language_code'):
+        row['language_code'] = str(row['language_code']).lower().replace('-', '_')
+    return row
+
+
+# TODO: Read from taxon table instead
+def normalize_taxon_counts(counts_path: PathOrStr = TAXON_COUNTS) -> Dict[int, int]:
+    """Read previously calculated taxon counts, and normalize to a logarithmic distribution"""
+    import numpy as np
+    import pandas as pd
+
+    if not Path(counts_path).is_file():
+        logger.warning(f'Taxon counts file not found: {counts_path}')
+        return {}
+
+    logger.info(f'Reading taxon counts from {counts_path}')
+    df = pd.read_parquet(counts_path)
+
+    def normalize(series):
+        with np.errstate(divide='ignore'):
+            series = np.log(series.copy())
+        series[np.isneginf(series)] = 0
+        return (series - series.mean()) / series.std()
+
+    logger.info('Normalizing taxon counts')
+    df['count_rank'] = normalize(df['observations_count']).fillna(-1)
+    df['count_rank'] = df['count_rank'] * TAXON_COUNT_RANK_FACTOR
+    df = df.sort_values(by='count_rank', ascending=False)
+    return df['count_rank'].to_dict()
