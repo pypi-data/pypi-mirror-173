1e78c55806e3c519a4379bfaa5b8317d15501532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:23 +0000

    Add package cornflakes/2.7.8/cornflakes-2.7.8-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-2.7.8-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/cli/_create.py
@@ -0,0 +1,45 @@
+import logging
+
+from click import style, version_option
+import pkg_resources
+
+from cornflakes.click import command, group, pass_context
+from cornflakes.logging import attach_log
+
+
+@group("create")
+@version_option(
+    prog_name="cornflakes",
+    version=pkg_resources.get_distribution("cornflakes").version,
+    message=style(
+        f"\033[95m{'cornflakes'}\033"
+        f"[0m \033[95mVersion\033[0m: \033[1m"
+        f"{pkg_resources.get_distribution('cornflakes').version}\033[0m"
+    ),
+)
+@pass_context
+def create_new_config(ctx, parent):
+    """Create config template."""
+    if ctx.invoked_subcommand is None:
+        parent.console.print("[blue]I was invoked without subcommand")
+        test(parent)
+
+
+@command("test")
+def test(parent):  # verbose, background_process, self, parent
+    """Test click."""
+    logging.info("call create")
+    logging.debug("debug log?")
+    for _ in range(5):
+        parent.console.print("[blue]HI")
+
+    @attach_log
+    class Test:
+        @classmethod
+        def test(cls):
+            cls.logger.info("test")
+
+    Test.test()
+
+
+create_new_config.add_command(test)
