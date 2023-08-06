3c09e09ee10044c8138ef7d3dda0138cc1ac91c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:38 +0000

    Add package konso-dice-roller/0.0.6/konso_dice_roller-0.0.6-py3-none-any.whl

---
+++ packages/konso-dice-roller/konso_dice_roller-0.0.6-py3-none-any.whl/konso_dice_roller/interface.py
@@ -0,0 +1,45 @@
+from .format import roll_as_text, roll_as_markdown_text
+from .parse import parse_input, validate_roll_info
+from .roll import Roll
+
+
+def roll_string_from_input(
+    input_string: str,
+    number_of_dice_limit: int = 0,
+    dice_sides_limit: int = 0,
+    bonus_absolute_value_limit: int = 0,
+) -> str:
+    """Will raise a ValueError on errors"""
+    if "rick" in input_string.lower():
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+    roll_info = parse_input(input_string)
+    validate_roll_info(
+        roll_info=roll_info,
+        number_of_dice_limit=number_of_dice_limit,
+        dice_sides_limit=dice_sides_limit,
+        bonus_absolute_value_limit=bonus_absolute_value_limit,
+    )
+    roll = Roll.from_roll_info(roll_info)
+    return roll_as_text(roll)
+
+
+def markdown_roll_string_from_input(
+    input_string: str,
+    number_of_dice_limit: int = 0,
+    dice_sides_limit: int = 0,
+    bonus_absolute_value_limit: int = 0,
+) -> str:
+    """Will raise a ValueError on errors"""
+    if "rick" in input_string.lower():
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+    roll_info = parse_input(input_string)
+    validate_roll_info(
+        roll_info=roll_info,
+        number_of_dice_limit=number_of_dice_limit,
+        dice_sides_limit=dice_sides_limit,
+        bonus_absolute_value_limit=bonus_absolute_value_limit,
+    )
+    roll = Roll.from_roll_info(roll_info)
+    return roll_as_markdown_text(roll)
