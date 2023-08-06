fa296db50cda8f24fb02b3ffa2c3784d96369f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:28 +0000

    Add package sinabs/1.0.6.dev1/sinabs-1.0.6.dev1.tar.gz

---
+++ packages/sinabs/sinabs-1.0.6.dev1.tar.gz/sinabs-1.0.6.dev1/sinabs/__init__.py
@@ -0,0 +1,11 @@
+from pbr.version import VersionInfo
+
+__version__ = VersionInfo("sinabs").release_string()
+
+from .network import Network
+from .synopcounter import SynOpCounter, SNNSynOpCounter
+from .from_torch import from_model
+from .utils import (
+    get_activations,
+    get_network_activations,
+)
