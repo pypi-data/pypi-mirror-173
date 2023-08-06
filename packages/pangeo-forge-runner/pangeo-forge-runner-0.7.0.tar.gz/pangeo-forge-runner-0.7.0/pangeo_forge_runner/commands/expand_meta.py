033e6e966a521744f08cd0ac1f613e77378a348b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:40 +0000

    Add package pangeo-forge-runner/0.7.0/pangeo-forge-runner-0.7.0.tar.gz

---
+++ packages/pangeo-forge-runner/pangeo-forge-runner-0.7.0.tar.gz/pangeo-forge-runner-0.7.0/pangeo_forge_runner/commands/expand_meta.py
@@ -0,0 +1,36 @@
+import json
+import tempfile
+from pathlib import Path
+
+from .. import Feedstock
+from ..stream_capture import redirect_stderr, redirect_stdout
+from .base import BaseCommand, common_aliases, common_flags
+
+
+class ExpandMeta(BaseCommand):
+    """
+    Application to expand meta.yaml to be fully formed.
+
+    Will execute arbitrary code if necessary to resolve
+    dict_object recipes.
+    """
+
+    aliases = common_aliases
+    flags = common_flags
+
+    def start(self):
+        with tempfile.TemporaryDirectory() as d:
+            self.fetch(d)
+            feedstock = Feedstock(Path(d) / self.feedstock_subdir)
+            with redirect_stderr(self.log, {"status": "running"}), redirect_stdout(
+                self.log, {"status": "running"}
+            ):
+                expanded = feedstock.get_expanded_meta()
+            if self.json_logs:
+                self.log.info(
+                    "Expansion complete",
+                    extra={"status": "completed", "meta": expanded},
+                )
+            else:
+                self.log.info("Expansion complete\n", extra={"status": "completed"})
+                self.log.info(json.dumps(expanded))
