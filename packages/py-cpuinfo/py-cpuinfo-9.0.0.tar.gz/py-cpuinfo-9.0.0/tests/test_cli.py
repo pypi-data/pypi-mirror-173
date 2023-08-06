4ef761f7eca6c5a04c4ff4e563e0cb90e1058ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:48 +0000

    Add package py-cpuinfo/9.0.0/py-cpuinfo-9.0.0.tar.gz

---
+++ packages/py-cpuinfo/py-cpuinfo-9.0.0.tar.gz/py-cpuinfo-9.0.0/tests/test_cli.py
@@ -0,0 +1,91 @@
+
+
+import unittest
+from cpuinfo import *
+import helpers
+
+
+
+class TestCLI(unittest.TestCase):
+	def setUp(self):
+		helpers.backup_data_source(cpuinfo)
+
+	def tearDown(self):
+		helpers.restore_data_source(cpuinfo)
+
+	def test_json(self):
+		from subprocess import Popen, PIPE
+		import json
+
+		command = [sys.executable, 'cpuinfo/cpuinfo.py', '--json']
+		p1 = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		output = p1.communicate()[0]
+
+		self.assertEqual(0, p1.returncode)
+
+		output = output.decode(encoding='UTF-8')
+
+		info = json.loads(output, object_hook = cpuinfo._utf_to_str)
+
+		self.assertEqual(list(cpuinfo.CPUINFO_VERSION), info['cpuinfo_version'])
+		self.assertEqual(cpuinfo.CPUINFO_VERSION_STRING, info['cpuinfo_version_string'])
+
+	def test_version(self):
+		from subprocess import Popen, PIPE
+
+		command = [sys.executable, 'cpuinfo/cpuinfo.py', '--version']
+		p1 = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		output = p1.communicate()[0]
+
+		self.assertEqual(0, p1.returncode)
+
+		output = output.decode(encoding='UTF-8')
+		output = output.strip()
+
+		self.assertEqual(cpuinfo.CPUINFO_VERSION_STRING, output)
+
+	def test_trace(self):
+		import os
+		import re
+		from subprocess import Popen, PIPE
+
+		# Get all log files before test
+		before_log_files = [f for f in os.listdir('.') if os.path.isfile(f) and re.match(r'^cpuinfo_trace_\d+-\d+-\d+_\d+-\d+-\d+-\d+.trace$', f)]
+		#print('\n', before_log_files)
+
+		# Run with trace to generate new log file
+		command = [sys.executable, 'cpuinfo/cpuinfo.py', '--trace']
+		p1 = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		output = p1.communicate()[0]
+		self.assertEqual(0, p1.returncode)
+
+		# Get all log files after test
+		after_log_files = [f for f in os.listdir('.') if os.path.isfile(f) and re.match(r'^cpuinfo_trace_\d+-\d+-\d+_\d+-\d+-\d+-\d+.trace$', f)]
+		#print('\n', after_log_files)
+
+		# Read the new log file into a string
+		new_log_file = list(set(after_log_files) - set(before_log_files))[0]
+		with open(new_log_file, 'r') as f:
+			output = f.read().strip()
+
+		# Remove the new log file
+		os.remove(new_log_file)
+
+		self.assertTrue(len(output) > 200)
+		self.assertTrue(output.startswith('!' * 80))
+		self.assertTrue(output.endswith('!' * 80))
+
+	def test_default(self):
+		from subprocess import Popen, PIPE
+
+		command = [sys.executable, 'cpuinfo/cpuinfo.py']
+		p1 = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		output = p1.communicate()[0]
+
+		self.assertEqual(0, p1.returncode)
+
+		output = output.decode(encoding='UTF-8')
+
+		version = output.split('Cpuinfo Version: ')[1].split('\n')[0].strip()
+
+		self.assertEqual(cpuinfo.CPUINFO_VERSION_STRING, version)
---
+++ packages/py-cpuinfo/py-cpuinfo-9.0.0.tar.gz/py-cpuinfo-9.0.0/tests/test_selinux.py
@@ -0,0 +1,103 @@
+
+
+import unittest
+from cpuinfo import *
+import helpers
+
+
+class MockDataSource_enforcing(object):
+	@staticmethod
+	def has_sestatus():
+		return True
+
+	@staticmethod
+	def sestatus_b():
+		returncode = 0
+		output = r'''
+SELinux status:                 enabled
+SELinuxfs mount:                /sys/fs/selinux
+SELinux root directory:         /etc/selinux
+Loaded policy name:             targeted
+Current mode:                   enforcing
+Mode from config file:          enforcing
+Policy MLS status:              enabled
+Policy deny_unknown status:     allowed
+Memory protection checking:     actual (secure)
+Max kernel policy version:      31
+'''
+		return returncode, output
+
+class MockDataSource_not_enforcing(object):
+	@staticmethod
+	def has_sestatus():
+		return True
+
+	@staticmethod
+	def sestatus_b():
+		returncode = 0
+		output = r'''
+SELinux status:                 enabled
+SELinuxfs mount:                /sys/fs/selinux
+SELinux root directory:         /etc/selinux
+Loaded policy name:             targeted
+Current mode:                   eating
+Mode from config file:          enforcing
+Policy MLS status:              enabled
+Policy deny_unknown status:     allowed
+Memory protection checking:     actual (secure)
+Max kernel policy version:      31
+'''
+		return returncode, output
+
+class MockDataSource_exec_mem_and_heap(object):
+	@staticmethod
+	def has_sestatus():
+		return True
+
+	@staticmethod
+	def sestatus_b():
+		returncode = 0
+		output = r'''
+allow_execheap                  on
+allow_execmem                   on
+'''
+		return returncode, output
+
+class MockDataSource_no_exec_mem_and_heap(object):
+	@staticmethod
+	def has_sestatus():
+		return True
+
+	@staticmethod
+	def sestatus_b():
+		returncode = 0
+		output = r'''
+allow_execheap                  off
+allow_execmem                   off
+'''
+		return returncode, output
+
+
+class TestSELinux(unittest.TestCase):
+	def setUp(self):
+		helpers.backup_data_source(cpuinfo)
+		self.trace = Trace(False, False)
+
+	def tearDown(self):
+		helpers.restore_data_source(cpuinfo)
+
+	def test_enforcing(self):
+		helpers.monkey_patch_data_source(cpuinfo, MockDataSource_enforcing)
+		self.assertEqual(True, cpuinfo._is_selinux_enforcing(self.trace))
+
+	def test_not_enforcing(self):
+		helpers.monkey_patch_data_source(cpuinfo, MockDataSource_not_enforcing)
+		self.assertEqual(False, cpuinfo._is_selinux_enforcing(self.trace))
+
+	def test_exec_mem_and_heap(self):
+		helpers.monkey_patch_data_source(cpuinfo, MockDataSource_exec_mem_and_heap)
+		self.assertEqual(False, cpuinfo._is_selinux_enforcing(self.trace))
+
+	def test_no_exec_mem_and_heap(self):
+		helpers.monkey_patch_data_source(cpuinfo, MockDataSource_no_exec_mem_and_heap)
+		self.assertEqual(True, cpuinfo._is_selinux_enforcing(self.trace))
