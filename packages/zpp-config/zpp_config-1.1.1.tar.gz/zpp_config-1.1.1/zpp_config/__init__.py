48351a43d8c90d37da92a286b805c2a1bbb83c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:34 +0000

    Add package zpp-config/1.1.1/zpp_config-1.1.1.tar.gz

---
+++ packages/zpp-config/zpp_config-1.1.1.tar.gz/zpp_config-1.1.1/zpp_config/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.1.1'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Module pour le chargement et la modification de fichier de configuration'
+__lastupdate__ = '10/08/2022'
+__releasenote__ = 'Patch dict value'
+
+from .config import *
