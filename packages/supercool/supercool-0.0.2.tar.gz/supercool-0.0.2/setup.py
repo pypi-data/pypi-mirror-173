02ab755433c67f4619f302132edff0d140b196ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:11 +0000

    Add package supercool/0.0.2/supercool-0.0.2.tar.gz

---
+++ packages/supercool/supercool-0.0.2.tar.gz/supercool-0.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import setuptools
+import re
+
+version = ""
+logn_description = ""
+with open('supercool/VERSION', 'r') as fd:
+    version = fd.readline().strip()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read().strip()
+
+setuptools.setup(
+    name = "supercool",
+    version = version,
+    author = "isaachan",
+    author_email = "isaachanstar@gmail.com",
+    description = "This is the SDK for example.",
+    long_description = long_description,
+    url = "http://example.com",
+    install_requires = [
+	'requests'
+    ],
+    packages = setuptools.find_packages(where="supercool"),
+    scripts=['scripts/supercool.py'],
+    classifiers = [
+	'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
+
+
