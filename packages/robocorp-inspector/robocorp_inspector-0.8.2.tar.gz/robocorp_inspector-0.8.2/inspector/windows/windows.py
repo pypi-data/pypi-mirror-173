f5cf0d9be5c64a7f312b9b1d8d7c76ceaafd4180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:26 +0000

    Add package robocorp-inspector/0.8.2/robocorp_inspector-0.8.2.tar.gz

---
+++ packages/robocorp-inspector/robocorp_inspector-0.8.2.tar.gz/robocorp_inspector-0.8.2/inspector/windows/windows.py
@@ -0,0 +1,12 @@
+from inspector_commons.bridge.bridge_windows import WindowsBridge  # type: ignore
+from inspector.windows.base import Window
+
+
+class WindowsWindow(Window):
+    BRIDGE = WindowsBridge
+    DEFAULTS = {
+        "title": "Robocorp App Element Locator",
+        "url": "windows.html",
+        "width": 560,
+        "height": 720,
+    }
