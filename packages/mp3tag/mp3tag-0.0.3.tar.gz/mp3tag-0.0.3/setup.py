dd1fe5d2e4f96b54888735ef519cf84089c5560b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:47 +0000

    Add package mp3tag/0.0.3/mp3tag-0.0.3.tar.gz

---
+++ packages/mp3tag/mp3tag-0.0.3.tar.gz/mp3tag-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+# from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'MP3 tag information reader and writer.'
+LONG_DESCRIPTION = 'MP3 contains many information about the music and stored in the tag. This package support read and write id3v2 tag to the mp3 file.'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name='mp3tag',
+    version=VERSION,
+    author='Wang Shiqiang',
+    author_email='cocowool@qq.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cocowool/mp3tag",
+    project_urls={
+        "Bug Tracker":"https://github.com/cocowool/mp3tag/issues"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[],
+
+    keywords=['mp3','id3','id3v2'],
+    classifiers=[],
+    python_requires=">=3.6"
+)
---
+++ packages/mp3tag/mp3tag-0.0.3.tar.gz/mp3tag-0.0.3/src/mp3tag/__init__.py
@@ -0,0 +1,7 @@
+# import musictag
+# import tag_reader
+# import tag_writer
+# from .mp3tag import tag_reader
+# from .mp3tag import tag_writer
+
+__version__ = "0.0.3"
