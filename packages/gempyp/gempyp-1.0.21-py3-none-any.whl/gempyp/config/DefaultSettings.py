79460bbc81b89da9ea896ffbce5ac532f87cab66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:47 +0000

    Add package gempyp/1.0.21/gempyp-1.0.21-py3-none-any.whl

---
+++ packages/gempyp/gempyp-1.0.21-py3-none-any.whl/gempyp/config/DefaultSettings.py
@@ -0,0 +1,16 @@
+import os
+
+# have to discuss about the default location
+DEFAULT_GEMPYP_FOLDER = os.getcwd()
+DEBUG = True
+THREADS = 8
+_VERSION = "1.0.21"
+
+urls = {
+
+    "suiteInfo": "https://apis.gemecosystem.com/suiteinfo/",
+    "suiteExec": "https://apis.gemecosystem.com/suiteexe",
+    "testcases": "https://apis.gemecosystem.com/testcase",
+
+}
+count = 0
