eb3026c22e032c3bce5d64f64e2b904399c420a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:36 +0000

    Add package ntrprtr/2.1.0/ntrprtr-2.1.0-py3-none-any.whl

---
+++ packages/ntrprtr/ntrprtr-2.1.0-py3-none-any.whl/ntrprtr/action/ActionType.py
@@ -0,0 +1,12 @@
+class ActionType():
+    DECIMAL = "decimal"
+    ASCII = "ascii"
+    EQUALS = "equals"
+    BITEQUALS = "bitequals"
+    BINARY = "binary"
+    HEXDUMP = "hexdump"
+    DOSDATE = "dosdate"
+    DOSTIME = "dostime"
+    UNIXTIME = "unixtime"
+    UNICODE = "unicode"
+    ENDIANESS = "endianess"
