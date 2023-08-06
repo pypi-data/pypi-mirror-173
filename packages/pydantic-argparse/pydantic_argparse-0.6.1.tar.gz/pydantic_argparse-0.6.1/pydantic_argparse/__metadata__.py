fa8860f069fd3268d1abc7c5ff0976e815e22dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:40 +0000

    Add package pydantic-argparse/0.6.1/pydantic_argparse-0.6.1.tar.gz

---
+++ packages/pydantic-argparse/pydantic_argparse-0.6.1.tar.gz/pydantic_argparse-0.6.1/pydantic_argparse/__metadata__.py
@@ -0,0 +1,22 @@
+"""Single-Source of Truth Package Versioning and Metadata.
+
+The `pydantic-argparse` package uses the `pyproject.toml` file as a
+single-source of truth for the package metadata. As such, rather than
+duplicating the metadata in code here, it is retrieved from the installed
+package metadata at runtime.
+
+The metadata exported are the `title`, `description`, `version`, `author` and
+`license` of the package
+"""
+
+
+# Standard
+from importlib import metadata
+
+
+# Retrieve Metadata from Package
+__title__: str = metadata.metadata(__package__)["name"]
+__description__: str = metadata.metadata(__package__)["summary"]
+__version__: str = metadata.metadata(__package__)["version"]
+__author__: str = metadata.metadata(__package__)["author"]
+__license__: str = metadata.metadata(__package__)["license"]
