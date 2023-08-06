32986b487eaba0821a2a6234baf4c95a18539fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:50 +0000

    Add package RealstatsModelRollout/0.2.0/RealstatsModelRollout-0.2.0-py3-none-any.whl

---
+++ packages/RealstatsModelRollout/RealstatsModelRollout-0.2.0-py3-none-any.whl/RealstatsModelRollout/global_functions.py
@@ -0,0 +1,25 @@
+from .settings import Settings
+import os
+
+class globalFunctions:
+    def __init__(self):
+        self._id = "Development"
+
+    ### get index location of char in string ###
+    def Find(s, ch):
+        return [i for i, ltr in enumerate(s) if ltr == ch]
+
+    def Path_is_dir(localpath = ""):
+        local_envpath=""
+        ### Get directory ###
+        print("Looking for directory")
+        if localpath[-1] == '/':
+            local_envpath = localpath
+        else:
+            local_envpath = localpath + "/"
+
+        isDirectory = os.path.isdir(local_envpath)
+        if isDirectory == True:
+            return local_envpath
+        else:
+            raise Exception("Given localpath: " + local_envpath + " is not a directory")
