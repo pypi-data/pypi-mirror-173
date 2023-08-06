636cc96b6c86b12c2aab5ba4aad7f56a9a299893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:40 +0000

    Add package tumoroscope-pymc/0.1.0/tumoroscope-pymc-0.1.0.tar.gz

---
+++ packages/tumoroscope-pymc/tumoroscope-pymc-0.1.0.tar.gz/tumoroscope-pymc-0.1.0/tumoroscope/__init__.py
@@ -0,0 +1,6 @@
+"""'Tumoroscope' model built with PyMC."""
+
+from .model import build_tumoroscope_model  # noqa: F401
+from .tumoroscope_data import TumoroscopeData  # noqa: F401
+
+__version__ = "0.1.0"
