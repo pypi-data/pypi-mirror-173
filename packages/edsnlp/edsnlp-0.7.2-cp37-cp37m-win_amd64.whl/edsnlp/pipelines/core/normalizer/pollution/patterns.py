3e9e89be2b86d1448a44b752ac860e450df4a87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:59 +0000

    Add package edsnlp/0.7.2/edsnlp-0.7.2-cp37-cp37m-win_amd64.whl

---
+++ packages/edsnlp/edsnlp-0.7.2-cp37-cp37m-win_amd64.whl/edsnlp/pipelines/core/normalizer/pollution/patterns.py
@@ -0,0 +1,37 @@
+# noinspection SpellCheckingInspection
+information = (
+    r"(?s)(=====+\s*)?(L\s*e\s*s\sdonnées\s*administratives,\s*sociales\s*|"
+    r"I?nfo\s*rmation\s*aux?\s*patients?|"
+    r"L[’']AP-HP\s*collecte\s*vos\s*données\s*administratives|"
+    r"L[’']Assistance\s*Publique\s*-\s*Hôpitaux\s*de\s*Paris\s*"
+    r"\(?AP-HP\)?\s*a\s*créé\s*une\s*base\s*de\s*données)"
+    r".{,2000}https?:\/\/recherche\.aphp\.fr\/eds\/droit-opposition[\s\.]*"
+)
+
+# Example : NBNbWbWbNbWbNBNbNbWbWbNBNbWbNbNbWbNBNbW...
+bars = r"(?i)([nbw]|_|-|=){5,}"
+
+# Biology tables: Prone to false positive with disease names
+biology = r"(\b.*[|¦].*\n)+"
+
+# Leftside note with doctor names
+doctors = r"(?mi)(^((dr)|(pr))(\.|\s|of).*)+"
+
+# Mails or websites
+web = r"(www\.\S*)|(\S*@\S*)"
+
+# Subsection with ICD-10 Codes
+coding = r".*?[a-zA-Z]\d{2,4}.*?(\n|[a-zA-Z]\d{2,4})"
+
+# New page
+footer = r"(?i)^\d\/\d\s?pat.*ipp.*\n?"
+
+pollution = dict(
+    information=information,
+    bars=bars,
+    biology=biology,
+    doctors=doctors,
+    web=web,
+    coding=coding,
+    footer=footer,
+)
---
+++ packages/edsnlp/edsnlp-0.7.2-cp37-cp37m-win_amd64.whl/edsnlp/pipelines/qualifiers/history/factory.py
@@ -0,0 +1,70 @@
+from datetime import timedelta
+from typing import List, Optional
+
+from spacy.language import Language
+
+from edsnlp.pipelines.qualifiers.history import History, patterns
+from edsnlp.pipelines.terminations import termination
+from edsnlp.utils.deprecation import deprecated_factory
+
+DEFAULT_CONFIG = dict(
+    attr="NORM",
+    history=patterns.history,
+    termination=termination,
+    use_sections=False,
+    use_dates=False,
+    history_limit=timedelta(14),
+    exclude_birthdate=True,
+    explain=False,
+    on_ents_only=True,
+)
+
+
+@deprecated_factory(
+    "antecedents",
+    "eds.history",
+    default_config=DEFAULT_CONFIG,
+    assigns=["span._.history"],
+)
+@deprecated_factory(
+    "eds.antecedents",
+    "eds.history",
+    default_config=DEFAULT_CONFIG,
+    assigns=["span._.history"],
+)
+@deprecated_factory(
+    "history",
+    "eds.history",
+    default_config=DEFAULT_CONFIG,
+    assigns=["span._.history"],
+)
+@Language.factory(
+    "eds.history",
+    default_config=DEFAULT_CONFIG,
+    assigns=["span._.history"],
+)
+def create_component(
+    nlp: Language,
+    name: str,
+    history: Optional[List[str]],
+    termination: Optional[List[str]],
+    use_sections: bool,
+    use_dates: bool,
+    history_limit: timedelta,
+    exclude_birthdate: bool,
+    attr: str,
+    explain: bool,
+    on_ents_only: bool,
+):
+    return History(
+        nlp,
+        attr=attr,
+        history=history,
+        termination=termination,
+        use_sections=use_sections,
+        use_dates=use_dates,
+        history_limit=history_limit,
+        exclude_birthdate=exclude_birthdate,
+        explain=explain,
+        on_ents_only=on_ents_only,
+    )
