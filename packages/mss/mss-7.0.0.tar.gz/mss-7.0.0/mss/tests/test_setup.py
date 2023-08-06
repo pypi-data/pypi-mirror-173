b42f8501a3ada544247bb826cf54a6ac8a69ef78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:13 +0000

    Add package mss/7.0.0/mss-7.0.0.tar.gz

---
+++ packages/mss/mss-7.0.0.tar.gz/mss-7.0.0/mss/tests/test_setup.py
@@ -0,0 +1,19 @@
+"""
+This is part of the MSS Python's module.
+Source: https://github.com/BoboTiG/python-mss
+"""
+from subprocess import STDOUT, check_output
+
+from mss import __version__
+
+CMD = "python -m build --sdist --wheel".split()
+
+
+def test_wheel_python_3_only():
+    """Ensure the produced wheel is Python 3 only."""
+    output = check_output(CMD, stderr=STDOUT, text=True)
+    text = f"mss-{__version__}-py3-none-any.whl"
+    assert text in output
+
+    print(output)
+    assert "warning" not in output.lower()
