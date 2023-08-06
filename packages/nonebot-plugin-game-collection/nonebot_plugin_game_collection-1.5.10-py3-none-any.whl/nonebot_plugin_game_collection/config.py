9af618ccb29c5854923a612f7810e4461d7e844f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:42 +0000

    Add package nonebot-plugin-game-collection/1.5.10/nonebot_plugin_game_collection-1.5.10-py3-none-any.whl

---
+++ packages/nonebot-plugin-game-collection/nonebot_plugin_game_collection-1.5.10-py3-none-any.whl/nonebot_plugin_game_collection/config.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+from pydantic import BaseModel, Extra
+from pathlib import Path
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    russian_path: Path = Path()
+    max_bet_gold: int = 1000
+    race_bet_gold: int = 100
+    gacha_gold: int = 500
+    sign_gold: Tuple[int, int] = (100, 300)
+    revolt_sign_gold:Tuple[int, int] = (800, 1200)
+    security_gold:Tuple[int, int] = (50, 200)
+
+    market_info_chain = False
+    market_info_type = "image"
