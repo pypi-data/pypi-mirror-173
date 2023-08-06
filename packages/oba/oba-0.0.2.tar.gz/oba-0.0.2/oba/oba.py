80436f195fa41930a563b4a07715aceed4f58519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:30 +0000

    Add package oba/0.0.2/oba-0.0.2.tar.gz

---
+++ packages/oba/oba-0.0.2.tar.gz/oba-0.0.2/oba/oba.py
@@ -0,0 +1,73 @@
+class NoneObj:
+    __obj = dict()
+
+    def __iter__(self):
+        return iter(NoneObj.__obj)
+
+    def __contains__(self, item):
+        return False
+
+    def __getitem__(self, item):
+        return self
+
+    def __getattr__(self, item):
+        return self
+
+    def __setattr__(self, key, value):
+        return
+
+    def __bool__(self):
+        return False
+
+    def __str__(self):
+        raise ValueError('NoneObj')
+    
+    
+class Obj:
+    none = NoneObj()
+
+    @staticmethod
+    def iterable(obj):
+        types = [list, dict, tuple]
+        for t in types:
+            if isinstance(obj, t):
+                return True
+        return False
+
+    @staticmethod
+    def raw(o: 'Obj'):
+        return object.__getattribute__(o, '__obj')
+
+    def __init__(self, obj):
+        object.__setattr__(self, '__obj', obj)
+
+    def __getitem__(self, item):
+        obj = Obj.raw(self)
+        try:
+            obj = obj.__getitem__(item)
+        except Exception:
+            return Obj.none
+        if Obj.iterable(obj):
+            return Obj(obj)
+        return obj
+
+    def __getattr__(self, item):
+        return self[item]
+
+    def __setitem__(self, key, value):
+        obj = Obj.raw(self)
+        obj[key] = value
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __contains__(self, item):
+        obj = Obj.raw(self)
+        return item in obj
+
+    def __iter__(self):
+        obj = Obj.raw(self)
+        for item in obj:
+            if Obj.iterable(item):
+                item = Obj(item)
+            yield item
