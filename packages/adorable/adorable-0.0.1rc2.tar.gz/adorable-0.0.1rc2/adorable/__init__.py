fc3c0ac1e11d4e7b14d791f8807a0559399e1bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:08 +0000

    Add package adorable/0.0.1rc2/adorable-0.0.1rc2.tar.gz

---
+++ packages/adorable/adorable-0.0.1rc2.tar.gz/adorable-0.0.1rc2/adorable/__init__.py
@@ -0,0 +1,21 @@
+from .color import (
+    Color3bit,
+    Color8bit,
+    Color24bit,
+)
+
+from .style import (
+    BOLD,
+    DIM,
+    ITALIC,
+    UNDERLINE,
+    BLINK,
+    INVERSE,
+    INVISIBLE,
+    STRIKETHROUGH
+)
+
+from .ansi import *
+from .markup import *
+from .utils import filter_ansi
+
