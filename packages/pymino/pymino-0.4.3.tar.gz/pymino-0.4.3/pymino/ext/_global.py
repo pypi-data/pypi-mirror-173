4406f2bce5c0587e037da97a06423720e1ababd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:03 +0000

    Add package pymino/0.4.3/pymino-0.4.3.tar.gz

---
+++ packages/pymino/pymino-0.4.3.tar.gz/pymino-0.4.3/pymino/ext/_global.py
@@ -0,0 +1,8 @@
+from .utilities.generate import *
+
+class Global:
+    def __init__(self, session: ClientSession):
+        """
+        WIP - TO BE FINISHED LATER
+        """
+        self.session = session
