e4bebefce8a770ff8a4704551ea22ce43813ad59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:48 +0000

    Add package checkov/2.1.295/checkov-2.1.295.tar.gz

---
+++ packages/checkov/checkov-2.1.295.tar.gz/checkov-2.1.295/checkov/common/checks_infra/solvers/attribute_solvers/not_equals_attribute_solver.py
@@ -0,0 +1,16 @@
+from typing import Optional, Any, Dict
+
+from checkov.common.graph.checks_infra.enums import Operators
+from .equals_attribute_solver import EqualsAttributeSolver
+
+
+class NotEqualsAttributeSolver(EqualsAttributeSolver):
+    operator = Operators.NOT_EQUALS  # noqa: CCE003  # a static attribute
+
+    def _get_operation(self, vertex: Dict[str, Any], attribute: Optional[str]) -> bool:
+        attr_val = vertex.get(attribute)  # type:ignore[arg-type]  # due to attribute can be None
+        # if this value contains an underendered variable, then we cannot evaluate the check,
+        # so return True (since we cannot return UNKNOWN)
+        if self._is_variable_dependant(attr_val, vertex['source_']):
+            return True
+        return not super()._get_operation(vertex, attribute)
