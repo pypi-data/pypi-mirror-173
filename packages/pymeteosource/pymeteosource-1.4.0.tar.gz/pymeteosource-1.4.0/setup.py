6d5da7dc29d65765df895c7a871ea21b9df2d333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:22 +0000

    Add package pymeteosource/1.4.0/pymeteosource-1.4.0.tar.gz

---
+++ packages/pymeteosource/pymeteosource-1.4.0.tar.gz/pymeteosource-1.4.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    version="1.4.0",
+    name="pymeteosource",
+    packages=find_packages(),
+    install_requires=["wheel", "requests", "pytz"],
+    extras_require={"pandas": "pandas"},
+    description="Meteosource API wrapper library",
+    author="Meteosource",
+    author_email="support@meteosource.com",
+    url="https://github.com/Meteosource/pymeteosource/archive/v1.4.0.tar.gz",
+    license="MIT",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
