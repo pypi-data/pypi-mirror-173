57c981727e8ae147c6f6e7a983c20fcd29861f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:52 +0000

    Add package zzd/0.3.36/zzd-0.3.36-py3.9.egg

---
+++ packages/zzd/zzd-0.3.36-py3.9.egg/zzd/__init__.py
@@ -0,0 +1,10 @@
+name = "zzdlab"
+version = "0.1"
+from zzd.utils.assess import multi_scores as scores
+from zzd.feature_encode import  encodes
+from zzd.methods.interolog  import interolog
+from zzd.methods.ppi_randomforest import ppi_randomforest
+from zzd import models
+
+
+
---
+++ packages/zzd/zzd-0.3.36-py3.9.egg/zzd/feature_encode/encodes.py
@@ -0,0 +1,11 @@
+import numpy as np
+from zzd.feature_encode.ac import ac
+from zzd.feature_encode.dpc import dpc
+from zzd.feature_encode.cksaap import cksaap
+from zzd.feature_encode.ct import ct
+from zzd.feature_encode.pdb2dpc import pdb2dpc
+from zzd.feature_encode.pdb2graph import pdb2graph
+from zzd.feature_encode.feature_combine import feature_combine
+
+
+
