680fcaa1d6f238f0d36f33501746ad16d3aae5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:46 +0000

    Add package celescope/1.13.0/celescope-1.13.0-py3-none-any.whl

---
+++ packages/celescope/celescope-1.13.0-py3-none-any.whl/celescope/vdj/multi_vdj.py
@@ -0,0 +1,51 @@
+from celescope.tools.multi import Multi
+from celescope.vdj.__init__ import __ASSAY__
+
+
+class Multi_vdj(Multi):
+    """
+    ## Usage
+    ```
+    multi_vdj \\
+        --mapfile ./vdj.mapfile \\
+        --type TCR \\
+        --thread 8 \\
+        --mod shell
+    ``` 
+    """
+
+    def mapping_vdj(self, sample):
+        step = 'mapping_vdj'
+        cmd_line = self.get_cmd_line(step, sample)
+        if self.args.not_consensus:
+            fq = f'{self.outdir_dic[sample]["cutadapt"]}/{sample}_clean_2.fq{self.fq_suffix}'
+        else:
+            fq = f'{self.outdir_dic[sample]["consensus"]}/{sample}_consensus.fq'
+        cmd = (
+            f'{cmd_line} '
+            f'--fq {fq} '
+        )
+        self.process_cmd(cmd, step, sample, m=15, x=self.args.thread)
+
+    def count_vdj(self, sample):
+        # count_vdj
+        step = 'count_vdj'
+        cmd_line = self.get_cmd_line(step, sample)
+        UMI_count_filter_file = (
+            f'{self.outdir_dic[sample]["mapping_vdj"]}/{sample}_UMI_count_filtered.tsv'
+        )
+        cmd = (
+            f'{cmd_line} '
+            f'--match_dir {self.col4_dict[sample]} '
+            f'--UMI_count_filter_file {UMI_count_filter_file} '
+        )
+        self.process_cmd(cmd, step, sample, m=8, x=self.args.thread)
+
+
+def main():
+    multi = Multi_vdj(__ASSAY__)
+    multi.run()
+
+
+if __name__ == '__main__':
+    main()
