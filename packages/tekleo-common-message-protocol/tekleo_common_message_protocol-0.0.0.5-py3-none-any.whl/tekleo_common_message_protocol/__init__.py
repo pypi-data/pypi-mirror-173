fc7fa47d13dd1031cb6460239b158383f1b044d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:58 +0000

    Add package tekleo-common-message-protocol/0.0.0.5/tekleo_common_message_protocol-0.0.0.5-py3-none-any.whl

---
+++ packages/tekleo-common-message-protocol/tekleo_common_message_protocol-0.0.0.5-py3-none-any.whl/tekleo_common_message_protocol/__init__.py
@@ -0,0 +1,36 @@
+from .image_base64 import ImageBase64
+from .image_url import ImageUrl
+from .ping_output import PingOutput
+from .point_pixel import PointPixel
+from .point_relative import PointRelative
+from .rectangle_pixel import RectanglePixel
+from .rectangle_relative import RectangleRelative
+from .text import Text
+
+from .nlp.nlp_named_entity import NlpNamedEntity
+
+from .object_detection.od_labeled_item import OdLabeledItem
+from .object_detection.od_output import OdOutput
+from .object_detection.od_prediction import OdPrediction
+from .object_detection.od_sample import OdSample
+
+__all__ = [
+    # General
+    ImageBase64,
+    ImageUrl,
+    PingOutput,
+    PointPixel,
+    PointRelative,
+    RectanglePixel,
+    RectangleRelative,
+    Text,
+
+    # NLP
+    NlpNamedEntity,
+
+    # Object detection
+    OdLabeledItem,
+    OdOutput,
+    OdPrediction,
+    OdSample,
+]
