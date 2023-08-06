b90debc3b501ce26c00b2df1c2e880fa7076e625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:03 +0000

    Add package square-elk-json-formatter/0.0.2/square_elk_json_formatter-0.0.2.tar.gz

---
+++ packages/square-elk-json-formatter/square_elk_json_formatter-0.0.2.tar.gz/square_elk_json_formatter-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.2"
+
+setup(
+    name="square_elk_json_formatter",
+    version=__version__,
+    license="MIT",
+    description="",
+    url="https://github.com/UKP-SQuARE/square-elk-json-formatter",
+    download_url=f"https://github.com/UKP-SQuARE/square-elk-json-formatter/archive/refs/tags/v{__version__}.tar.gz",
+    author="UKP",
+    author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
+    packages=find_packages(),
+    install_requires=["python-json-logger==2.0.1"]
+)
