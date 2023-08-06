5b2b849bdd131c350c625ed5d748cfadd4fd3fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:45 +0000

    Add package lnschema_bionty/0.4.5/lnschema_bionty-0.4.5-py2.py3-none-any.whl

---
+++ packages/lnschema_bionty/lnschema_bionty-0.4.5-py2.py3-none-any.whl/lnschema_bionty/__init__.py
@@ -0,0 +1,66 @@
+"""Schema module for universal biological entities (`zdno`).
+
+Import the package::
+
+   import lnschema_bionty
+
+Biological entities, all initialized via `Bionty <https://lamin.ai/docs/bionty>`__:
+
+.. autosummary::
+   :toctree: .
+
+   gene
+   protein
+   cell_marker
+   cell_type
+   species
+   tissue
+   disease
+   featureset
+
+Link tables:
+
+.. autosummary::
+   :toctree: .
+
+   featureset_gene
+   featureset_protein
+   featureset_cell_marker
+
+Tracking versions & migrations:
+
+.. autosummary::
+   :toctree: .
+
+   version_zdno
+   migration_zdno
+
+Auxiliary modules:
+
+.. autosummary::
+   :toctree: .
+
+   id
+
+"""
+# This is lnschema-module zdno.
+_schema_id = "zdno"
+_migration = "267d12e6f6f1"
+__version__ = "0.4.5"
+
+from . import id  # noqa
+from ._core import (  # noqa
+    cell_marker,
+    cell_type,
+    disease,
+    featureset,
+    featureset_cell_marker,
+    featureset_gene,
+    featureset_protein,
+    gene,
+    migration_zdno,
+    protein,
+    species,
+    tissue,
+    version_zdno,
+)
