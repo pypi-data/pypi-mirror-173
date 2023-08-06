4d35f3e743e0105bc70d37d5f317a4d879afe131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:33 +0000

    Add package mahdijadaliha/0.21/mahdijadaliha-0.21.tar.gz

---
+++ packages/mahdijadaliha/mahdijadaliha-0.21.tar.gz/mahdijadaliha-0.21/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='mahdijadaliha',
+    version='0.21',
+    description='highlights keywords syntax, and use template files to generate code',
+    url='https://db2sql.com/snippy',
+    author='Mahdi Jadaliha',
+    author_email='jadaliha@gmail.com',
+    license='MIT',
+    py_modules = ['mahdijadaliha','mahdijadaliha.src','mahdijadaliha.src.template'],
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('templates', ['mahdijadaliha/data/sql_templates.sql']),
+        ('keywords', ['mahdijadaliha/data/sql_keywords.txt'])
+    ],
+    zip_safe=False)
