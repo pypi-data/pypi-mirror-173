ae2ca0a84d2eaf99845e27f28fd3b18c0168ed6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:31 +0000

    Add package kivyir/0.1.2/kivyir-0.1.2-py3-none-any.whl

---
+++ packages/kivyir/kivyir-0.1.2-py3-none-any.whl/kivyir/IrTextInput.py
@@ -0,0 +1,13 @@
+from .ConfigInput import *
+from kivy.uix.textinput import TextInput
+
+
+class IrTextInput(TextInput):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.text: self.text = cleaning(self.text)
+        self.text_language = "fa"
+        self.text_size = self.size
+
+    def on_size(self, *args):
+        self.text_size = self.size
