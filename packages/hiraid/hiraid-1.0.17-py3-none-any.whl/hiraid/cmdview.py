3e0ef9fc6d97555445481c0b87fb9879210938c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:38 +0000

    Add package hiraid/1.0.17/hiraid-1.0.17-py3-none-any.whl

---
+++ packages/hiraid/hiraid-1.0.17-py3-none-any.whl/hiraid/cmdview.py
@@ -0,0 +1,29 @@
+
+
+class Cmdview():
+    def __init__(self,cmd):
+        self.cmd = cmd
+        self.executed = False
+        self.expectedreturn = 0
+        self.returncode = None
+        self.stdout = None
+        self.stderr = None
+        self.view = {}
+        self.altview = {}
+        self.data = []
+        self.undocmds = []
+        self.undodefs = []
+        self.stats = {}
+
+class CmdviewConcurrent():
+    def __init__(self,returncode=[],stdout=[],stderr=[]):
+        self.returncode = returncode
+        self.stdout = stdout
+        self.stderr = stderr
+        self.view = {}
+        self.data = []
+        self.cmds = []
+        self.undocmds = []
+        self.undodefs = []
+        self.stats = {}
+
