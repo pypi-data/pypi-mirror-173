a081150508418c775d9c3bc697750512a2c01f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:50 +0000

    Add package SigProfilerExtractor/1.1.15/SigProfilerExtractor-1.1.15-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.15-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.15'
+version = '1.1.15'
+Update = 'Sync export_probabilities parameter with new SPA version'
+    
+    
