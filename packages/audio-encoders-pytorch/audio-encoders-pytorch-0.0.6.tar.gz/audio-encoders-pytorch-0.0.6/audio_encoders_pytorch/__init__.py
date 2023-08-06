c96d9f77227680ea1774514b13e6572eaa48b2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:52 +0000

    Add package audio-encoders-pytorch/0.0.6/audio-encoders-pytorch-0.0.6.tar.gz

---
+++ packages/audio-encoders-pytorch/audio-encoders-pytorch-0.0.6.tar.gz/audio-encoders-pytorch-0.0.6/audio_encoders_pytorch/__init__.py
@@ -0,0 +1,12 @@
+from .modules import (
+    STFT,
+    AutoEncoder1d,
+    Bottleneck,
+    Decoder1d,
+    Discriminator1d,
+    Encoder1d,
+    NoiserBottleneck,
+    STFTAutoEncoder1d,
+    TanhBottleneck,
+    VariationalBottleneck,
+)
