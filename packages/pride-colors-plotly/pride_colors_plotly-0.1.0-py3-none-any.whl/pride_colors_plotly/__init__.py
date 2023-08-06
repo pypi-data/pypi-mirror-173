292e6bd20088744994dbe2a3805498a3eef8b83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:44 +0000

    Add package pride-colors-plotly/0.1.0/pride_colors_plotly-0.1.0-py3-none-any.whl

---
+++ packages/pride-colors-plotly/pride_colors_plotly-0.1.0-py3-none-any.whl/pride_colors_plotly/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""PlotLY and matplotlib pride flag color maps and templates to make your plots proud :)"""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
