e63a71c4e8dacfe251dbff467795bdd1f23cd5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:47 +0000

    Add package nonebot-plugin-pixivbot/1.4.0rc2/nonebot_plugin_pixivbot-1.4.0rc2.tar.gz

---
+++ packages/nonebot-plugin-pixivbot/nonebot_plugin_pixivbot-1.4.0rc2.tar.gz/nonebot_plugin_pixivbot-1.4.0rc2/src/nonebot_plugin_pixivbot/__init__.py
@@ -0,0 +1,33 @@
+"""
+nonebot-plugin-pixivbot
+
+@Author         : ssttkkl
+@License        : MIT
+@GitHub         : https://github.com/ssttkkl/nonebot-plugin-pixivbot
+"""
+
+# ================= provide beans =================
+from importlib import import_module
+
+from nonebot import logger
+
+from .global_context import context
+from .nb_providers import provide
+
+provide(context)
+
+# =========== register query & service ============
+from . import query
+from . import service
+
+# ============== load custom protocol =============
+supported_modules = ["nonebot_plugin_pixivbot_onebot_v11", "nonebot_plugin_pixivbot_kook"]
+
+for p in supported_modules:
+    try:
+        import_module(p)
+        logger.success("Loaded Module: " + p)
+    except ModuleNotFoundError:
+        pass
+
+__all__ = ("context",)
