0441fe2b06489ee4a1a6da92df11cc9cf7c7cc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:45 +0000

    Add package refill/0.7/refill-0.7.tar.gz

---
+++ packages/refill/refill-0.7.tar.gz/refill-0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+install_requires = ["pyparsing>=3.0", "typing_extensions", "babel", "dataclass_utils"]
+extras_require = {
+    "pptx": ["python-pptx"],
+}
+
+setup(
+    name="refill",
+    version="0.7",
+    description="Reimagined filling of document templates",
+    license="Apache License, Version 2.0",
+    url="https://github.com/trackuity/refill",
+    packages=find_packages(exclude=("tests",)),
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    extras_require={
+        "all": list(set(sum(extras_require.values(), []))),
+        **extras_require,
+    },
+)
