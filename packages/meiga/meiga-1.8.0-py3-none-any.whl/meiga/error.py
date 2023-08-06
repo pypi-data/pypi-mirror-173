19ed136103d3bb6ec87af7c72b409e903e94b478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:19 +0000

    Add package meiga/1.8.0/meiga-1.8.0-py3-none-any.whl

---
+++ packages/meiga/meiga-1.8.0-py3-none-any.whl/meiga/error.py
@@ -0,0 +1,18 @@
+class Error(Exception):
+    def __init__(self):
+        self.message = None
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+            return True
+        return False
+
+    def __repr__(self) -> str:
+        suffix = ""
+        if hasattr(self, "message") and self.message is not None:
+            suffix = f": {self.message}"
+
+        return f"{self.__class__.__name__}{suffix}"
+
+    def __hash__(self):
+        return hash((self.message,))
