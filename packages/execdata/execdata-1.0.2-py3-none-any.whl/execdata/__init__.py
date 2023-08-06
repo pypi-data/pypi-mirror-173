f3c5028d22780f5c0b982fe01b81d261d491c25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:05 +0000

    Add package execdata/1.0.2/execdata-1.0.2-py3-none-any.whl

---
+++ packages/execdata/execdata-1.0.2-py3-none-any.whl/execdata/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: BDFD
+Date: 2021-10-27 18:42:50
+LastEditTime: 2021-10-28 10:44:23
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \6.0-PyPI_Missing_Value_Table\src\missing_value_table\__init__.py
+'''
+
+from execdata.templateproj import add_one
