355958c227123d618ccf218a1ac3b319e35ada6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:06 +0000

    Add package kiba-core/0.4.1.dev8/kiba_core-0.4.1.dev8-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.4.1.dev8-py3-none-any.whl/core/store/saver.py
@@ -0,0 +1,35 @@
+import contextlib
+from typing import AsyncIterator
+from typing import Optional
+
+from sqlalchemy.engine import ResultProxy
+from sqlalchemy.sql import ClauseElement
+
+from core.exceptions import InternalServerErrorException
+from core.store.database import Database
+from core.store.database import DatabaseConnection
+
+
+class SavingException(InternalServerErrorException):
+    pass
+
+
+class Saver:
+
+    def __init__(self, database: Database):
+        self.database = database
+
+    @contextlib.asynccontextmanager
+    async def create_transaction(self) -> AsyncIterator[DatabaseConnection]:
+        async with self.database.create_transaction() as connection:
+            yield connection
+
+    async def _execute(self, query: ClauseElement, connection: Optional[DatabaseConnection] = None) -> ResultProxy:
+        try:
+            if connection:
+                return await self.database.execute(query=query, connection=connection)
+            async with self.create_transaction() as connection:
+                return await self.database.execute(query=query, connection=connection)
+        # NOTE(krishan711): this could probs be more specific e.g. sqlalchemy.dialects.postgresql.asyncpg.IntegrityError
+        except Exception as exception:
+            raise SavingException(message=f'Error running save operation: {str(exception)}') from exception
