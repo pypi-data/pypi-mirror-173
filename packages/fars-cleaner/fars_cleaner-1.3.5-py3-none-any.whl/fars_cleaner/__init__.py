c55126073c4c23cd2a9b0328a588966f6a979f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:52 +0000

    Add package fars-cleaner/1.3.5/fars_cleaner-1.3.5-py3-none-any.whl

---
+++ packages/fars-cleaner/fars_cleaner-1.3.5-py3-none-any.whl/fars_cleaner/__init__.py
@@ -0,0 +1,7 @@
+name = 'fars_cleaner'
+__version__ = "1.3.5"
+
+from fars_cleaner.datasets import FARSFetcher
+from fars_cleaner.fars_processor import FARSProcessor
+from fars_cleaner.data_loader import load_pipeline
+#from .data_loader import pipeline
