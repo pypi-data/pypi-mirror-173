1801254495c60d88ec20fff63992d35715cf6970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:25 +0000

    Add package asciibanner/1.0/asciibanner-1.0.tar.gz

---
+++ packages/asciibanner/asciibanner-1.0.tar.gz/asciibanner-1.0/README.md
@@ -0,0 +1,36 @@
+# Asciibanner
+
+<p align="center">
+  Asciibanner a python script that allow you to do ASCII-Art with symbols
+  <br>
+  <img alt="PyPI" src="https://img.shields.io/pypi/v/asciibanner">
+  <br>
+</p>
+
+## Features
+
+ - [x] Ascii-Art with multiples build-in symbols 
+   - [x] `#`
+   - [x] `?`
+   - [x] `/`
+   - [ ] `!`
+ - [x] Ascii-Art with custom symbols
+ - [ ] Ascii-Art of animals
+
+## Installation
+
+You can now install it from pypi (latest version is <img alt="PyPI" src="https://img.shields.io/pypi/v/asciibanner">) with this command:
+
+```
+sudo python3 -m pip install asciibanner
+```
+
+## Usage
+
+```
+import asciibanner
+```
+
+## Contributing
+
+Pull requests are welcome. Feel free to open an issue if you want to add other features.
---
+++ packages/asciibanner/asciibanner-1.0.tar.gz/asciibanner-1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("DESCRIPTION.txt") as file:
+    description = file.read()
+
+with open("README.md") as file:
+    long_description = file.read()
+
+REQUIREMENTS = ['numpy', 'Pillow']
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Environment :: Console',
+    'Natural Language :: English'
+    ]
+
+setup(name='asciibanner',
+    version='1.0',
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://root-me.org',
+    author='Dicashsin',
+    author_email='dicahsin@proton.me',
+    license='MIT',
+    packages=['asciibanner'],
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS
+    )
