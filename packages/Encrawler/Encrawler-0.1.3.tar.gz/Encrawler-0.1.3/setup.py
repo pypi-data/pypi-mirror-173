87b70f169d0b67154ef2cfd08c1965c34aa98ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:29 +0000

    Add package Encrawler/0.1.3/Encrawler-0.1.3.tar.gz

---
+++ packages/Encrawler/Encrawler-0.1.3.tar.gz/Encrawler-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+
+requires = [
+    'requests>=2.27.1',
+    'loguru>=0.6.0',
+    'lxml==4.6.3',
+    'playwright>=1.27.1',
+    'pydantic==1.9.1',
+    'tqdm==4.62.2',
+]
+
+
+setup(
+    name="Encrawler",
+    version="0.1.3",
+    packages=find_packages(),
+    description="用于提供各种搜索引擎的返回结果",
+    author="phimes",
+    author_email="phimes@163.com",
+)
