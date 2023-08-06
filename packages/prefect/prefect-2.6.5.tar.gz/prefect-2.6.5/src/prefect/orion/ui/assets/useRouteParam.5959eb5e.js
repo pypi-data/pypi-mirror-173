533728e84eff3afce5ccdfec1192d5b93ff0b9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:05 +0000

    Add package prefect/2.6.5/prefect-2.6.5.tar.gz

---
+++ packages/prefect/prefect-2.6.5.tar.gz/prefect-2.6.5/src/prefect/orion/ui/assets/useRouteParam.5959eb5e.js
@@ -0,0 +1,1 @@
+import{D as u,c as o}from"./index.daad7f12.js";function m(e,t=!0){const a=u();return o(()=>{let r=a.params[e];return t&&Array.isArray(r)&&r.length&&([r]=r),r})}export{m as u};
---
+++ packages/prefect/prefect-2.6.5.tar.gz/prefect-2.6.5/src/prefect/orion/ui/assets/useToast.c2057a3e.js
@@ -0,0 +1,1 @@
+import{ae as a,a4 as n}from"./index.daad7f12.js";function u(){const o=[];return a(()=>o.forEach(s=>s.dismiss())),(...s)=>{const t=n(...s);return o.push(t),t}}export{u};
