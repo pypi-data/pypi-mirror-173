a146f7ae9df175c14e55646bb7fd839016a415e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:52 +0000

    Add package ShamirDecoder/1.0.1/ShamirDecoder-1.0.1.tar.gz

---
+++ packages/ShamirDecoder/ShamirDecoder-1.0.1.tar.gz/ShamirDecoder-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Shamir\'s Secret Sharing Decoder'
+LONG_DESCRIPTION = 'Shamir\'s Secret Sharing Decoder for educational purposes.'
+
+# Setting up
+setup(
+    name="ShamirDecoder",
+    version=VERSION,
+    author="PunGrumpy",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'shamir', 'education', 'decoder'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
