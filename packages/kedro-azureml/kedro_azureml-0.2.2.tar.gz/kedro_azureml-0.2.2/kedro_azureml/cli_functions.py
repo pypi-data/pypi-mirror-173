d0f2551fb06218ef3e0381702f6d81d47155f30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:07 +0000

    Add package kedro-azureml/0.2.2/kedro_azureml-0.2.2.tar.gz

---
+++ packages/kedro-azureml/kedro_azureml-0.2.2.tar.gz/kedro_azureml-0.2.2/kedro_azureml/cli_functions.py
@@ -0,0 +1,54 @@
+import json
+import logging
+import os
+from contextlib import contextmanager
+
+import click
+
+from kedro_azureml.generator import AzureMLPipelineGenerator
+from kedro_azureml.utils import CliContext, KedroContextManager
+
+logger = logging.getLogger()
+
+
+@contextmanager
+def get_context_and_pipeline(ctx: CliContext, image: str, pipeline: str, params: str):
+    with KedroContextManager(
+        ctx.metadata.package_name, ctx.env, parse_extra_params(params, True)
+    ) as mgr:
+        storage_account_key = os.getenv("AZURE_STORAGE_ACCOUNT_KEY", "")
+        if not storage_account_key:
+            click.echo(
+                click.style(
+                    "Environment variable AZURE_STORAGE_ACCOUNT_KEY not set, falling back to CLI prompt",
+                    fg="yellow",
+                )
+            )
+            storage_account_key = click.prompt(
+                f"Please provide Azure Storage Account Key for "
+                f"storage account {mgr.plugin_config.azure.temporary_storage.account_name}",
+                hide_input=True,
+            )
+
+        generator = AzureMLPipelineGenerator(
+            pipeline,
+            ctx.env,
+            mgr.plugin_config,
+            mgr.context.params,
+            image,
+            params,
+            storage_account_key,
+        )
+        az_pipeline = generator.generate()
+        yield mgr, az_pipeline
+
+
+def parse_extra_params(params, silent=False):
+    if params and (parameters := json.loads(params.strip("'"))):
+        if not silent:
+            click.echo(
+                f"Running with extra parameters:\n{json.dumps(parameters, indent=4)}"
+            )
+    else:
+        parameters = None
+    return parameters
