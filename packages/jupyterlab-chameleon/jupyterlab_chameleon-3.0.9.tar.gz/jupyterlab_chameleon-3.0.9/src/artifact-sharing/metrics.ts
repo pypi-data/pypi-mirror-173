ce82ad8640badcba976c493d3555307d7642bb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:53 +0000

    Add package jupyterlab-chameleon/3.0.9/jupyterlab_chameleon-3.0.9.tar.gz

---
+++ packages/jupyterlab-chameleon/jupyterlab_chameleon-3.0.9.tar.gz/jupyterlab_chameleon-3.0.9/src/artifact-sharing/metrics.ts
@@ -0,0 +1,38 @@
+import { URLExt } from '@jupyterlab/coreutils';
+import { NotebookActions } from '@jupyterlab/notebook';
+import { ServerConnection } from '@jupyterlab/services';
+
+export class CellExecutionCount {
+  constructor() {
+    NotebookActions.executed.connect(this._executed, this);
+  }
+
+  private _executed(emitter: any, context: any) {
+    alert('Cell exec!');
+    let path = context.notebook.parent.context._path.split('/');
+    alert(`PATH ${path}`);
+    if (path.length > 1) {
+      path = path[0];
+    } else {
+      path = '';
+    }
+    const body = {
+      metric: 'cell_execution_count',
+      path: path
+    };
+    ServerConnection.makeRequest(
+      Private.getUrl(this._serverSettings),
+      { method: 'PUT', body: JSON.stringify(body) },
+      this._serverSettings
+    );
+  }
+
+  private _serverSettings = ServerConnection.makeSettings();
+}
+
+namespace Private {
+  export function getUrl(settings: ServerConnection.ISettings): string {
+    const parts = [settings.baseUrl, 'chameleon', 'metrics'];
+    return URLExt.join.call(URLExt, ...parts);
+  }
+}
