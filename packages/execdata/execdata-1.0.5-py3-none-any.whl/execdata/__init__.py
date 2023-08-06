468c5e0bed7653fcc3648f15fe4a837f156db30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:27 +0000

    Add package execdata/1.0.5/execdata-1.0.5-py3-none-any.whl

---
+++ packages/execdata/execdata-1.0.5-py3-none-any.whl/execdata/__init__.py
@@ -0,0 +1,14 @@
+'''
+Date         : 2022-10-25 15:44:41
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2022-10-26 11:17:01
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from execdata.templateproj import add_one
+from execdata.preprocess import encode, split
+from execdata.model_evaluate import model_evaluate
