07935e76164e8ba50b7978113205fb236cd0e5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:08 +0000

    Add package pycolorlogs/1.1.3/pycolorlogs-1.1.3.tar.gz

---
+++ packages/pycolorlogs/pycolorlogs-1.1.3.tar.gz/pycolorlogs-1.1.3/pycolorlogs/__init__.py
@@ -0,0 +1,20 @@
+from pycolorlogs.colors import ( AscIIColors as AscIIColors,
+    ColorFormatter as ColorFormatter,
+    init_logs as init_logs
+    )
+
+from logging import (
+    # logging level constants
+    INFO      as INFO, 
+    DEBUG     as DEBUG,
+    ERROR     as ERROR, 
+    FATAL     as FATAL, 
+    CRITICAL  as CRITICAL, 
+    WARNING   as WARNING, 
+    # logging functions
+    info      as info, 
+    debug     as debug, 
+    error     as error, 
+    warning   as warning,
+    critical  as critical, 
+    exception as exception)
