a77686394d84d7970247acbcb51a3093916fc92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:08 +0000

    Add package greenhack/0.1.3/greenhack-0.1.3-py3-none-any.whl

---
+++ packages/greenhack/greenhack-0.1.3-py3-none-any.whl/greenhack/start_loop.py
@@ -0,0 +1,52 @@
+import asyncio
+import sys
+
+import greenlet
+
+from greenhack.exempt import exempt
+
+
+async def _loop(out, task):
+    try:
+        while True:
+            try:
+                result = await task()
+            except:
+                task = out.throw(*sys.exc_info())
+            else:
+                task = out.switch(result)
+    finally:
+        out.other_greenlet = None
+
+
+def start_loop():
+    """
+    Start an event loop in a new greenlet that will execute
+    the exempted coroutines.
+    """
+    current = greenlet.getcurrent()
+
+    def run(task, current=current):
+        asyncio.run(_loop(current, task))
+
+    current.other_greenlet = greenlet.greenlet(run)
+
+
+
+if __name__ == '__main__':
+    start_loop()
+
+    @exempt
+    async def sleep(x):
+        await asyncio.sleep(x)
+        return x
+
+    @exempt
+    async def download(url):
+        import httpx
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(url)
+            return len(resp.content)
+
+    print(sleep(0.5))
+    print(download('https://www.python.org/'))
