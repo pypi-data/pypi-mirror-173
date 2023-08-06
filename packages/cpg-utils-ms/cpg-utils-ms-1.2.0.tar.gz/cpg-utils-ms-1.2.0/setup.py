85950106458b2e568d5bcdc6d889cd92696f3e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:25 +0000

    Add package cpg-utils-ms/1.2.0/cpg-utils-ms-1.2.0.tar.gz

---
+++ packages/cpg-utils-ms/cpg-utils-ms-1.2.0.tar.gz/cpg-utils-ms-1.2.0/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='cpg-utils-ms',
+    # This tag is automatically updated by bumpversion
+    version='1.2.0',
+    description='Library of convenience functions specific to the CPG (MS version)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/gregsmi/cpg-utils',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'google-auth',
+        'google-cloud-secret-manager',
+        'azure-identity',
+        'azure-keyvault-secrets',
+        'azure-storage-blob',
+        'cloudpathlib[all]',
+        'toml',
+        'frozendict',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-mock',
+            'pytest-cov'
+        ],
+        'workflows': [
+            'hail',
+            'networkx',
+            'sample-metadata-ms',
+            'analysis-runner-ms',
+            'gnomad',
+            'pandas',
+            'peddy',
+            'fsspec',
+            'slack_sdk',
+            'elasticsearch == 8.*',
+            'coloredlogs',
+            'bokeh',
+            'numpy',
+            'click',
+            'elasticsearch == 8.*',
+            'slack_sdk',
+        ],
+    },
+    package_data={
+        'cpg_utils': ['config-template.toml'],
+    },
+    keywords='bioinformatics',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
