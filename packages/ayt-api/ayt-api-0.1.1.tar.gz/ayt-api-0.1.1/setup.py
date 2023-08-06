b560ae18404f6e65fe13ec559547d80c39f76b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:05 +0000

    Add package ayt-api/0.1.1/ayt-api-0.1.1.tar.gz

---
+++ packages/ayt-api/ayt-api-0.1.1.tar.gz/ayt-api-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as readme_file:
+    readme = readme_file.read()
+    readme_file.close()
+
+with open("requirements.txt", 'r') as requirements_file:
+    requirements = requirements_file.readlines()
+    requirements_file.close()
+
+setup(
+    name="ayt-api",
+    description="An Asynchronous, Object oriented python library for the YouTube api",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    author="Revnoplex",
+    author_email="revnoplex.business@protonmail.com",
+    version="0.1.1",
+    url="https://github.com/Revnoplex/ayt-api",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    setup_requires=["wheel"],
+    tests_require=['pytest>=7.1.2'],
+    test_suite='tests',
+    python_requires=">=3.7.0",
+)
