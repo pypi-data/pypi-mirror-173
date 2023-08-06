66e45e984603baaa108ae1d3d15dfcfc4f38e97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package tap-gladly/0.4.1/tap_gladly-0.4.1-py3-none-any.whl

---
+++ packages/tap-gladly/tap_gladly-0.4.1-py3-none-any.whl/tap_gladly/client.py
@@ -0,0 +1,75 @@
+"""REST client handling, including gladlyStream base class."""
+from pathlib import Path
+from typing import Any, Dict, Iterable, Optional
+
+import requests
+from singer_sdk.authenticators import BasicAuthenticator
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class gladlyStream(RESTStream):
+    """gladly stream class."""
+
+    # OR use a dynamic url_base:
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config["api_url_base"]
+
+    records_jsonpath = "$[*]"  # Or override `parse_response`.
+    next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
+
+    @property
+    def authenticator(self) -> BasicAuthenticator:
+        """Return a new authenticator object."""
+        return BasicAuthenticator.create_for_stream(
+            self,
+            username=self.config["username"],
+            password=self.config["password"],
+        )
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return headers
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """No pagination."""
+        return None
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+        return params
+
+    def prepare_request_payload(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Optional[dict]:
+        """Prepare the data payload for the REST API request.
+
+        By default, no payload will be sent (return None).
+        """
+        # TODO: Delete this method if no payload is required. (Most REST APIs.)
+        return None
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records."""
+        # TODO: Parse response body and return a set of records.
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
