43d0781127792bf664f62cbb531e9302743b0900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:59 +0000

    Add package zanshinsdk/1.3.3/zanshinsdk-1.3.3.tar.gz

---
+++ packages/zanshinsdk/zanshinsdk-1.3.3.tar.gz/zanshinsdk-1.3.3/zanshinsdk/__init__.py
@@ -0,0 +1,9 @@
+from zanshinsdk.client import Client, AlertState, AlertSeverity, ScanTargetKind, Languages, ScanTargetAWS, \
+    ScanTargetAZURE, ScanTargetGCP, ScanTargetHUAWEI, ScanTargetDOMAIN, Roles, validate_uuid
+from zanshinsdk.iterator import AbstractPersistentAlertsIterator, PersistenceEntry
+from zanshinsdk.alerts_history import FilePersistentAlertsIterator
+from zanshinsdk.following_alerts_history import FilePersistentFollowingAlertsIterator
+from zanshinsdk.version import __version__
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
