62bb72c4e1862a8bfb7956a16656bde0ef68a48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:38 +0000

    Add package regions/0.7/regions-0.7-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/regions/regions-0.7-cp311-cp311-macosx_10_9_x86_64.whl/regions/shapes/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This subpackage defines region shapes.
+"""
+from .annulus import *  # noqa
+from .circle import *  # noqa
+from .ellipse import *  # noqa
+from .line import *  # noqa
+from .point import *  # noqa
+from .polygon import *  # noqa
+from .rectangle import *  # noqa
+from .text import *  # noqa
