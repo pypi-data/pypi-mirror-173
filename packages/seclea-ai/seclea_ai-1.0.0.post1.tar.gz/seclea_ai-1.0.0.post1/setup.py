486328f5b022522d341266dafd7e126ec9e2ad93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:24 +0000

    Add package seclea-ai/1.0.0.post1/seclea_ai-1.0.0.post1.tar.gz

---
+++ packages/seclea-ai/seclea_ai-1.0.0.post1.tar.gz/seclea_ai-1.0.0.post1/setup.py
@@ -0,0 +1,40 @@
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="seclea_ai",
+    version="1.0.0-1",
+    author="octavio",
+    author_email="octavio.delser@gmail.com",
+    description="Seclea integration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seclea/seclea_ai",
+    packages=find_packages(exclude=["**test", "**example_files"]),
+    package_data={
+        "seclea_ai": [
+            "lib/seclea_utils/clib/pigz.exe",
+            "lib/seclea_utils/clib/pigz/zopfli/src/zopfli/*",
+            "lib/seclea_utils/clib/pigz/zopfli/*",
+            "lib/seclea_utils/clib/pigz/*",
+        ]
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "dill>=0.3.4",
+        "pandas>=1.3.0",
+        "pickleDB>=0.9.2",
+        "requests>=2.0.0",
+        "zstandard>=0.15.2",
+    ],
+)
