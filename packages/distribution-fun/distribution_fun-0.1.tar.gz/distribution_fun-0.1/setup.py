c69f401071a85cf25cafba15fbc377c07f5d941e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:46 +0000

    Add package distribution-fun/0.1/distribution_fun-0.1.tar.gz

---
+++ packages/distribution-fun/distribution_fun-0.1.tar.gz/distribution_fun-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distribution_fun',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_fun'],
+      author= 'Jarmila Ruzicka',
+      zip_safe=False)
