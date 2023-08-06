9c1b7640415aa209e98f083b55dbd27597dcd475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:38 +0000

    Add package plone.app.imagecropping/3.0.0/plone.app.imagecropping-3.0.0-py3-none-any.whl

---
+++ packages/plone.app.imagecropping/plone.app.imagecropping-3.0.0-py3-none-any.whl/plone/app/imagecropping/events.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from plone.app.imagecropping.interfaces import ICroppingInfoChangedEvent
+from plone.app.imagecropping.interfaces import ICroppingInfoRemovedEvent
+from zope.interface import implementer
+from zope.interface.interfaces import ObjectEvent
+
+
+@implementer(ICroppingInfoChangedEvent)
+class CroppingInfoChangedEvent(ObjectEvent):
+    """ """
+
+
+@implementer(ICroppingInfoRemovedEvent)
+class CroppingInfoRemovedEvent(ObjectEvent):
+    """ """
