48980509412ed16d853662ee9effa676479ea525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:09 +0000

    Add package SqlalchemyMixin/0.0.2/SqlalchemyMixin-0.0.2-py3-none-any.whl

---
+++ packages/SqlalchemyMixin/SqlalchemyMixin-0.0.2-py3-none-any.whl/sqlalchemy_mixins/__init__.py
@@ -0,0 +1,11 @@
+from .active_record import ActiveRecordMixin
+from .date_mixin import DateMixin
+from .image_mixin import ImageMixin
+from .smart_query import SmartQueryMixin
+from .repr_mixin import ReprMixin
+from .user_mixin import UserMixin
+
+
+class BaseMixin(ActiveRecordMixin, DateMixin, ImageMixin, SmartQueryMixin, ReprMixin):
+    __abstract__ = True
+    __repr__ = ReprMixin.__repr__
