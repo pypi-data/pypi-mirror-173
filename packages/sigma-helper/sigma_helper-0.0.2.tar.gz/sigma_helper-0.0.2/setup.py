0f5aee45b85546dc8cb12c445d4ca483743f3847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:23 +0000

    Add package sigma-helper/0.0.2/sigma_helper-0.0.2.tar.gz

---
+++ packages/sigma-helper/sigma_helper-0.0.2.tar.gz/sigma_helper-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = 'sigma_helper',
+    version = '0.0.2',
+    author = 'sigmoid',
+    author_email = 'ajitg@sigmoidanalytics.com',
+    description = 'data quality helper functions',
+    packages = ['sigma_helper'],
+    install_requires = ['datetime']
+)
---
+++ packages/sigma-helper/sigma_helper-0.0.2.tar.gz/sigma_helper-0.0.2/src/test.py
@@ -0,0 +1,3 @@
+import sigma_helper
+
+print(sigma_helper.sigma_dq_helper_generate_run_id())
