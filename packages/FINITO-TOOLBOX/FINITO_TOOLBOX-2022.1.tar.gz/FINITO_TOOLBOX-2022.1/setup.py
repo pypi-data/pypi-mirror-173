c744911c5bae542688a7e1c40d6c26e4a0f6c2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:25 +0000

    Add package FINITO-TOOLBOX/2022.1/FINITO_TOOLBOX-2022.1.tar.gz

---
+++ packages/FINITO-TOOLBOX/FINITO_TOOLBOX-2022.1.tar.gz/FINITO_TOOLBOX-2022.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'FINITO_TOOLBOX',
+    version = '2022.1',
+    author = 'Prof. Wanderlei M. Pereira Junior, Prof. Davidson de Oliveira França Junior, Prof. Marcos Rabelo Napoleão, Civil Eng. Gabriel B. Carvalho, Civil Eng. José Vitor Carvalho and Civil Eng. Murilo Carneiro Rodrigues',
+    author_email = 'wanderlei_junior@ufcat.edu.br',
+    url='https://wmpjrufg.github.io/FINITO_TOOLBOX/',
+    packages = ['FINITO_TOOLBOX']
+)
