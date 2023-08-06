9e76602e18372a99e3c195df65abed67b4aac9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:20 +0000

    Add package nekton/0.2.6rc3/nekton-0.2.6rc3-py3-none-any.whl

---
+++ packages/nekton/nekton-0.2.6rc3-py3-none-any.whl/nekton/utils/bin.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+
+from os.path import abspath
+from os.path import dirname as d
+
+parent_dir = f"{d(d(abspath(__file__)))}"
+PATH_TO_BIN = os.path.join(parent_dir, "bins/dcm2nii")
+
+
+def make_exec_bin():
+    """makes the binary executable on host PC"""
+    process = subprocess.Popen(
+        ["chmod", "+x", PATH_TO_BIN],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    process.communicate()
+
+
+def run_bin(path: str, outpath:str = None):
+    """run the binary on a given directory
+
+    Args:
+        path ([str]): directory where dicom exists
+    """
+    if outpath is None:
+        process = subprocess.Popen(
+            [PATH_TO_BIN, "-m", "y", "-z", "y", path],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        )
+    else:
+        process = subprocess.Popen(
+            [PATH_TO_BIN,"-m", "y", "-z", "y","-o", outpath, path],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        )
+    process.communicate()
