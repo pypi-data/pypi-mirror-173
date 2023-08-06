bd9df64746862d45a12cb5ed6de3817211054e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:07 +0000

    Add package doblib/0.11.0/doblib-0.11.0-py3-none-any.whl

---
+++ packages/doblib/doblib-0.11.0-py3-none-any.whl/doblib/base.py
@@ -0,0 +1,15 @@
+# © 2021-2022 Florian Kantelberg (initOS GmbH)
+# License Apache-2.0 (http://www.apache.org/licenses/).
+
+import os
+
+ODOO_CONFIG = os.path.abspath("etc/odoo.cfg")
+ADDON_PATH = "/tmp/addons"
+
+SECTION = "bootstrap"
+# Mapping of environment variables to configurations
+ENVIRONMENT = {
+    "ODOO_VERSION": ("odoo", "version"),
+    "BOOTSTRAP_MODE": (SECTION, "mode"),
+    "BOOTSTRAP_DEBUGGER": (SECTION, "debugger"),
+}
