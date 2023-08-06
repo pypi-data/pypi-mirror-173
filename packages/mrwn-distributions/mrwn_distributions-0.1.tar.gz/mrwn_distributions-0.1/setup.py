19bf44d84599c058692ffae2da05c02f081fea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:57 +0000

    Add package mrwn-distributions/0.1/mrwn_distributions-0.1.tar.gz

---
+++ packages/mrwn-distributions/mrwn_distributions-0.1.tar.gz/mrwn_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'mrwn_distributions',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['mrwn_distributions'],
+      author = 'Marwan Musa',
+      author_email = 'marwanlmusa@gmail.com',
+      zip_safe=False)
