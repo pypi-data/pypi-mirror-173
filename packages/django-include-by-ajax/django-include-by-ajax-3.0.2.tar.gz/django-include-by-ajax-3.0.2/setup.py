7ec6d47bd81cc9cd018cbe4e94699d78459d77a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:31 +0000

    Add package django-include-by-ajax/3.0.2/django-include-by-ajax-3.0.2.tar.gz

---
+++ packages/django-include-by-ajax/django-include-by-ajax-3.0.2.tar.gz/django-include-by-ajax-3.0.2/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "Django>=2.2",
+]
+
+setup(
+    author="Aidas Bendoraitis",
+    author_email="aidasbend@yahoo.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    description="A Django App Providing the `{% include_by_ajax %}` Template Tag",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="django_include_by_ajax",
+    name="django-include-by-ajax",
+    packages=find_packages(include=["include_by_ajax"]),
+    url="https://github.com/archatas/django-include-by-ajax",
+    version="3.0.2",
+    zip_safe=False,
+)
