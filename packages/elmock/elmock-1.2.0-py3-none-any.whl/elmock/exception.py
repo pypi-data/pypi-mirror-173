6563d537328a3e2d9208cb8990bd641f4b83e4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:08 +0000

    Add package elmock/1.2.0/elmock-1.2.0-py3-none-any.whl

---
+++ packages/elmock/elmock-1.2.0-py3-none-any.whl/elmock/exception.py
@@ -0,0 +1,39 @@
+class UnexpectedMethod(Exception):
+    def __init__(self, method: str = "undefined"):
+        self.message = f"Method {method} was not expected"
+
+
+class UnexpectedArguments(Exception):
+    def __init__(self, method: str = "undefined", *args, **kwargs):
+        self.message = (
+            f"Method {method} did not expected to be called with {args}, {kwargs}"
+        )
+
+
+class UnexpectedCall(Exception):
+    def __init__(
+        self,
+        method: str,
+        latest_call_id: str | None,
+        after: str | None,
+        *args,
+        **kwargs,
+    ):
+        self.message = (
+            f"Call to method {method} with arguments {args}, {kwargs} was not expected"
+        )
+        if latest_call_id != after:
+            self.message += ". Broken linked called."
+
+
+class NotFullFilled(Exception):
+    def __init__(self, data):
+        self.message = "Some calls where not full filled:\n"
+        for d in data:
+            self.message += (
+                f"- {d.method} "
+                f"  arg: {d.args}"
+                f"  kwargs: {d.kwargs}"
+                f"  was expected to happen {d.expected} "
+                f"  but was called {d.called}"
+            )
