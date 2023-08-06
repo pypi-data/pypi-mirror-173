fee154201151da90e9ff9af0878f62bb0826da18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:13 +0000

    Add package kili/2.123.0/kili-2.123.0.tar.gz

---
+++ packages/kili/kili-2.123.0.tar.gz/kili-2.123.0/src/kili/services/asset_import/types.py
@@ -0,0 +1,31 @@
+"""Import service types"""
+
+from typing import Union
+
+from typing_extensions import TypedDict
+
+
+class AssetLike(TypedDict, total=False):
+    """
+    General type of an asset obejct through the import functions
+    """
+
+    content: str
+    json_content: Union[dict, str, list]
+    external_id: str
+    status: str
+    json_metadata: Union[str, dict]
+    is_honeypot: bool
+
+
+class KiliResolverAsset(AssetLike, TypedDict, total=True):
+    """
+    Type of an asset object to be sent in Kili resolvers
+    """
+
+    content: str
+    json_content: Union[dict, str, list]
+    external_id: str
+    status: str
+    json_metadata: Union[str, dict]
+    is_honeypot: bool
