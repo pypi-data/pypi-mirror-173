31e4fd498b38b942f2612a217891e99c958c057e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:58 +0000

    Add package mizu/0.1.7/mizu-0.1.7.tar.gz

---
+++ packages/mizu/mizu-0.1.7.tar.gz/mizu-0.1.7/src/lib.rs
@@ -0,0 +1,10 @@
+use pyo3::prelude::*;
+mod parser;
+
+/// Mizu's core
+#[pymodule]
+fn mizu(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(parser::parse, m)?)?;
+    m.add_function(wrap_pyfunction!(parser::parse_ext, m)?)?;
+    Ok(())
+}
