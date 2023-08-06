55d880bec33340b5948bbef8762ee391ecf3c00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:33 +0000

    Add package nginx-static-analysis/0.1.6/nginx-static-analysis-0.1.6.tar.gz

---
+++ packages/nginx-static-analysis/nginx-static-analysis-0.1.6.tar.gz/nginx-static-analysis-0.1.6/nginx_analysis/dataclasses.py
@@ -0,0 +1,140 @@
+import re
+from pathlib import Path
+from typing import Any, Callable, Iterable, List, Optional, Tuple, TypeVar, Union
+
+from pydantic import BaseModel
+
+T = TypeVar("T")
+
+
+def compare_objects(this: T, that: T, fields: List[str]) -> bool:
+    for field in fields:
+        if not getattr(this, field) == getattr(that, field):
+            return False
+    return True
+
+
+class DirectiveFilter(BaseModel):
+    directive: str
+    value: Optional[str] = None
+
+    def match(self, line: "NginxLineConfig") -> bool:
+        if line.directive != self.directive:
+            return False
+        if self.value is None:
+            return True
+        return self.value in line.args
+
+
+class CombinedFilters(BaseModel):
+    filters: List[Union[DirectiveFilter, "CombinedFilters"]] = []
+    operator: Callable[..., bool] = any
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def match(self, line: "NginxLineConfig") -> bool:
+        matches = []
+        for f in self.filters:
+            matches.append(f.match(line))
+        return self.operator(matches)
+
+    def __add__(
+        self, other: Union[DirectiveFilter, "CombinedFilters"]
+    ) -> "CombinedFilters":
+        self.filters.append(other)
+        return self
+
+    def __iter__(self):
+        return self.filters
+
+
+CombinedFilters.update_forward_refs()
+
+
+class AnyFilter(CombinedFilters):
+    operator: Callable[[Iterable], bool] = any
+
+
+class AllFilter(CombinedFilters):
+    operator: Callable[[Iterable], bool] = all
+
+
+class NginxLineConfig(BaseModel):
+    directive: str
+    line: int
+    args: List[str]
+    file: Optional[Path]  # Only filled in after parsing
+    block: Optional[List["NginxLineConfig"]]
+    parent: Optional["NginxLineConfig"]
+
+    @property
+    def parent_blocks(self) -> List[str]:
+        if not self.parent:
+            return [self.directive]
+        return self.parent.parent_blocks + [self.directive]
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, NginxLineConfig):
+            return False
+
+        comparison_fields = ["line", "directive", "file", "args"]
+        return compare_objects(self, other, comparison_fields)
+
+    def __str__(self) -> str:
+        return f"{self.file}:{self.line}"
+
+
+class NginxFileConfig(BaseModel):
+    file: Path
+    status: str
+    errors: List[str]
+    parsed: List[NginxLineConfig]
+    included_in: Optional[Tuple["NginxFileConfig", NginxLineConfig]]
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, NginxFileConfig):
+            return False
+
+        comparison_fields = ["file", "status", "errors", "parsed"]
+        return compare_objects(self, other, comparison_fields)
+
+    def __str__(self) -> str:
+        return f"{self.file}"
+
+
+# Fixes the following error:
+# `pydantic.errors.ConfigError: field "block" not yet prepared so type is still a ForwardRef, you might need to call NginxLineConfig.update_forward_refs().`
+NginxLineConfig.update_forward_refs()
+
+
+class NginxErrorConfig(BaseModel):
+    file: Path
+    error: str
+    line: int
+
+
+class RootNginxConfig(BaseModel):
+    status: str
+    errors: List[NginxErrorConfig]
+    config: List[NginxFileConfig]
+
+    def get_files(self, file_path_regex: str) -> List[NginxFileConfig]:
+        matching_file_configs = []
+        for file_config in self.config:
+            if re.match(file_path_regex, str(file_config.file)):
+                matching_file_configs.append(file_config)
+
+        if not matching_file_configs:
+            # Wildcards are allowed to have no matches
+            if "*" not in file_path_regex:
+                raise IndexError(f"{file_path_regex} not found in config")
+
+        return matching_file_configs
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, RootNginxConfig):
+            return False
+
+        comparison_fields = ["status", "errors", "config"]
+        return compare_objects(self, other, comparison_fields)
---
+++ packages/nginx-static-analysis/nginx-static-analysis-0.1.6.tar.gz/nginx-static-analysis-0.1.6/nginx_analysis/input.py
@@ -0,0 +1,66 @@
+import sys
+from argparse import ArgumentParser, FileType
+from io import TextIOWrapper
+from typing import Iterator, List, Optional, Union
+
+from loguru import logger
+
+
+def setup_logger(verbose: bool):
+    log_level = "DEBUG" if verbose else "INFO"
+    logger.remove()
+    logger.add(sys.stderr, level=log_level, format="{message}")
+
+
+def get_args(args: Optional[List[str]] = None):
+    parser = ArgumentParser(prog="nginx-static-analysis")
+    parser.add_argument(
+        "-f", "--file", help="Root nginx file", default="/etc/nginx/nginx.conf"
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="Set verbosity level to debug", action="store_true"
+    )
+    subparsers = parser.add_subparsers(help="Commands available to run", required=True)
+
+    directive_parser = subparsers.add_parser(
+        "directive", help="Parse directive from configs", aliases=["d"]
+    )
+    directive_parser.add_argument(
+        "directives", nargs="+", help="Specify directives to look for"
+    )
+    directive_parser.add_argument(
+        "--values", nargs="*", help="Specify values to look for", default=[]
+    )
+
+    url_parser = subparsers.add_parser(
+        "url", help="Find all configs that are hit when making a request"
+    )
+    url_parser.add_argument("url", help="Url to simulate request for")
+    url_parser.add_argument(
+        "-H", "--headers", help="Header in the request. Can be used multiple times"
+    )
+
+    # Stdin logs
+    parser.add_argument(
+        "logs",
+        nargs="*",
+        default=sys.stdin,
+        type=FileType("r"),
+        help="Log file(s) to parse or stdin",
+    )
+    return parser.parse_args(args=args)
+
+
+def get_loglines(log_input: Union[List[TextIOWrapper], TextIOWrapper]) -> Iterator[str]:
+    if isinstance(log_input, list):
+        # Arg is given, not stdin
+        for f in log_input:
+            for l in f.buffer.readlines():
+                yield l.decode("utf-8", errors="ignore")
+    else:
+        while not log_input.isatty():
+            content = log_input.buffer.readline()
+            if not content:
+                break
+            line = content.decode("utf-8", errors="ignore")
+            yield line
