537b4f7e063bf573dc55eec610080b5eb17a45b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:17 +0000

    Add package civiproxy-logs2json/1.0.2/civiproxy_logs2json-1.0.2.tar.gz

---
+++ packages/civiproxy-logs2json/civiproxy_logs2json-1.0.2.tar.gz/civiproxy_logs2json-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="civiproxy_logs2json",
+    version="1.0.2",
+    author="Marc Michalsky",
+    author_email="michalsky@forumZFD.de",
+    packages=["civiproxy_logs2json"],
+    description="Translate a CiviProxy logfile into JSON format.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarcMichalsky/civiproxy_logs2json",
+    license='MIT',
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'cpl2j = civiproxy_logs2json.__main__:main',
+        ],
+    },
+)
