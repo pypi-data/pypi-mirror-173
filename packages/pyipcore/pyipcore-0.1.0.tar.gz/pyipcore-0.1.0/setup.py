91a028bf048a7288dfe2581bb7c22af1b449a52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:42 +0000

    Add package pyipcore/0.1.0/pyipcore-0.1.0.tar.gz

---
+++ packages/pyipcore/pyipcore-0.1.0.tar.gz/pyipcore-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyipcore",
+    version="0.1.0",
+    author="eaglebaby",
+    author_email="2229066748@qq.com",
+    description="pyipcore = python (pseudo) (verilog) IP core (rebuilder). Properly adjust your own. v file as an open source IP core, so that it can be easily reconfigured to new projects.",
+
+    #url="http://iswbm.com/", 
+
+    packages=find_packages(),
+
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    install_requires=['pyperclip', "files3"],
+
+    python_requires='>=3',
+
+    entry_points = {
+        'console_scripts': [
+            'iprebuild = ipg:IPRebuild',
+        ]
+    },
+    package_data={
+        'ipcore':['example.v']
+    },
+)
