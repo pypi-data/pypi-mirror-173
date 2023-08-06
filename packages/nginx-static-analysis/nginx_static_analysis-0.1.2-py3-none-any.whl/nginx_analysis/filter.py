6719ce7d705bc17fb0df4aa79ce0af6f8703287e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:12 +0000

    Add package nginx-static-analysis/0.1.2/nginx_static_analysis-0.1.2-py3-none-any.whl

---
+++ packages/nginx-static-analysis/nginx_static_analysis-0.1.2-py3-none-any.whl/nginx_analysis/filter.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+
+from nginx_analysis.dataclasses import (
+    AllFilter,
+    AnyFilter,
+    CombinedFilters,
+    DirectiveFilter,
+)
+
+
+def args_to_filter(
+    directives: List[str], values: List[Optional[str]]
+) -> CombinedFilters:
+    if len(values) > len(directives):
+        raise RuntimeError(f"Found more values than directives")
+
+    if len(directives) > len(values):
+        if len(directives) == len(values) + 1:
+            values.append(None)
+        else:
+            raise RuntimeError(f"Found more than one directive without value")
+
+    return AnyFilter(
+        filters=[
+            DirectiveFilter(directive=d, value=v) for d, v in zip(directives, values)
+        ]
+    )
+
+
+def logline_to_filters(logline: dict) -> CombinedFilters:
+    filters = AllFilter()
+    if "server_name" in logline:
+        filters += DirectiveFilter(
+            directive="server_name", value=logline["server_name"]
+        )
+    if "request" in logline:
+        try:
+            location = logline["request"].split(" ")[1]
+        except IndexError:
+            pass
+        else:
+            filters += DirectiveFilter(directive="location", value=location)
+
+    return filters
