3b934491b8fd7048f0070934fdddea9f7eaab456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:52 +0000

    Add package square-skill-api/0.0.34/square_skill_api-0.0.34.tar.gz

---
+++ packages/square-skill-api/square_skill_api-0.0.34.tar.gz/square_skill_api-0.0.34/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.34"
+
+setup(
+    name="square_skill_api",
+    version=__version__,
+    license="MIT",
+    description="",
+    url="https://github.com/UKP-SQuARE/square-skill-api",
+    download_url=f"https://github.com/UKP-SQuARE/square-skill-api/archive/refs/tags/v{__version__}.tar.gz",
+    author="UKP",
+    author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
+    packages=find_packages(
+        exclude=("tests", ".gitignore", "pytest.ini", "requirements.dev.txt")
+    ),
+    install_requires=[
+        "uvicorn>=0.15.0",
+        "fastapi>=0.65.2",
+        "pydantic>=1.8.2",
+        "numpy>=1.21.3",
+    ],
+)
