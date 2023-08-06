61e00a4b5be7a2fd3e6c46856697fc2f549f4b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:06 +0000

    Add package maria/0.0.12/maria-0.0.12.tar.gz

---
+++ packages/maria/maria-0.0.12.tar.gz/maria-0.0.12/setup.py
@@ -0,0 +1,41 @@
+from os import path
+import setuptools
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='maria',
+    version='0.0.12',
+    description="Simulates atmospheric emission for ground-based telescopes",
+    long_description=long_description,
+    author="Thomas Morris",
+    author_email='thomasmorris@princeton.edu',
+    url='https://github.com/tomachito/maria',
+    python_requires='>=3.7',
+    packages=setuptools.find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    package_data={
+        'maria': [ 'am.npy',
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+        ]
+    },
+    install_requires=['astropy',
+                      'healpy',
+                      'matplotlib',
+                      'numpy',
+                      'pandas',
+                      'scipy',
+                      'tqdm',
+                      'weathergen'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
