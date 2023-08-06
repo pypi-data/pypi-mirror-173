05555070984d439a768f97cf9e4d175ba9216c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:07 +0000

    Add package pythonping/1.1.4/pythonping-1.1.4.tar.gz

---
+++ packages/pythonping/pythonping-1.1.4.tar.gz/pythonping-1.1.4/test/test_network.py
@@ -0,0 +1,12 @@
+import unittest
+from pythonping.network import Socket
+
+class UtilsTestCase(unittest.TestCase):
+    """Tests for Socket class"""
+
+
+
+    def test_raise_explicative_error_on_name_resolution_failure(self):
+        """Test a runtime error is generated if the name cannot be resolved"""
+        with self.assertRaises(RuntimeError):
+            Socket('invalid', 'raw')
