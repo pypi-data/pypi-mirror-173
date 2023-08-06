e6a3bd5c19088275f2b09572c9e363c1aa52cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:18 +0000

    Add package microservice-utils/0.4.0/microservice-utils-0.4.0.tar.gz

---
+++ packages/microservice-utils/microservice-utils-0.4.0.tar.gz/microservice-utils-0.4.0/microservice_utils/google_cloud/adapters/cloud_run.py
@@ -0,0 +1,40 @@
+import typing
+
+from google.cloud import run_v2
+
+from microservice_utils.google_cloud.models import GcpProjectConfig
+
+
+async def get_cloud_run_urls(project: GcpProjectConfig) -> list[str]:
+    client = run_v2.ServicesAsyncClient()
+    request = run_v2.ListServicesRequest(parent=project.location_path)
+    page_result = await client.list_services(request=request)
+
+    return [response.uri async for response in page_result]
+
+
+async def get_service_url(
+    project: GcpProjectConfig,
+    matches: list[str],
+    exclude: list[str] = None,
+    url_provider: typing.Callable[
+        [GcpProjectConfig], typing.Awaitable[list[str]]
+    ] = get_cloud_run_urls,
+) -> str:
+    urls = await url_provider(project)
+    matches = [url for url in urls if all(match in url for match in matches)]
+
+    if exclude:
+        non_excluded_matches = []
+
+        for url in matches:
+            for i in exclude:
+                if i not in url:
+                    non_excluded_matches.append(url)
+
+        matches = non_excluded_matches
+
+    if len(matches) != 1:
+        raise RuntimeError(f"Expected 1 service match, got {len(matches)}")
+
+    return matches[0]
