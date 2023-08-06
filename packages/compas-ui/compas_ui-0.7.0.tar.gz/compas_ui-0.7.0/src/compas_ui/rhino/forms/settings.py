e3ce73e61e5b2f206d66392bd00c778dc2c264aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:22 +0000

    Add package compas-ui/0.7.0/compas_ui-0.7.0.tar.gz

---
+++ packages/compas-ui/compas_ui-0.7.0.tar.gz/compas_ui-0.7.0/src/compas_ui/rhino/forms/settings.py
@@ -0,0 +1,222 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import Rhino
+import Rhino.UI
+import Eto.Drawing
+import Eto.Forms
+import decimal
+from compas.colors import Color
+
+
+class CustomCell(Eto.Forms.CustomCell):
+    def OnGetIdentifier(self, args):
+        return str(args.Item.GetValue(0))
+
+    def OnCreateCell(self, args):
+        item = args.Item
+        value = item.GetValue(1)
+
+        if isinstance(value, bool):
+            control = Eto.Forms.CheckBox()
+            control.Checked = value
+
+            def on_checked(sender, e):
+                item.SetValue(1, control.Checked)
+
+            control.CheckedChanged += on_checked
+
+        elif isinstance(value, int):
+            control = Eto.Forms.NumericUpDown()
+            control.Value = value
+
+            def on_value_changed(sender, e):
+                item.SetValue(1, int(control.Value))
+
+            control.ValueChanged += on_value_changed
+
+        elif isinstance(value, float):
+            control = Eto.Forms.NumericUpDown()
+            control.Value = value
+            precision = str(value)
+            d = decimal.Decimal(precision).as_tuple()
+            control.DecimalPlaces = -d.exponent
+            control.Increment = 10**d.exponent
+
+            def on_value_changed(sender, e):
+                item.SetValue(1, float(control.Value))
+
+            control.ValueChanged += on_value_changed
+
+        elif isinstance(value, str):
+            control = Eto.Forms.TextBox()
+            control.Text = str(value)
+
+            def on_text_changed(sender, e):
+                item.SetValue(1, str(control.Text))
+
+            control.TextChanged += on_text_changed
+
+        elif isinstance(value, (Color, tuple, list)):
+            if isinstance(value, (tuple, list)):
+                value = Color(*value)
+            control = Eto.Forms.ColorPicker()
+            control.Value = Eto.Drawing.Color.FromArgb(*value.rgb255)
+
+            def on_value_changed(sender, e):
+                color = Eto.Drawing.Color(control.Value)
+                item.SetValue(1, Color(color.R, color.G, color.B))
+
+            control.ValueChanged += on_value_changed
+
+        else:
+            control = Eto.Forms.Label()
+
+        control.Size = Eto.Drawing.Size(100, 25)
+
+        return control
+
+
+class SettingsForm(Eto.Forms.Dialog[bool]):
+    def __init__(
+        self, settings, title="Settings", width=500, height=500, use_tab=False
+    ):
+        self._settings = None
+        self._names = None
+        self._values = None
+        self.settings = settings
+        self.use_tab = use_tab
+
+        self.Title = title
+        self.Padding = Eto.Drawing.Padding(0)
+        self.Resizable = True
+        self.MinimumSize = Eto.Drawing.Size(0.5 * width, 0.5 * height)
+        self.ClientSize = Eto.Drawing.Size(width, height)
+
+        layout = Eto.Forms.DynamicLayout()
+        layout.BeginVertical(
+            Eto.Drawing.Padding(0, 0, 0, 0), Eto.Drawing.Size(0, 0), True, True
+        )
+
+        if self.use_tab:
+
+            control = Eto.Forms.TabControl()
+            control.TabPosition = Eto.Forms.DockPosition.Top
+
+            self.tables = {}
+            for key, settings in self.settings.items():
+                if not isinstance(settings, dict):
+                    raise TypeError(
+                        "When use_tab is True the firt level of the settings value must be all dicts."
+                    )
+                tab = Eto.Forms.TabPage(Text=key)
+                control.Pages.Add(tab)
+                table = self.map_tree(settings)
+                tab.Content = self.tables[key] = table
+            layout.AddRow(control)
+
+        else:
+            self.table = self.map_tree(settings)
+            layout.AddRow(self.table)
+
+        layout.EndVertical()
+        layout.BeginVertical(
+            Eto.Drawing.Padding(12, 18, 12, 24), Eto.Drawing.Size(6, 0), False, False
+        )
+        layout.AddRow(None, self.ok, self.cancel)
+        layout.EndVertical()
+
+        self.Content = layout
+
+    def map_tree(self, settings):
+        """Create the items for the form."""
+        table = Eto.Forms.TreeGridView()
+        treecollection = Eto.Forms.TreeGridItemCollection()
+        table.ShowHeader = True
+        column = Eto.Forms.GridColumn()
+        column.HeaderText = "Key"
+        column.Editable = False
+        column.Sortable = True
+        column.Expand = True
+        column.DataCell = Eto.Forms.TextBoxCell(table.Columns.Count)
+        table.Columns.Add(column)
+
+        column = Eto.Forms.GridColumn()
+        column.HeaderText = "Value"
+        column.Editable = True
+        column.Sortable = False
+        column.DataCell = CustomCell()
+
+        table.Columns.Add(column)
+
+        def add_items(parent, items):
+            keys = list(items.keys())
+            keys.sort()
+            for key in keys:
+                value = items[key]
+                item = Eto.Forms.TreeGridItem(Values=(key, value))
+                if isinstance(value, dict):
+                    add_items(item.Children, value)
+                parent.Add(item)
+
+        add_items(treecollection, settings)
+        table.DataStore = treecollection
+        return table
+
+    @property
+    def ok(self):
+        self.DefaultButton = Eto.Forms.Button(Text="OK")
+        self.DefaultButton.Click += self.on_ok
+        return self.DefaultButton
+
+    @property
+    def cancel(self):
+        self.AbortButton = Eto.Forms.Button(Text="Cancel")
+        self.AbortButton.Click += self.on_cancel
+        return self.AbortButton
+
+    @property
+    def settings(self):
+        return self._settings
+
+    @settings.setter
+    def settings(self, settings):
+        self._settings = settings.copy()
+
+    @property
+    def names(self):
+        return self._names
+
+    @property
+    def values(self):
+        return self._values
+
+    def on_ok(self, sender, event):
+        try:
+
+            def set_value(items, setting):
+                for item in items:
+                    key = item.GetValue(0)
+                    value = item.GetValue(1)
+                    if isinstance(value, dict):
+                        set_value(item.Children, setting[key])
+                    else:
+                        setting[key] = value
+
+            if not self.use_tab:
+                set_value(self.table.DataStore, self.settings)
+            else:
+                for key in self.settings:
+                    set_value(self.tables[key].DataStore, self.settings[key])
+
+        except Exception as e:
+            print(e)
+            self.Close(False)
+        self.Close(True)
+
+    def on_cancel(self, sender, event):
+        self.Close(False)
+
+    def show(self):
+        return self.ShowModal(Rhino.UI.RhinoEtoApp.MainWindow)
---
+++ packages/compas-ui/compas_ui-0.7.0.tar.gz/compas_ui-0.7.0/src/compas_ui/rhino/install.py
@@ -0,0 +1,189 @@
+import os
+import sys
+import json
+from shutil import copyfile
+from subprocess import call
+
+import compas
+from compas.plugins import plugin
+import compas_rhino
+from compas_rhino.install import install as install_packages
+from compas_rhino.install import _filter_installable_packages
+from compas_rhino.install_plugin import install_plugin
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+
+
+HERE = os.path.dirname(__file__)
+
+
+@plugin(category="install", tryfirst=True)
+def installable_rhino_packages():
+    return ["compas_ui"]
+
+
+def check_folders(plugin_name, version="7.0"):
+    try:
+        print("Checking that the plugin folder exists")
+        plugin_name = "COMPAS"
+        plugin_path = os.path.join(HERE, "ui", plugin_name)
+        if not os.path.isdir(plugin_path):
+            raise Exception("Cannot find the plugin: {}".format(plugin_path))
+        print(plugin_path, "Exists")
+        print("PASSED.\n")
+
+        print("Checking that the plugin folder contains a dev folder")
+        plugin_path = os.path.abspath(plugin_path)
+        plugin_dev = os.path.join(plugin_path, "dev")
+        if not os.path.isdir(plugin_dev):
+            raise Exception("Cannot find the dev folder at: {}".format(plugin_dev))
+        print(plugin_dev, "Exists")
+        print("PASSED.\n")
+
+        print("Checking that the dev folder contains config.json")
+        plugin_config = os.path.join(plugin_dev, "config.json")
+        if not os.path.isfile(plugin_config):
+            raise Exception("Cannot find the config file at: {}".format(plugin_config))
+        print(plugin_config, "Exists")
+        print("PASSED.\n")
+
+        print("Checking that the Rhino scripts folder exists")
+        rhino_scripts_path = compas_rhino._get_rhino_scripts_path(version)
+        if not os.path.isdir(rhino_scripts_path):
+            raise Exception(
+                "Cannot find the Rhino scripts folder at: {}".format(rhino_scripts_path)
+            )
+        print(rhino_scripts_path, "Exists")
+        print("PASSED.\n")
+
+        print("Checking that user has write access to the Rhino scripts folder")
+        if not os.access(rhino_scripts_path, os.W_OK):
+            raise Exception(
+                "User does not have write access to the Rhino scripts folder at: {}".format(
+                    rhino_scripts_path
+                )
+            )
+        print("User has write access to the Rhino scripts folder")
+        print("PASSED.\n")
+
+        print("Checking that the Rhino PythonPlugins folder exists")
+        python_plugins_path = compas_rhino._get_rhino_pythonplugins_path(version)
+        if not os.path.isdir(python_plugins_path):
+            raise Exception(
+                "Cannot find the PythonPlugins folder at: {}".format(
+                    python_plugins_path
+                )
+            )
+        print(python_plugins_path, "Exists")
+        print("PASSED.\n")
+
+        print("Checking that user has write access to the Rhino PythonPlugins folder")
+        if not os.access(python_plugins_path, os.W_OK):
+            raise Exception(
+                "User does not have write access to the Rhino PythonPlugins folder at: {}".format(
+                    python_plugins_path
+                )
+            )
+        print("User has write access to the Rhino PythonPlugins folder")
+        print("PASSED.\n")
+
+        print("\n All checks passed.")
+        return True
+
+    except Exception as e:
+        print("FAILED:")
+        print(e)
+        return False
+
+
+def check_dependencies(requirements_path=None):
+    try:
+        dependencies = []
+        with open(
+            requirements_path
+            or os.path.join(HERE, "..", "..", "..", "requirements.txt"),
+            "r",
+        ) as f:
+            for line in f.readlines():
+                if line.startswith("#"):
+                    continue
+                dependencies.append(line.rstrip())
+
+        print("Required packages:")
+        print(dependencies)
+
+        pkg_resources.require(dependencies)
+
+        print("\n All checks passed.")
+        return True
+    except (DistributionNotFound, VersionConflict) as e:
+        print("FAILED:")
+        print(e)
+        return False
+
+
+def install(version="7.0"):
+    plugin_name = "COMPAS"
+    plugin_path = os.path.join(HERE, "ui", plugin_name)
+    if not os.path.isdir(plugin_path):
+        raise Exception("Cannot find the plugin: {}".format(plugin_path))
+
+    plugin_path = os.path.abspath(plugin_path)
+    plugin_dev = os.path.join(plugin_path, "dev")
+    plugin_config = os.path.join(plugin_dev, "config.json")
+
+    with open(plugin_config, "r") as f:
+        config = json.load(f)
+
+    packages = []
+    packages = _filter_installable_packages(version, packages)
+
+    if "packages" in config:
+        for name in config["packages"]:
+            if name not in packages:
+                packages.append(name)
+
+    install_packages(version=version, packages=packages)
+    install_plugin(plugin_path)
+
+    if compas.WINDOWS:
+        plugin_ruipy = os.path.join(plugin_dev, "rui.py")
+        plugin_rui = "{}.rui".format(plugin_name)
+        python_plugins_path = compas_rhino._get_rhino_pythonplugins_path(version)
+
+        call(sys.executable + " " + plugin_ruipy, shell=True)
+        copyfile(
+            os.path.join(plugin_dev, plugin_rui),
+            os.path.join(python_plugins_path, "..", "..", "UI", plugin_rui),
+        )
+
+
+def main(plugin_name, version):
+    print("=" * 20, "Checking Folders", "=" * 20)
+    if not check_folders(plugin_name, version):
+        return
+
+    # print("="*20, "Checking Dependencies", "="*20)
+    # if not check_dependencies():
+    #     return
+
+    print("=" * 20, "Running Installation", "=" * 20)
+    install(version)
+
+    print("=" * 20, "Installation Completed", "=" * 20)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="COMPAS UI installer command line utility."
+    )
+    parser.add_argument(
+        "--version",
+        default="7.0",
+        choices=["6.0", "7.0"],
+        help="Version of Rhino.",
+    )
+    args = parser.parse_args()
+    main("COMPAS", args.version)
