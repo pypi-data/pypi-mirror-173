f24c97bd3ce1af5de186b7dd458698f596d9a1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:46 +0000

    Add package crystal-toolkit/2022.10.3/crystal_toolkit-2022.10.3.tar.gz

---
+++ packages/crystal-toolkit/crystal_toolkit-2022.10.3.tar.gz/crystal_toolkit-2022.10.3/crystal_toolkit/apps/tests/test_main.py
@@ -0,0 +1,19 @@
+import time
+
+from crystal_toolkit.apps.examples.tests.typing import DashDuo
+from crystal_toolkit.apps.main import app
+
+
+def test_main_app_startup(dash_duo: DashDuo):
+    dash_duo.start_server(app)
+    # dash_duo.clear_storage()
+
+    dash_duo.wait_for_element("#StructureMoleculeComponent_title", timeout=4)
+    time.sleep(10)
+
+    dash_duo.percy_snapshot("main_app_startup-layout")
+    dash_duo.take_snapshot("main_app_startup-layout")
+
+    assert (
+        dash_duo.get_logs() == []
+    ), f"Browser console should not contain errors: {dash_duo.get_logs()}"
