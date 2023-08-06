dce07d5ea6b383834bb3270864a79438bb7858f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package super-rockket/1.0.0/super_rockket-1.0.0.tar.gz

---
+++ packages/super-rockket/super_rockket-1.0.0.tar.gz/super_rockket-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='super_rockket',
+   version='1.0.0',
+   author='Rachid el wa3er',
+   author_email='rn.benbetka@esi-sba.dz',
+   packages=['super_rockket'],
+   url='http://pypi.python.org/pypi/super_rockket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['super_rockket']
+)
