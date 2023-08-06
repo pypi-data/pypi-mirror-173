0d19571a185af9a17ceab3cd6f4b6589cc1d9f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:47 +0000

    Add package mov-cli/0.2.8/mov-cli-0.2.8.tar.gz

---
+++ packages/mov-cli/mov-cli-0.2.8.tar.gz/mov-cli-0.2.8/mov_cli/__main__.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import platform
+
+import click
+from .utils.config import config
+
+from .websites.theflix import Theflix
+from .websites.vidsrc import Vidsrc
+from .websites.eja import eja
+from .websites.trailers import trailers
+from .websites.ask4movie import Ask4Movie
+calls = {
+    "theflix": [Theflix, "https://theflix.to"],
+    "vidsrc": [Vidsrc, "https://v2.vidsrc.me"],
+    "eja" : [eja, "https://eja.tv"],
+    "trailers": [trailers, "https://trailers.to"],
+    "ask4movie": [Ask4Movie, "https://ask4movie.mx"],
+}
+
+if platform.system() == "Windows":
+    os.system("color FF")  # Fixes colour in Windows 10 CMD terminal.
+
+@click.command()
+@click.option(
+    "-p",
+    "--provider",
+    prompt=f"\ntheflix\nvidsrc\neja\ntrailers\nask4movie\n\nThe name of the provider",
+    help='The name of the provider ex: "theflix"',
+    default=f"{config.getprovider()}",
+)
+@click.option("-q", "--query", default=None, help="Your search query")
+@click.option(
+    "-r",
+    "--result",
+    default=None,
+    help="The Result Number you want to be played",
+    type=int,
+)
+def movcli(provider, query, result):  # TODO add regex
+    provider_data = calls.get(provider, calls["theflix"])
+    provider = provider_data[0](provider_data[1])
+        # provider.redo(query) if query is not None else provider.redo()
+    provider.redo(query, result)  # if result else provider.redo(query)
+
+
+if __name__ == '__main__':
+    movcli()
