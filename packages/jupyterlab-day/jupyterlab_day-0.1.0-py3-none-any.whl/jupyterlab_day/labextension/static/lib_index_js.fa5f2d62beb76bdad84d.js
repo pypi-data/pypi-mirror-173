92acb117f29cd3f7326904d3c84dad86fa24003e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:57 +0000

    Add package jupyterlab-day/0.1.0/jupyterlab_day-0.1.0-py3-none-any.whl

---
+++ packages/jupyterlab-day/jupyterlab_day-0.1.0-py3-none-any.whl/jupyterlab_day/labextension/static/lib_index_js.fa5f2d62beb76bdad84d.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkjupyterlab_day"] = self["webpackChunkjupyterlab_day"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the jupyterlab-day extension.
+ */
+const plugin = {
+    id: 'jupyterlab-day:plugin',
+    autoStart: true,
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    activate: (app, manager) => {
+        const style = 'jupyterlab-day/index.css';
+        manager.register({
+            name: 'JupyterLab Day',
+            isLight: true,
+            themeScrollbars: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.fa5f2d62beb76bdad84d.js.map
