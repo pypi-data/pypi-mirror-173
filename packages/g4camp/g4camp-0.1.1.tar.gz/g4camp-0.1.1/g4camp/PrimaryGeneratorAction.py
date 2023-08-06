ada7ffe6a6c49e9b78351d3311fb36d2903a3968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:09 +0000

    Add package g4camp/0.1.1/g4camp-0.1.1.tar.gz

---
+++ packages/g4camp/g4camp-0.1.1.tar.gz/g4camp-0.1.1/g4camp/PrimaryGeneratorAction.py
@@ -0,0 +1,21 @@
+from geant4_pybind import *
+
+class PrimaryGeneratorAction(G4VUserPrimaryGeneratorAction):
+
+  def __init__(self):
+    super().__init__()
+    nofParticles = 1
+    #self.fParticleGun = G4ParticleGun(nofParticles)
+    self.fParticleGun = G4GeneralParticleSource()
+
+    # default particle kinematic
+    #particleDefinition = G4ParticleTable.GetParticleTable().FindParticle("mu-")
+    #self.fParticleGun.SetParticleDefinition(particleDefinition)
+    #self.fParticleGun.SetParticleMomentumDirection(G4ThreeVector(0, 0, 1))
+    #self.fParticleGun.SetParticlePosition(G4ThreeVector(0, 0, 0))
+    #self.fParticleGun.SetParticleEnergy(100*GeV)
+
+
+  def GeneratePrimaries(self, anEvent):
+    # This function is called at the begining of event
+    self.fParticleGun.GeneratePrimaryVertex(anEvent)
