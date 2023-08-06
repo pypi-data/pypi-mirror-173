733e1c709d47751dda1ade251f5b4f58230da122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:32 +0000

    Add package restalker/1.2.9/restalker-1.2.9.tar.gz

---
+++ packages/restalker/restalker-1.2.9.tar.gz/restalker-1.2.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="restalker",
+    version="1.2.9",
+    description="Text analyzer package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/junquera/stalker",
+    author="Javier Junquera Sánchez",
+    author_email="javier@junquera.io",
+    license="MIT",
+    packages=["restalker"],
+    install_requires=[
+        "based58",
+        "bech32ref",
+        "bip-utils",
+        "bs4",
+        "nltk",
+        "numpy",
+        "monero",
+        "rake-nltk",
+        "web3",
+    ],
+    entry_points=dict(console_scripts=["restalker=restalker.restalker:main"]),
+)
