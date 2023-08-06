da40440ccae9de662d0d1e2a21469204e4a192b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:06 +0000

    Add package jinja2-fnmatch-extension/1.0.2/jinja2_fnmatch_extension-1.0.2-py3-none-any.whl

---
+++ packages/jinja2-fnmatch-extension/jinja2_fnmatch_extension-1.0.2-py3-none-any.whl/jinja2_fnmatch_extension/__init__.py
@@ -0,0 +1,20 @@
+import fnmatch
+import jinja2
+from jinja2.ext import Extension
+
+
+try:
+    from jinja2 import pass_eval_context as eval_context
+except ImportError:
+    from jinja2 import evalcontextfilter as eval_context
+
+@eval_context
+def _fnmatch(eval_ctx, value, pattern):
+    return fnmatch.fnmatch(value, pattern)
+
+
+class FnMatchExtension(Extension):
+
+    def __init__(self, environment):
+        super(FnMatchExtension, self).__init__(environment)
+        environment.filters['fnmatch'] = _fnmatch
