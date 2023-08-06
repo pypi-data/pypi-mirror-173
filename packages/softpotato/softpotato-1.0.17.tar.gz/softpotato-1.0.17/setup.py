9f7708a8a734a413efee23823c9cb6e0e97a876b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:25 +0000

    Add package softpotato/1.0.17/softpotato-1.0.17.tar.gz

---
+++ packages/softpotato/softpotato-1.0.17.tar.gz/softpotato-1.0.17/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, Extension
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='softpotato',
+    version="1.0.17",
+    license='GPL3',
+    author='Oliver Rodriguez',
+    author_email='oliver.rdz@softpotato.xyz',
+    description='Open source electrochemistry toolkit',
+    #long_description=long_description,
+    #long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    url='https://softpotato.xyz',
+    keywords='Electrochemistry',
+    python_requires=">=3.6",
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+    ],
+)
