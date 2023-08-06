8b9d7c8cbbd7f7aeb4797e72268ea4162612ebb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package prelude-cli/0.9.4/prelude-cli-0.9.4.tar.gz

---
+++ packages/prelude-cli/prelude-cli-0.9.4.tar.gz/prelude-cli-0.9.4/prelude_cli/templates/template.c
@@ -0,0 +1,26 @@
+/*
+NAME: $NAME
+CREATED: $CREATED
+*/
+#include <stdlib.h>
+#include <string.h>
+
+int test(void)
+{
+    char *command = "whoami";
+    return system(command);
+}
+
+int clean(void)
+{
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (strcmp(argv[1], "clean") == 0) {
+        return clean();
+    } else {
+        return test();
+    }
+}
