9cabe03bf5ea5e864951386c079a66caab5e2876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:10 +0000

    Add package iot-rdb/0.1.1/iot-rdb-0.1.1.tar.gz

---
+++ packages/iot-rdb/iot-rdb-0.1.1.tar.gz/iot-rdb-0.1.1/iot_rdb/__init__.py
@@ -0,0 +1,34 @@
+# Parsing every util class for redis connection
+from iot_rdb.util import IotRedisBasic
+from iot_rdb.util import IotRedisPublisher
+from iot_rdb.util import IotRedisSubscriber
+from iot_rdb.util import IotRedisPubSub
+
+
+class IotRedisCommon:
+    """Common variable to all containers
+
+    This standard is for defining all PubSub clients
+    in every container to be created.
+
+    class CrossConnection:
+        "container_name" = (tuple with channels to listen)
+        "container_name2" = ()
+        ...
+    }
+
+    Channels publishing now:
+        - CONTROLLER : control_input
+        - OPCUA: raw_outputs, perturbations, inputs
+        - AUTOENCODER: filtered_outputs
+        - DB:
+        - DBDRIVER:
+        - MANAGER:
+    """
+
+    class CrossConnection:
+        OPCUA = ("control_input")
+        CONTROLLER = ("raw_outputs", "filtered_outputs")
+        AUTOENCODER = ("raw_outputs", "perturbations", "inputs")
+        DB = ("raw_outputs", "filtered_outputs", "perturbations", "inputs")
+        DBDRIVER = ("raw_outputs", "filtered_outputs", "perturbations", "inputs")
