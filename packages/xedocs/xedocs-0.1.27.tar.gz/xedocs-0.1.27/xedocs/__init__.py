1eb0667660c8594f56db83bd586aca98035b28cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:17 +0000

    Add package xedocs/0.1.27/xedocs-0.1.27.tar.gz

---
+++ packages/xedocs/xedocs-0.1.27.tar.gz/xedocs-0.1.27/xedocs/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for xedocs."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = "joe.mosbacher@gmail.com"
+__version__ = "0.1.27"
+import logging
+
+logger = logging.getLogger(__name__)
+
+from ._settings import settings
+from ._frames import frames
+from . import schemas
+from .utils import *
+from .xedocs import *
+from . import api
+from .contexts import *
+
+try:
+    from . import widgets
+
+    gui = widgets.XedocsEditor()
+except ImportError:
+    logger.warning("Could not import editors, GUI not available.")
---
+++ packages/xedocs/xedocs-0.1.27.tar.gz/xedocs-0.1.27/xedocs/api.py
@@ -0,0 +1,56 @@
+import os
+import rframe
+import requests
+from requests import PreparedRequest
+from requests.auth import AuthBase
+
+from ._settings import settings
+
+
+class ApiAuth(AuthBase):
+    authenticator = None
+
+    def __init__(self, authenticator=None) -> None:
+        self.authenticator = authenticator
+
+    def __call__(self, r: PreparedRequest) -> PreparedRequest:
+        if self.authenticator is not None:
+            r.headers["Authorization"] = f"Bearer {self.authenticator.token}"
+        return r
+
+
+class RestClient(rframe.RestClient):
+    _token = None
+
+    @property
+    def token(self):
+        if self._token is None:
+            self._token = settings.get_api_token()
+        return self._token
+
+    @property
+    def headers(self):
+        if "Authorization" not in self._headers and self.token:
+            self._headers["Authorization"] = f"Bearer {self.token}"
+        return self._headers
+
+    def __init__(self, url, token=None, headers=None, client=None) -> None:
+        self.base_url = url
+        self._headers = headers if headers is not None else {}
+        self._token = token
+
+        if client is None:
+            client = requests
+        self.client = client
+
+
+def api_client(url, token=None):
+    headers = {}
+    if token is not None:
+        headers["Authorization"] = f"Bearer {token}"
+
+    client = RestClient(
+        url,
+        headers=headers,
+    )
+    return client
