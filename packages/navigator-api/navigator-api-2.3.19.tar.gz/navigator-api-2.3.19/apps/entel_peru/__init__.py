3bfc8cf5c18d3dd38d217f2e519bea3294c16559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:31 +0000

    Add package navigator-api/2.3.19/navigator-api-2.3.19.tar.gz

---
+++ packages/navigator-api/navigator-api-2.3.19.tar.gz/navigator-api-2.3.19/apps/entel_peru/__init__.py
@@ -0,0 +1,12 @@
+"""
+Program Init.
+
+entel_peru Program.
+"""
+from navigator.handlers.types import AppConfig
+
+class entel_peru(AppConfig):
+    __version__ = '0.0.1'
+    app_description = """
+    API for entel_peru
+    """
---
+++ packages/navigator-api/navigator-api-2.3.19.tar.gz/navigator-api-2.3.19/apps/places/__init__.py
@@ -0,0 +1,12 @@
+"""
+Program Init.
+
+places Program.
+"""
+from navigator.handlers.types import AppConfig
+
+class places(AppConfig):
+    __version__ = '0.0.1'
+    app_description = """
+    API for places
+    """
