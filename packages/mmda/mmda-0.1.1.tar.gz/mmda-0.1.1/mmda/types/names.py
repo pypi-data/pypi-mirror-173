0a4e9a39e2d883b82200f5f57f1f6c3c1b76e55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:39 +0000

    Add package mmda/0.1.1/mmda-0.1.1.tar.gz

---
+++ packages/mmda/mmda-0.1.1.tar.gz/mmda-0.1.1/mmda/types/names.py
@@ -0,0 +1,18 @@
+"""
+
+Names of fields, as strings
+
+@kylel
+
+"""
+
+# document field names
+Symbols = 'symbols'
+Images = 'images'
+
+Pages = 'pages'
+Tokens = 'tokens'
+Rows = 'rows'
+Sentences = 'sents'
+Blocks = 'blocks'
+Words = 'words'
