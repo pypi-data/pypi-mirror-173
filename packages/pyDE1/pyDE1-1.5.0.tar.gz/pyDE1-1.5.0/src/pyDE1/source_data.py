3dbc80c872db8832715497cb963936286bc9aec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:11 +0000

    Add package pyDE1/1.5.0/pyDE1-1.5.0.tar.gz

---
+++ packages/pyDE1/pyDE1-1.5.0.tar.gz/pyDE1-1.5.0/src/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "965eddb",
+    "subject": "MBD: remove logging of last_sent in set_address",
+    "ref_names": "HEAD -> devel-2022-10",
+    "author_date": "Mon Oct 24 20:36:29 2022 -0700",
+    "commit_date": "Mon Oct 24 20:36:29 2022 -0700",
+}
+source_data = {
+    "git": git_data,
+}
