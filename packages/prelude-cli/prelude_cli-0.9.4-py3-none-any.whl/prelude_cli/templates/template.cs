bec18e3263401563174aac93261ba67f90715d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package prelude-cli/0.9.4/prelude_cli-0.9.4-py3-none-any.whl

---
+++ packages/prelude-cli/prelude_cli-0.9.4-py3-none-any.whl/prelude_cli/templates/template.cs
@@ -0,0 +1,25 @@
+/*
+NAME: $NAME
+CREATED: $CREATED
+*/
+using System;
+
+class TTP {
+    static int Test() {
+        Console.WriteLine("Testing");
+        return 0;
+    }
+
+    static int Clean() {
+        Console.WriteLine("Clean");
+        return 0;
+    }
+
+    static void Main(string[] args) {
+        if (args[0].Contains("clean")) {
+            Clean();
+        } else {
+            Test();
+        }
+    }
+}
