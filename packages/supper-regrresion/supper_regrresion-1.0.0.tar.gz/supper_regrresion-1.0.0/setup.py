a35d36fa0b01fc66bab9259366f807fbb5689338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:04 +0000

    Add package supper-regrresion/1.0.0/supper_regrresion-1.0.0.tar.gz

---
+++ packages/supper-regrresion/supper_regrresion-1.0.0.tar.gz/supper_regrresion-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='supper_regrresion',
+   version='1.0.0',
+   author='Rachid el wa3er',
+   author_email='rn.benbetka@esi-sba.dz',
+   packages=['supper_regrresion'],
+   url='http://pypi.python.org/pypi/supper_regrresion/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[]
+)
