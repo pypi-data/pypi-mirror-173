a31ea94f65f6179858c18f752141259fdc9aef5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:33:20 +0000

    Add package NORDic/1.0.6/NORDic-1.0.6.tar.gz

---
+++ packages/NORDic/NORDic-1.0.6.tar.gz/NORDic-1.0.6/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import atexit
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import check_call
+
+NAME = "NORDic"
+VERSION = "1.0.6"
+
+setup(name=NAME,
+    version=VERSION,
+    author="Clémence Réda",
+    author_email="clemence.reda@inserm.fr",
+    url="https://github.com/clreda/NORDic",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords='',
+    description="Network Oriented Repurposing of Drugs (NORDic): network identification and master regulator detection",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={'':"src"},
+    python_requires='~=3.8.5',
+    install_requires=[
+        "clingo==5.6.1",
+        "graphviz==0.20.1",
+        "bonesis==0.4.91",
+        "matplotlib==3.3.4",
+        "scikit_learn==1.1.2",
+        "scipy==1.6.2",
+        "qnorm==0.5.1",
+        "tqdm==4.62.3",
+        "cmapPy==4.0.1",
+	"mpbn-sim==0.1"
+    ],
+    entry_points={},
+)
