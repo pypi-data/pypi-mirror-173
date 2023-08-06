e79445740a9380c3e1932d46c055fb75bfb148de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:52 +0000

    Add package atc-dataplatform/1.1.10/atc_dataplatform-1.1.10-py3-none-any.whl

---
+++ packages/atc-dataplatform/atc_dataplatform-1.1.10-py3-none-any.whl/atc/atc_exceptions.py
@@ -0,0 +1,20 @@
+class AtcException(Exception):
+    pass
+
+
+class NoTableException(AtcException):
+    value = "No table found!"
+    pass
+
+
+class UnkownPathException(AtcException):
+    value = "Something went wrong during reading of path!"
+    pass
+
+
+class ColumnDoesNotExistException(AtcException):
+    pass
+
+
+class MoreThanTwoDataFramesException(AtcException):
+    pass
