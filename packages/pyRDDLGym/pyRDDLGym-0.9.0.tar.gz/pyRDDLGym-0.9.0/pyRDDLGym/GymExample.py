fc656ab0fc52cb2d080ed63e17e919194fa4ec49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:18 +0000

    Add package pyRDDLGym/0.9.0/pyRDDLGym-0.9.0.tar.gz

---
+++ packages/pyRDDLGym/pyRDDLGym-0.9.0.tar.gz/pyRDDLGym-0.9.0/pyRDDLGym/GymExample.py
@@ -0,0 +1,53 @@
+from pyRDDLGym import RDDLEnv
+from pyRDDLGym import ExampleManager
+from pyRDDLGym.Policies.Agents import RandomAgent
+
+# ENV = 'PowerGeneration'
+# ENV = 'MarsRover'
+# ENV = 'UAV continuous'
+# ENV = 'UAV discrete'
+# ENV = 'UAV mixed'
+# ENV = 'Wildfire'
+# ENV = 'MountainCar'
+# ENV = 'CartPole continuous'
+# ENV = 'CartPole discrete'
+# ENV = 'Elevators'
+ENV = 'RecSim'
+# ENV = 'RaceCar'
+
+def main():
+    # get the environment info
+    EnvInfo = ExampleManager.GetEnvInfo(ENV)
+    # set up the environment class, choose instance 0 because every example has at least one example instance
+    myEnv = RDDLEnv.RDDLEnv(domain=EnvInfo.get_domain(), instance=EnvInfo.get_instance(0))
+    # set up the environment visualizer
+    myEnv.set_visualizer(EnvInfo.get_visualizer())
+
+    # set up an example aget
+    agent = RandomAgent(action_space=myEnv.action_space, num_actions=myEnv.NumConcurrentActions)
+
+    total_reward = 0
+    state = myEnv.reset()
+    for step in range(myEnv.horizon):
+        myEnv.render()
+        action = agent.sample_action()
+        next_state, reward, done, info = myEnv.step(action)
+        total_reward += reward
+        print()
+        print('step       = {}'.format(step))
+        print('state      = {}'.format(state))
+        print('action     = {}'.format(action))
+        print('next state = {}'.format(next_state))
+        print('reward     = {}'.format(reward))
+        state = next_state
+        if done:
+            break
+    print("episode ended with reward {}".format(total_reward))
+    myEnv.close()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
