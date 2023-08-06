f49fc6de46f815afb58c85f81439ec20dd0e26c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:54 +0000

    Add package salamanderspace/0.0.6/salamanderspace-0.0.6.tar.gz

---
+++ packages/salamanderspace/salamanderspace-0.0.6.tar.gz/salamanderspace-0.0.6/src/salamanderspace/__init__.py
@@ -0,0 +1,31 @@
+"""
+Name: Jason Saladiner Jr.
+Date Created: 04/28/2022
+Last Updated: 04/28/2022
+Purpose: 
+General Assumptions:
+
+Included Functions/Submodules:
+	
+"""
+##Documentation
+## In command line:
+
+##Doesn't work bc webbrowser wont open chrome#### To create documentation html files
+##Doesn't work bc webbrowser wont open chrome#### > pyton -m pdoc --html -o SalamanderSpace\Documentation\html SalamanderSpace
+
+## To create local host 
+### > pip install pdoc3
+### > python -m pdoc --http : SalamanderSpace
+## Go to chrome and type in search bar : "localhost:8080"
+
+import FileIO, Shocks, Tables, vlm
+
+__all__ = ["FileIO", "Shocks", "Tables", "vlm"]
+
+
+
+
+
+
+
