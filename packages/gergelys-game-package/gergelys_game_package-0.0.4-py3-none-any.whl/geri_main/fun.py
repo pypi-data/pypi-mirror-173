a3ac1e39bc448a4a15edbdea9387f5b6cd939022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:47 +0000

    Add package gergelys-game-package/0.0.4/gergelys_game_package-0.0.4-py3-none-any.whl

---
+++ packages/gergelys-game-package/gergelys_game_package-0.0.4-py3-none-any.whl/geri_main/fun.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+subprocess.check_call([sys.executable, "-m", "pip", "install", "GitPython"])
+
+print("Git package installed")
+
+from git import Repo
+
+def print_my_name():
+    print("My name is Geri, I made this package")
+
+def simple_addition(a=5,b=4):
+    return a+b
+
+def run_game():
+    Repo.clone_from("https://github.com/gregoryhornyak/python_package_test.git","/py_game")
+    os.startfile("C:/py_game/game/SortTheCourt.exe")
