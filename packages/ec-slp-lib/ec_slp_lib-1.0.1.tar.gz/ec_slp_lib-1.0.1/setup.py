2bbce7240cb224c672e0d8b4cf24a955faeb8085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:43 +0000

    Add package ec-slp-lib/1.0.1/ec_slp_lib-1.0.1.tar.gz

---
+++ packages/ec-slp-lib/ec_slp_lib-1.0.1.tar.gz/ec_slp_lib-1.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['ec_slp_lib']
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'ec-slp-lib',
+    'version': '1.0.1',
+    'description': 'Wrapper for Electron Cash commands based on its CLI and RPC commands',
+    'long_description': None,
+    'author': 'uak',
+    'author_email': '4626956-uak@users.noreply.gitlab.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/uak/electron-cash-slp-basic-lib',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
