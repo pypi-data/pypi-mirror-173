18a09df5eb71c5c8d2d11efa709987d4bc25ccaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:37 +0000

    Add package frankfnl-probability/0.1/frankfnl_probability-0.1.tar.gz

---
+++ packages/frankfnl-probability/frankfnl_probability-0.1.tar.gz/frankfnl_probability-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='frankfnl_probability',
+    version='0.1',
+    description='Gaussian and Binomial distributions',
+    packages=['frankfnl_probability'],
+    author = 'Francisco Nava',
+    author_email = 'f.nava.morales92@gmail.com',
+    zip_safe = False
+)
