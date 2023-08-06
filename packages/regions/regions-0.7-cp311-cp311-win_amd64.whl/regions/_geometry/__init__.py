9730fef139f1ed72f5fefcbbede692a3ea2cd099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:38 +0000

    Add package regions/0.7/regions-0.7-cp311-cp311-win_amd64.whl

---
+++ packages/regions/regions-0.7-cp311-cp311-win_amd64.whl/regions/_geometry/__init__.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Geometry subpackage for low-level geometry functions.
+"""
+
+from .circular_overlap import *  # noqa
+from .elliptical_overlap import *  # noqa
+from .rectangular_overlap import *  # noqa
+from .polygonal_overlap import *  # noqa
---
+++ packages/regions/regions-0.7-cp311-cp311-win_amd64.whl/regions/core/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This subpackage provides the core functionality for regions.
+"""
+
+from .attributes import *  # noqa
+from .bounding_box import *  # noqa
+from .compound import *  # noqa
+from .core import *  # noqa
+from .mask import *  # noqa
+from .metadata import *  # noqa
+from .pixcoord import *  # noqa
+from .regions import *  # noqa
