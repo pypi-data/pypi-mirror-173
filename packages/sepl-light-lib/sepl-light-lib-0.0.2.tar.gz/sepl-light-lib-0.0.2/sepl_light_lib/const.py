14f8d4eb71d6b9f190db0d71f461a57a34487049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:14 +0000

    Add package sepl-light-lib/0.0.2/sepl-light-lib-0.0.2.tar.gz

---
+++ packages/sepl-light-lib/sepl-light-lib-0.0.2.tar.gz/sepl-light-lib-0.0.2/sepl_light_lib/const.py
@@ -0,0 +1,18 @@
+from math import pi as _pi
+
+m = 1.0
+cm = 1e-2 * m
+mm = 1e-3 * m
+um = 1e-6 * m
+nm = 1e-9 * m
+
+rad = 1.0
+mrad = 1e-3 * rad
+urad = 1e-6 * rad
+
+deg = (2 * _pi * rad) / 360
+
+W = 1.0
+mW = 1e-3 * W
+
+pi = _pi
