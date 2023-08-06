33e3d56e7c66c538835ef39377eca0301dddf854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:44 +0000

    Add package edc-glucose/0.1.24/edc-glucose-0.1.24.tar.gz

---
+++ packages/edc-glucose/edc-glucose-0.1.24.tar.gz/edc-glucose-0.1.24/edc_glucose/form_validators/glucose_form_validator_mixin.py
@@ -0,0 +1,13 @@
+from edc_constants.constants import YES
+
+from ..utils import validate_glucose_as_millimoles_per_liter
+
+
+class GlucoseFormValidatorMixin:
+    def validate_glucose_test(self) -> None:
+        self.required_if(YES, field="glucose_performed", field_required="glucose_date")
+        self.required_if(YES, field="glucose_performed", field_required="fasting")
+        self.required_if(YES, field="glucose_performed", field_required="glucose_value")
+        validate_glucose_as_millimoles_per_liter("glucose", self.cleaned_data)
+        self.required_if(YES, field="glucose_performed", field_required="glucose_quantifier")
+        self.required_if(YES, field="glucose_performed", field_required="glucose_units")
