9f48a1722dc629af074fc2e69cbc5f6b679bcca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:19 +0000

    Add package learning-loop-node/0.7.36/learning_loop_node-0.7.36.tar.gz

---
+++ packages/learning-loop-node/learning_loop_node-0.7.36.tar.gz/learning_loop_node-0.7.36/learning_loop_node/trainer/training.py
@@ -0,0 +1,27 @@
+from learning_loop_node.trainer.training_data import TrainingData
+from pydantic import BaseModel
+from typing import Optional
+from learning_loop_node.context import Context
+
+
+class Training(BaseModel):
+    base_model_id: Optional[str]
+    id: str
+    context: Context
+
+    project_folder: str
+    images_folder: str
+
+    training_folder: Optional[str]
+
+    data: Optional[TrainingData]
+
+    training_number: Optional[int]
+
+
+class TrainingOut(BaseModel):
+    confusion_matrix: Optional[dict]
+    train_image_count: Optional[int]
+    test_image_count: Optional[int]
+    trainer_id: Optional[str]
+    hyperparameters: Optional[dict]
