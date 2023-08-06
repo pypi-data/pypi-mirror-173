2bca0127d214037d0a6d87da8f8954ecf6f2d6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package mealpy/2.5.1/mealpy-2.5.1.tar.gz

---
+++ packages/mealpy/mealpy-2.5.1.tar.gz/mealpy-2.5.1/mealpy/human_based/HBO.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 00:47, 16/10/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.sciencedirect.com/science/article/abs/pii/S0957417420305261#!
+# https://github.com/qamar-askari/HBO/blob/master/HBO.m
