31b935ca56fd3fd94ab8a84be2aa5315b4ca7d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package square-skill-helpers/0.0.9/square_skill_helpers-0.0.9.tar.gz

---
+++ packages/square-skill-helpers/square_skill_helpers-0.0.9.tar.gz/square_skill_helpers-0.0.9/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+__version__ = "0.0.9"
+
+setup(
+    name="square_skill_helpers",
+    version=__version__,
+    license="MIT",
+    description="",
+    url="https://github.com/UKP-SQuARE/square-skill-helpers",
+    download_url=f"https://github.com/UKP-SQuARE/square-skill-helpers/archive/refs/tags/v{__version__}.tar.gz",
+    author="UKP",
+    author_email="baumgaertner@ukp.informatik.tu-darmstadt.de",
+    packages=find_packages(exclude=(".gitignore", "tests")),
+    install_requires=[
+        "requests==2.26.0",
+        "numpy==1.21.3",
+        "square-auth>=0.0.3",
+        "aiohttp>=3.8.1"
+    ],
+)
