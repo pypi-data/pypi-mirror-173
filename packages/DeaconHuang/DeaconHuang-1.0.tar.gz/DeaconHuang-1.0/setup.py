0069139b3e46d0322f505370c5ac13250000f5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:47 +0000

    Add package DeaconHuang/1.0/DeaconHuang-1.0.tar.gz

---
+++ packages/DeaconHuang/DeaconHuang-1.0.tar.gz/DeaconHuang-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="DeaconHuang",
+    version="1.0",
+    author="Deacon",
+    author_email="deacon.huang@163.com",
+    description="This project provides powerful functions",
+    long_description="DeaconHuang",
+    long_description_content_type="text/plain; charset=UTF-8",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
