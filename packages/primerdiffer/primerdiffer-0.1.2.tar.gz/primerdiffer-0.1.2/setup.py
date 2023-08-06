88139a9485cb18b6cabf22575e9095060980dbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:27 +0000

    Add package primerdiffer/0.1.2/primerdiffer-0.1.2.tar.gz

---
+++ packages/primerdiffer/primerdiffer-0.1.2.tar.gz/primerdiffer-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from primerdiffer import __version__
+
+setup(
+    name='primerdiffer',
+    version=__version__,
+    packages=['', "primerdiffer", "bin"],
+    url='https://github.com/Runsheng/primerdiffer',
+    license='GPL-2',
+    author='runsheng',
+    author_email='runsheng.lee@gmail.com',
+    description='batch design of primers for genotyping two or more haplotypes in hybrids',
+    install_requires = ["primer3-py>=0.6.1",
+                        "biopython>=1.78"],
+    scripts = ['bin/primerdesign.py',
+               'bin/ispcr.py'
+                ]
+)
+
