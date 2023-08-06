1d9845f3668344e7fdef58e961fbd289bb495642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:11 +0000

    Add package opendatalab/0.0.2/opendatalab-0.0.2-py3.8.egg

---
+++ packages/opendatalab/opendatalab-0.0.2-py3.8.egg/opendatalab/__version__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Shanghai AI Lab. Licensed under MIT License.
+#
+# from datetime import date
+
+
+"""OpenDataLab python SDK version info."""
+
+__url__ = "http://opendatalab-test.shlab.tech" # "https://opendatalab.com"
+__version__ = "0.0.2"
+__svc__ = '1.8'
+odl_clientId = "ypkl8bwo0eb5ao1b96no"  # "kmz3bkwzlaa3wrq8pvwa"
+uaa_url_prefix = "http://uaa-staging.mai4u.com/gw/uaa-be"  # "https://sso.openxlab.org.cn/gw/uaa-be"
