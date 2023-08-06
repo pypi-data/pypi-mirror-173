9da9638af385960fff9180bbe9149e2fd311a9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:25 +0000

    Add package datajuggler/0.4.10.4/datajuggler-0.4.10.4.tar.gz

---
+++ packages/datajuggler/datajuggler-0.4.10.4.tar.gz/datajuggler-0.4.10.4/datajuggler/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from datajuggler.core import aDict, uDict, iList
+from datajuggler.keys import Keylist, Keypath
+
+from datajuggler.utils import (
+    StrCase, urange, omit_values, replace_values,
+    split_chunks, add_df, df_compare, rename_duplicates,
+)
+from datajuggler.strings import (
+    searchstr, substr, is_match_string, remove_accents,
+)
+
+__all__ = [
+    "aDict",
+    "uDict",
+    "iList",
+    "Keylist",
+    "Keypath",
+    "StrCase",
+    "urange",
+    "omit_values",
+    "replace_values",
+    "add_df",
+    "df_compare",
+    "split_chunks",
+    "rename_duplicates",
+    "searchstr",
+    "substr",
+    "is_match_string",
+    "remove_accents",
+]
