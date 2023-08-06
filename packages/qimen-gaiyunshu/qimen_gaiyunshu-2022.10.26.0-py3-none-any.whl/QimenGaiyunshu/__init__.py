8451ad57f9b525959efd7ffdb4553deba1769866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:25 +0000

    Add package qimen-gaiyunshu/2022.10.26.0/qimen_gaiyunshu-2022.10.26.0-py3-none-any.whl

---
+++ packages/qimen-gaiyunshu/qimen_gaiyunshu-2022.10.26.0-py3-none-any.whl/QimenGaiyunshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-gaiyunshu
+https://github.com/apachecn/qimen-gaiyunshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.26.0"
