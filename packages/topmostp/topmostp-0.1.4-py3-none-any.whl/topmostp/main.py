6b25d0d3df43f4e847b5177f792bed5f64bf7b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:36 +0000

    Add package topmostp/0.1.4/topmostp-0.1.4-py3-none-any.whl

---
+++ packages/topmostp/topmostp-0.1.4-py3-none-any.whl/topmostp/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import typer
+import topmostp.helper as helper
+
+app = typer.Typer(add_completion=False)
+
+def intro():
+    helper.banner()
+    helper.check_config_folder()
+
+@app.command()
+def update():
+    """
+        Update ports list
+    """
+    intro()
+    helper.update_ports()
+
+@app.command()
+def all(n: int, silent: bool = typer.Option(False, "--silent")):
+    """
+        Retrieve list of all ports (both TCP and UDP) by frequency
+    """
+    if not silent:
+        intro()
+    ports = helper.get_ports(n, tcp=True, udp=True)
+    print(",".join(ports))
+
+@app.command()
+def tcp(n: int, silent: bool = typer.Option(False, "--silent")):
+    """
+        Retrieve list of TCP ports by frequency
+    """
+    if not silent:
+        intro()
+    ports = helper.get_ports(n, tcp=True, udp=False)
+    print(",".join(ports))
+
+@app.command()
+def udp(n: int, silent: bool = typer.Option(False, "--silent")):
+    """
+        Retrieve list of UDP ports by frequency
+    """
+    if not silent:
+        intro()
+    ports = helper.get_ports(n, tcp=False, udp=True)
+    print(",".join(ports))
+
+@app.command()
+def stats(port: int, port_type: helper.PortTypeArg):
+    """
+        Retrieve stats about a port
+    """
+    intro()
+    helper.port_info(port, port_type.value)
+
+@app.command()
+def find(service: str):
+    """
+        Find info about about a specific service
+    """
+    intro()
+    helper.find_services(service)
