27fcef4b5d32f4e7fd68f4b6d9da0360bc880f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:27 +0000

    Add package seamm-installer/2022.10.25/seamm_installer-2022.10.25-py2.py3-none-any.whl

---
+++ packages/seamm-installer/seamm_installer-2022.10.25-py2.py3-none-any.whl/seamm_installer/metadata.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+"""Metadata about packages, etc."""
+
+core_packages = (
+    "molsystem",
+    "reference-handler",
+    "seamm",
+    "seamm-dashboard",
+    "seamm-datastore",
+    "seamm-ff-util",
+    "seamm-installer",
+    "seamm-jobserver",
+    "seamm-util",
+    "seamm-widgets",
+)
+molssi_plug_ins = (
+    "control-parameters-step",
+    "crystal-builder-step",
+    "custom-step",
+    "dftbplus-step",
+    "forcefield-step",
+    "from-smiles-step",
+    "lammps-step",
+    "loop-step",
+    "mopac-step",
+    "packmol-step",
+    "psi4-step",
+    "quickmin-step",
+    "rdkit-step",
+    "read-structure-step",
+    "set-cell-step",
+    "supercell-step",
+    "table-step",
+)
+external_plug_ins = []
+
+excluded_plug_ins = (
+    "chemical-formula",
+    "cms-plots",
+    "seamm-dashboard-client",
+    "seamm-cookiecutter",
+    "cassandra-step",
+    "solvate-step",
+)
+development_packages = (
+    "black",
+    "codecov",
+    "flake8",
+    "nodejs",
+    "pytest",
+    "pytest-cov",
+    "pygments",
+    "sphinx",
+    "twine",
+    "watchdog",
+)
+development_packages_pip = (
+    "build",
+    "rinohtype",
+    "seamm-cookiecutter",
+    "sphinx-rtd-theme",
+    "pystemmer",
+)
