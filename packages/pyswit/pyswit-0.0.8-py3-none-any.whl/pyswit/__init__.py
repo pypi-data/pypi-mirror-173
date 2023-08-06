95fb1712a45197b6559a336cade9b85d6f7b3239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:24 +0000

    Add package pyswit/0.0.8/pyswit-0.0.8-py3-none-any.whl

---
+++ packages/pyswit/pyswit-0.0.8-py3-none-any.whl/pyswit/__init__.py
@@ -0,0 +1,20 @@
+from pyswit.user import User
+from pyswit.workspace import Workspace
+from pyswit.channel import Channel
+from pyswit.message import Message
+from pyswit.project import Project
+from pyswit.task import Task
+
+__version__ = "0.0.8"
+
+
+class Pyswit:
+    def __init__(self, access_token: str):
+        api_args = {"access_token": access_token}
+
+        self.user = User(**api_args)
+        self.workspace = Workspace(**api_args)
+        self.message = Message(**api_args)
+        self.channel = Channel(**api_args)
+        self.project = Project(**api_args)
+        self.task = Task(**api_args)
