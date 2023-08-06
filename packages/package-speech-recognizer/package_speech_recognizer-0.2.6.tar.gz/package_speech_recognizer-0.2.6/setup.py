780d49ca861527de7c6e38991065f4ecc20e80a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:22 +0000

    Add package package-speech-recognizer/0.2.6/package_speech_recognizer-0.2.6.tar.gz

---
+++ packages/package-speech-recognizer/package_speech_recognizer-0.2.6.tar.gz/package_speech_recognizer-0.2.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "package_speech_recognizer",
+version="0.2.6",
+description="In this package you can find the easiest way to create a speech recogniser including background recognizing solution",
+author="Ankit Biswas",
+packages=['package_speech_recognizer'],
+install_requires=['SpeechRecognition', 'pyttsx3'],
+)
