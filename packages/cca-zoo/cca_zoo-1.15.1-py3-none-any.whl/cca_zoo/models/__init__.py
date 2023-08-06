2affff0c6b97bd4c6d1dab0550e9b9280e968582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:43 +0000

    Add package cca-zoo/1.15.1/cca_zoo-1.15.1-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.15.1-py3-none-any.whl/cca_zoo/models/__init__.py
@@ -0,0 +1,49 @@
+from ._grcca import GRCCA
+from ._iterative import (
+    PLS_ALS,
+    SCCA_PMD,
+    ElasticCCA,
+    SCCA_Parkhomenko,
+    SCCA_IPLS,
+    SCCA_ADMM,
+    SCCA_Span,
+    SWCCA,
+)
+from ._multiview import MCCA, KCCA, GCCA, KGCCA, TCCA, KTCCA
+from ._ncca import NCCA
+from ._partialcca import PartialCCA
+from ._prcca import PRCCA
+from ._rcca import rCCA, CCA, PLS
+
+__all__ = [
+    "GCCA",
+    "KGCCA",
+    "PLS_ALS",
+    "SCCA_PMD",
+    "ElasticCCA",
+    "SCCA_Parkhomenko",
+    "SCCA_IPLS",
+    "SCCA_ADMM",
+    "SCCA_Span",
+    "SWCCA",
+    "MCCA",
+    "KCCA",
+    "NCCA",
+    "PartialCCA",
+    "rCCA",
+    "CCA",
+    "PLS",
+    "TCCA",
+    "KTCCA",
+    "PRCCA",
+    "GRCCA"
+]
+
+try:
+    from ._stochastic import StochasticPowerPLS, IncrementalPLS
+
+    __all__.extend("StochasticPowerPLS", "IncrementalPLS")
+except:
+    pass
+
+classes = __all__
