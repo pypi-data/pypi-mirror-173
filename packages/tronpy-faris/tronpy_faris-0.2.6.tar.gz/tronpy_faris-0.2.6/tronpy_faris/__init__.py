e860b21b32f41588c6509cf495a30a9446b6f39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:28 +0000

    Add package tronpy-faris/0.2.6/tronpy_faris-0.2.6.tar.gz

---
+++ packages/tronpy-faris/tronpy_faris-0.2.6.tar.gz/tronpy_faris-0.2.6/tronpy_faris/__init__.py
@@ -0,0 +1,7 @@
+from tronpy_faris.tron import Tron
+from tronpy_faris.async_tron import AsyncTron
+from tronpy_faris.contract import Contract, ShieldedTRC20
+from tronpy_faris.async_contract import AsyncContract
+
+TRX = 1_000_000
+SUN = 1
