4286cdeabcab362726d8ae93ce05408880694726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:16 +0000

    Add package diclass/1.3.0/diclass-1.3.0.tar.gz

---
+++ packages/diclass/diclass-1.3.0.tar.gz/diclass-1.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="diclass",
+    version="1.3.0",
+    author="Moses Dastmard",
+    description="converts python dictoinary to class object",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
