3707a7040c5088ee3ee6ecc9cf22529990c7c21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:41 +0000

    Add package py-ballisticcalc/1.0.9/py_ballisticcalc-1.0.9-cp311-cp311-win_amd64.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-1.0.9-cp311-cp311-win_amd64.whl/py_ballisticcalc/__init__.py
@@ -0,0 +1,9 @@
+
+__author__ = "o-murphy"
+__copyright__ = ("",)
+
+__credits__ = ["O-Murphy"]
+__version__ = "1.0.9"
+
+__all__ = ['profile', 'atmosphere', 'bmath', 'drag', 'drag_tables', 'projectile', 'shot_parameters',
+           'trajectory_calculator', 'trajectory_data', 'weapon', 'wind', 'multiple_bc']
