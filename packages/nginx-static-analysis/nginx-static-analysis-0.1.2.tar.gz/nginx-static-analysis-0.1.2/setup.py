d6ebd844ade82597e8d80f3cb526535c012c4d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:12 +0000

    Add package nginx-static-analysis/0.1.2/nginx-static-analysis-0.1.2.tar.gz

---
+++ packages/nginx-static-analysis/nginx-static-analysis-0.1.2.tar.gz/nginx-static-analysis-0.1.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def get_requires_based_on_dist():
+    return ["crossplane"]
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="nginx-static-analysis",
+    version="0.1.2",
+    description="Parse Nginx configurations in a clear manner for debugging purposes",
+    url="https://github.com/AlexanderGrooff/nginx-static-analysis",
+    packages=find_packages(include=["nginx_analysis"], exclude=["tests"]),
+    author="Alexander Grooff",
+    author_email="alexandergrooff@gmail.com",
+    install_requires=get_requires_based_on_dist(),
+    entry_points={
+        "console_scripts": [
+            "nginx-static-analysis = nginx_analysis.main:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
