7abfb5a0887609500bb14b7f7a5875daf7f9db3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:59 +0000

    Add package PyFastDub/3.1.1/PyFastDub-3.1.1.tar.gz

---
+++ packages/PyFastDub/PyFastDub-3.1.1.tar.gz/PyFastDub-3.1.1/fastdub/__init__.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import argparse
+
+__all__ = ('PrettyViewPrefix',)
+
+if not hasattr(argparse, 'BooleanOptionalAction'):
+    class BooleanOptionalAction(argparse.Action):
+        def __init__(self, option_strings, dest, default=None, type=None,
+                     choices=None, required=False, help=None, metavar=None):
+            _option_strings = ()
+            for option_string in option_strings:
+                _option_strings += option_string,
+                if option_string.startswith('--'):
+                    option_string = '--no-' + option_string[2:]
+                    _option_strings += option_string,
+            if help is not None and default is not None and default is not argparse.SUPPRESS:
+                help += " (default: %(default)s)"
+            super().__init__(option_strings=_option_strings, dest=dest, nargs=0, default=default, type=type,
+                             choices=choices, required=required, help=help, metavar=metavar)
+
+        def __call__(self, parser, namespace, values, option_string=None):
+            if option_string in self.option_strings:
+                setattr(namespace, self.dest, not option_string.startswith('--no-'))
+
+        def format_usage(self):
+            return ' | '.join(self.option_strings)
+
+
+    argparse.BooleanOptionalAction = BooleanOptionalAction
+
+
+class PrettyViewPrefix:
+    __slots__ = ()
+
+    @staticmethod
+    def pretty_units_of_any(size: float, div: float, iter_prefix, base: str) -> str:
+        for prefix in iter_prefix[:-1]:
+            if size < div:
+                return f'{size:,g}{prefix}{base}'
+            size /= div
+        return f'{size:,g}{iter_prefix[-1]}{base}'
+
+    @classmethod
+    def pretty_units_of_inf(cls, size: float):
+        return cls.pretty_units_of_any(size, 1000., ('', *'kMGTPEZY'), 'B')
+
+    @classmethod
+    def pretty_units_of_time(cls, size: float):
+        return cls.pretty_units_of_any(size, 60., 'smh', '')
