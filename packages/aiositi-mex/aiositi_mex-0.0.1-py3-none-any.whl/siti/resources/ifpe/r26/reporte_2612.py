94364b91af5ce112348c6d74410fe41cd2c4ee68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:04 +0000

    Add package aiositi-mex/0.0.1/aiositi_mex-0.0.1-py3-none-any.whl

---
+++ packages/aiositi-mex/aiositi_mex-0.0.1-py3-none-any.whl/siti/resources/ifpe/r26/reporte_2612.py
@@ -0,0 +1,47 @@
+from pydantic import conlist
+
+from ...base import Resource
+from ..base import ReportIFPE, Resendable, Sendable, Updateable
+
+
+class IdentificadorComisionista(Resource):
+    identificador_comisionista: str
+
+
+class IdentificadorModuloEstablecimiento(Resource):
+    tipo_movimiento: str
+    clave_modulo_establecimiento: str
+    rfc_modulo_establecimiento: str
+    clave_localidad_modulo_establecimiento: str
+    clave_estado_modulo_establecimiento: str
+    clave_municipio_moldulo_establecimiento: str  # la api dice moldulo
+    codigo_postal_modulo_establecimiento: str
+    latitud_ubicacion_modulo_establecimiento: str
+    longitud_ubicacion_modulo_establecimiento: str
+
+
+class BajaModuloEstablecimiento(Resource):
+    causa_baja_modulo_establecimiento: str
+
+
+class InformacionSolicitada(Resource):
+    identificador_comisionista: IdentificadorComisionista
+    identificador_modulo_establecimiento: IdentificadorModuloEstablecimiento
+    baja_modulo_establecimiento: BajaModuloEstablecimiento
+
+
+class Reporte2612(ReportIFPE, Sendable, Updateable, Resendable):
+    """
+    Este reporte recaba información referente a los módulos o
+    establecimientos que los comisionistas bancarios y cambiarios
+    tengan habilitados para representar a las propias Instituciones
+    de Fondos de Pago Electrónico con sus clientes y con el público
+    en general, reportando a la CNBV los movimientos de altas,
+    bajas y/o actualizaciones de dichos módulos o establecimientos.
+    """
+
+    _resource = '/IFPE/R26/2612'
+
+    informacion_solicitada: conlist(  # type: ignore[valid-type]
+        InformacionSolicitada, min_items=1
+    )
