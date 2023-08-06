a53f4455a35fa31389045ba996ce868e42831854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:55 +0000

    Add package preln/0.5.5/preln-0.5.5-py3-none-any.whl

---
+++ packages/preln/preln-0.5.5-py3-none-any.whl/Preln/core/stopwords.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import logging
+from pathlib import Path
+import csv
+
+path = Path(__file__).parent / "../_external/stopwords.csv"
+stopwords = pd.read_csv(path)["stopwords"].tolist()
+
+
+def stopwords_es(text, debug):
+    """
+    Method for eliminate text punctuation.
+
+    :param text: The text to be preprocessed
+    :type: str
+
+    :param debug: Apply debugging
+    :type: bool
+    """
+
+    if debug:
+        logging.basicConfig(
+            level=logging.DEBUG, format="%(asctime)s | %(levelname)8s | %(message)s"
+        )
+
+    text = text.split()
+    text = " ".join(word for word in text if word not in stopwords)
+    logging.debug("-- Stopwords cleanned!")
+
+    return text
+
+
+if __name__ == "__main__":
+    stopwords_es()
