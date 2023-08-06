e65553c7b82dc2627916a8b840b62026f2c17282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:29 +0000

    Add package panel-sharing/0.5.0/panel_sharing-0.5.0-py3-none-any.whl

---
+++ packages/panel-sharing/panel_sharing-0.5.0-py3-none-any.whl/panel_sharing/utils.py
@@ -0,0 +1,34 @@
+"""A module of shared utilities"""
+import logging
+import os
+from contextlib import contextmanager
+from pathlib import Path
+
+import panel as pn
+
+
+@contextmanager
+def set_directory(path: Path):
+    """Sets the cwd within the context
+
+    Args:
+        path (Path): The path to the cwd
+
+    Yields:
+        None
+    """
+
+    origin = Path().absolute()
+    try:
+        path.mkdir(parents=True, exist_ok=True)
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
+
+
+def exception_handler(ex):
+    """A general exception handler for panel apps"""
+    logging.exception("Error", exc_info=ex)
+    if pn.state.notifications and ex:
+        pn.state.notifications.error(f"Error. {ex}")
