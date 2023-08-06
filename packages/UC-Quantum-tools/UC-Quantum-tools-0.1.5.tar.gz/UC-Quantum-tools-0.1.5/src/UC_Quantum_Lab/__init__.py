4c5ce9923f319a359916925d3f3858226a4bce78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:26 +0000

    Add package UC-Quantum-tools/0.1.5/UC-Quantum-tools-0.1.5.tar.gz

---
+++ packages/UC-Quantum-tools/UC-Quantum-tools-0.1.5.tar.gz/UC-Quantum-tools-0.1.5/src/UC_Quantum_Lab/__init__.py
@@ -0,0 +1,20 @@
+import os 
+
+_config_dir = ".UCQ_config"
+_layout_file = os.path.join(_config_dir, "layout.json")
+_trigger_file = ".trigger"
+_master_show = False
+_show_plt = False
+_states = {}
+_circs = []
+_hists = []
+
+
+# cleans up the config directory on init of this python module
+if _config_dir in os.listdir():
+    _master_show = True
+    #print("cleaning up config dir")
+    for item in os.listdir(_config_dir):
+        # deletes png html or the trigger file from the config dir
+        if item.endswith(".png") or item.endswith(".html") or item == _trigger_file:
+            os.remove(os.path.join(_config_dir, item))
