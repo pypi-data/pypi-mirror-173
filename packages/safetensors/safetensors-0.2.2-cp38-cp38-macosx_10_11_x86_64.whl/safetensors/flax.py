a4292495e362e04c871f4bad3e3f1bd067442807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:22 +0000

    Add package safetensors/0.2.2/safetensors-0.2.2-cp38-cp38-macosx_10_11_x86_64.whl

---
+++ packages/safetensors/safetensors-0.2.2-cp38-cp38-macosx_10_11_x86_64.whl/safetensors/flax.py
@@ -0,0 +1,42 @@
+from typing import Dict, Optional
+
+import numpy as np
+
+import jax.numpy as jnp
+from safetensors import numpy
+
+
+def _np2jnp(numpy_dict: Dict[str, np.ndarray]) -> Dict[str, jnp.DeviceArray]:
+    for k, v in numpy_dict.items():
+        numpy_dict[k] = jnp.array(v)
+    return numpy_dict
+
+
+def _jnp2np(jnp_dict: Dict[str, jnp.DeviceArray]) -> Dict[str, np.array]:
+    for k, v in jnp_dict.items():
+        jnp_dict[k] = np.asarray(v)
+    return jnp_dict
+
+
+def save(tensors: Dict[str, jnp.DeviceArray], metadata: Optional[Dict[str, str]] = None) -> bytes:
+    np_tensors = _jnp2np(tensors)
+    return numpy.save(np_tensors, metadata=metadata)
+
+
+def save_file(
+    tensors: Dict[str, jnp.DeviceArray],
+    filename: str,
+    metadata: Optional[Dict[str, str]] = None,
+):
+    np_tensors = _jnp2np(tensors)
+    return numpy.save_file(np_tensors, filename, metadata=metadata)
+
+
+def load(buffer: bytes) -> Dict[str, jnp.DeviceArray]:
+    flat = numpy.load(buffer)
+    return _np2jnp(flat)
+
+
+def load_file(filename: str) -> Dict[str, jnp.DeviceArray]:
+    flat = numpy.load_file(filename)
+    return _np2jnp(flat)
