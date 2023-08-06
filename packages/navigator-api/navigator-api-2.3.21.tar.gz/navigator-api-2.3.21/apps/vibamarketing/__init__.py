39b571195ee55d2f5061f19844e87c3a14d3a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:10 +0000

    Add package navigator-api/2.3.21/navigator-api-2.3.21.tar.gz

---
+++ packages/navigator-api/navigator-api-2.3.21.tar.gz/navigator-api-2.3.21/apps/vibamarketing/__init__.py
@@ -0,0 +1,12 @@
+"""
+Program Init.
+
+vibamarketing Program.
+"""
+from navigator.handlers.types import AppConfig
+
+class vibamarketing(AppConfig):
+    __version__ = '0.0.1'
+    app_description = """
+    API for vibamarketing
+    """
