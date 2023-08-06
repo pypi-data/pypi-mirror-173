9049c3087df351d5abaeea430f91bb7993e80eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:40 +0000

    Add package estruttura/0.2.0/estruttura-0.2.0-py2.py3-none-any.whl

---
+++ packages/estruttura/estruttura-0.2.0-py2.py3-none-any.whl/estruttura/__init__.py
@@ -0,0 +1,57 @@
+from ._attribute import MutableStructureAttribute, StructureAttribute
+from ._bases import (
+    CollectionStructure,
+    ImmutableCollectionStructure,
+    ImmutableStructure,
+    ImmutableStructureMeta,
+    MutableCollectionStructure,
+    MutableStructure,
+    MutableStructureMeta,
+    Structure,
+    StructureMeta,
+)
+from ._class import (
+    AttributeMap,
+    ClassStructure,
+    ClassStructureMeta,
+    ImmutableClassStructure,
+    ImmutableClassStructureMeta,
+    MutableClassStructure,
+    MutableClassStructureMeta,
+)
+from ._dict import DictStructure, ImmutableDictStructure, MutableDictStructure
+from ._list import ImmutableListStructure, ListStructure, MutableListStructure
+from ._relationship import Relationship, RelationshipTypesInfo
+from ._set import ImmutableSetStructure, MutableSetStructure, SetStructure
+
+__all__ = [
+    "StructureAttribute",
+    "MutableStructureAttribute",
+    "StructureMeta",
+    "Structure",
+    "ImmutableStructureMeta",
+    "ImmutableStructure",
+    "MutableStructureMeta",
+    "MutableStructure",
+    "CollectionStructure",
+    "ImmutableCollectionStructure",
+    "MutableCollectionStructure",
+    "AttributeMap",
+    "ClassStructureMeta",
+    "ClassStructure",
+    "ImmutableClassStructureMeta",
+    "ImmutableClassStructure",
+    "MutableClassStructureMeta",
+    "MutableClassStructure",
+    "DictStructure",
+    "ImmutableDictStructure",
+    "MutableDictStructure",
+    "ListStructure",
+    "ImmutableListStructure",
+    "MutableListStructure",
+    "Relationship",
+    "RelationshipTypesInfo",
+    "SetStructure",
+    "ImmutableSetStructure",
+    "MutableSetStructure",
+]
