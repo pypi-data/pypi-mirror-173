aabff88ed00f616732415b4046971c38a77f1013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:15 +0000

    Add package Alpha-Rabbit/1.0.0/Alpha_Rabbit-1.0.0.tar.gz

---
+++ packages/Alpha-Rabbit/Alpha_Rabbit-1.0.0.tar.gz/Alpha_Rabbit-1.0.0/setup.py
@@ -0,0 +1,50 @@
+# -*-coding:utf-8 -*-
+
+"""
+1. python3 setup.py sdist bdist_wheel
+2. twine upload dist/*
+"""
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Alpha_Rabbit",
+    version="1.0.0",
+    author="lijiongting",
+    author_email="448986334@qq.com",
+    description="Alpha_Rabbit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'redis',
+        'pymongo',
+        'boto3',
+        'python-dateutil',
+        'psycopg2-binary~=2.8.6',
+        'pyyaml',
+        'requests'
+    ],
+    zip_safe=True,
+)
