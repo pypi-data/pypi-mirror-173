6eee4c045e36bd546a7b0763c037dab399a97b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:22 +0000

    Add package drfcommon/0.1.8.5/drfcommon-0.1.8.5.tar.gz

---
+++ packages/drfcommon/drfcommon-0.1.8.5.tar.gz/drfcommon-0.1.8.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['drfcommon']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=3.2,<4.0', 'djangorestframework>=3.12.2,<4.0.0']
+
+setup_kwargs = {
+    'name': 'drfcommon',
+    'version': '0.1.8.5',
+    'description': 'a common tools for drf',
+    'long_description': 'None',
+    'author': 'tplinux',
+    'author_email': 'tplinux@163.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/pydtools/drfcommon/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
