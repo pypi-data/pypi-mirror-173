4fba985bd9acbe62fb8caf7380748cfe6f89dc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:09 +0000

    Add package Dre-binomial-distribution/0.1/Dre_binomial_distribution-0.1.tar.gz

---
+++ packages/Dre-binomial-distribution/Dre_binomial_distribution-0.1.tar.gz/Dre_binomial_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Dre_binomial_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Dre_binomial_distributions'],
+      author= 'Sobe-Olisa Andrew',
+      author_email = 'nwangenesobeolisa@gmail.com',
+      zip_safe=False)
