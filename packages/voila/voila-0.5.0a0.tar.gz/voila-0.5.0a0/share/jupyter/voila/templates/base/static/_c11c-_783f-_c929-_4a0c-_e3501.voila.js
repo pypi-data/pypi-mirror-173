2de14fdb950c38f2af1cda406047280cc327a0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:28 +0000

    Add package voila/0.5.0a0/voila-0.5.0a0.tar.gz

---
+++ packages/voila/voila-0.5.0a0.tar.gz/voila-0.5.0a0/share/jupyter/voila/templates/base/static/_c11c-_783f-_c929-_4a0c-_e3501.voila.js
@@ -0,0 +1,61 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(globalThis["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] = globalThis["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] || []).push([["_c11c-_783f-_c929-_4a0c-_e3501"],{
+
+/***/ "?c11c":
+/*!**************************************!*\
+  !*** ./terminal-highlight (ignored) ***!
+  \**************************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/./terminal-highlight_(ignored)?");
+
+/***/ }),
+
+/***/ "?783f":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/fs_(ignored)?");
+
+/***/ }),
+
+/***/ "?c929":
+/*!**********************!*\
+  !*** path (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/path_(ignored)?");
+
+/***/ }),
+
+/***/ "?4a0c":
+/*!*******************************!*\
+  !*** source-map-js (ignored) ***!
+  \*******************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/source-map-js_(ignored)?");
+
+/***/ }),
+
+/***/ "?e350":
+/*!*********************!*\
+  !*** url (ignored) ***!
+  \*********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/url_(ignored)?");
+
+/***/ })
+
+}]);
---
+++ packages/voila/voila-0.5.0a0.tar.gz/voila-0.5.0a0/share/jupyter/voila/templates/base/static/_dd8e0.voila.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(globalThis["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] = globalThis["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] || []).push([["_dd8e0"],{
+
+/***/ "?dd8e":
+/*!****************************!*\
+  !*** node-fetch (ignored) ***!
+  \****************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/node-fetch_(ignored)?");
+
+/***/ })
+
+}]);
