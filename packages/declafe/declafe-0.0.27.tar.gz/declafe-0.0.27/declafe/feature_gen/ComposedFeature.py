dc741b9a9e1560b91a2e8ab6afbc0fdcef5997b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:47 +0000

    Add package declafe/0.0.27/declafe-0.0.27.tar.gz

---
+++ packages/declafe/declafe-0.0.27.tar.gz/declafe-0.0.27/declafe/feature_gen/ComposedFeature.py
@@ -0,0 +1,34 @@
+from typing import List, TYPE_CHECKING
+
+import pandas as pd
+
+from declafe.feature_gen import FeatureGen
+
+__all__ = ["ComposedFeature"]
+
+if TYPE_CHECKING:
+  from declafe.feature_gen.unary.UnaryColumnFeature import UnaryFeature
+
+
+class ComposedFeature(FeatureGen):
+
+  def __init__(self, head: FeatureGen, nexts: List["UnaryFeature"]):
+    self.head = head
+    self.nexts = nexts
+    super().__init__()
+
+  def __post_init__(self):
+    if len(self.nexts) == 0:
+      raise ValueError("nextsが空です")
+
+  def gen(self, df: pd.DataFrame) -> pd.Series:
+    result = self.head.generate(df)
+
+    for f in self.nexts:
+      df[f.feature_name] = result
+      result = f.gen_unary(result)
+
+    return result
+
+  def _feature_name(self) -> str:
+    return self.nexts[-1].feature_name
