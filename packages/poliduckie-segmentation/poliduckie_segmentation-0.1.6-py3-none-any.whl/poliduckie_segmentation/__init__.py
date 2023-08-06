d75200766a7fc1ba89232347b1f8c59f199ee795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:34 +0000

    Add package poliduckie-segmentation/0.1.6/poliduckie_segmentation-0.1.6-py3-none-any.whl

---
+++ packages/poliduckie-segmentation/poliduckie_segmentation-0.1.6-py3-none-any.whl/poliduckie_segmentation/__init__.py
@@ -0,0 +1,8 @@
+from poliduckie_segmentation.segmentation import Segmentation
+from poliduckie_segmentation.control import MPC
+
+__version__ = '0.1.6'
+__author__ = 'PoliDuckies'
+__email__ = 'duckietown@aeapolimi.it'
+__license__ = 'MIT'
+__url__ = 'github.com/poliduckie/poliduckie_segmentation'
