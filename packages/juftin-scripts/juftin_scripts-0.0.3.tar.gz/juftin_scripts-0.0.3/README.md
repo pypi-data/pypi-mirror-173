5414d54ac803332778a83443a3bea0fbf9e15442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:17 +0000

    Add package juftin-scripts/0.0.3/juftin_scripts-0.0.3.tar.gz

---
+++ packages/juftin-scripts/juftin_scripts-0.0.3.tar.gz/juftin_scripts-0.0.3/README.md
@@ -0,0 +1,14 @@
+# juftin-scripts
+
+Helpful Python scripts by @juftin
+
+#### Check Out the [Docs](https://juftin.github.io/juftin-scripts/)
+#### Looking to contribute? See the [Contributing Guide](docs/source/contributing.md)
+#### See the [Changelog](https://github.com/juftin/juftin-scripts/releases)
+
+___________
+___________
+
+<br/>
+
+<p align="center"><a href="https://github.com/juftin"><img src="https://raw.githubusercontent.com/juftin/juftin/main/static/juftin.png" width="120" height="120" alt="logo"></p>
---
+++ packages/juftin-scripts/juftin_scripts-0.0.3.tar.gz/juftin_scripts-0.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['juftin_scripts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.2,<9.0.0',
+ 'rich-click>=1.5.2,<2.0.0',
+ 'rich>=12.6.0,<13.0.0',
+ 'textual>=0.2.1,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['browse = juftin_scripts.code_browser:main',
+                     'rotate-aws-profile = juftin_scripts.rotation:rotate']}
+
+setup_kwargs = {
+    'name': 'juftin-scripts',
+    'version': '0.0.3',
+    'description': 'Helpful Python scripts by @juftin',
+    'long_description': '# juftin-scripts\n\nHelpful Python scripts by @juftin\n\n#### Check Out the [Docs](https://juftin.github.io/juftin-scripts/)\n#### Looking to contribute? See the [Contributing Guide](docs/source/contributing.md)\n#### See the [Changelog](https://github.com/juftin/juftin-scripts/releases)\n\n___________\n___________\n\n<br/>\n\n<p align="center"><a href="https://github.com/juftin"><img src="https://raw.githubusercontent.com/juftin/juftin/main/static/juftin.png" width="120" height="120" alt="logo"></p>\n',
+    'author': 'Justin Flannery',
+    'author_email': 'juftin@juftin.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/juftin/juftin-scripts',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
