901753995a59c649ba4a0b326869bdb32717279b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:21 +0000

    Add package imgui-bundle/0.6.3/imgui_bundle-0.6.3-cp38-cp38-win_amd64.whl

---
+++ packages/imgui-bundle/imgui_bundle-0.6.3-cp38-cp38-win_amd64.whl/imgui_bundle/__init__.pyi
@@ -0,0 +1,16 @@
+from . import imgui as imgui
+from . import imgui_internal as imgui_internal
+from . import hello_imgui as hello_imgui
+from . import implot as implot
+from . import imgui_color_text_edit as imgui_color_text_edit
+from . import imgui_node_editor as imgui_node_editor
+from . import imgui_knobs as imgui_knobs
+from . import im_file_dialog as im_file_dialog
+from . import  imspinner as imspinner
+from . import  imgui_md as imgui_md
+from . import run as run, current_node_editor_context as current_node_editor_context
+from . utilities import (
+    static as static,
+)
+from . imgui import (ImVec2 as ImVec2, ImVec4 as ImVec4, ImColor as ImColor)
+from . im_col32 import IM_COL32 as IM_COL32
