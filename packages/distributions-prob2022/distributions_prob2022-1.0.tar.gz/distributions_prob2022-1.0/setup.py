bf0149df77ff1312f5ce515c72654d92484695ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:19 +0000

    Add package distributions-prob2022/1.0/distributions_prob2022-1.0.tar.gz

---
+++ packages/distributions-prob2022/distributions_prob2022-1.0.tar.gz/distributions_prob2022-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_prob2022',
+      version='1.0',
+      description='Gaussian distributions and Binomial disributions',
+      packages=['distributions_prob2022'],
+      author = 'James Mugwanya',
+      author_email = 'jamesmugwanya90@gmail.com',
+      zip_safe=False)
