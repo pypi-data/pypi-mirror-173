89ce2112be403dd543e54251ea04bbe32a9e4a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:14 +0000

    Add package animationer/1.0.2/animationer-1.0.2.tar.gz

---
+++ packages/animationer/animationer-1.0.2.tar.gz/animationer-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="animationer",
+    version="1.0.2",
+    packages=["animationer"],
+    requires=["pal"],
+    author='LeWolfYT',
+    author_email='ciblox3@gmail.com',
+    description="A lightweight animation library for console applications. Made entirely in my free time (OUTDATED! GET PAL INSTEAD!)",
+    license="MIT"
+)
