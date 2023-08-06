76c9dafd91328b797411c1200b040d8cda24d4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:55 +0000

    Add package ShellcodeFormatter/1.0.0/ShellcodeFormatter-1.0.0.tar.gz

---
+++ packages/ShellcodeFormatter/ShellcodeFormatter-1.0.0.tar.gz/ShellcodeFormatter-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='ShellcodeFormatter',
+     version='1.0.0',
+     scripts=['ShellcodeFormatter'] ,
+     author="Eth0",
+     author_email="e@eth0.re",
+     description="Transform binary shellcode into various formats",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/eth0-re/ShellcodeFormatter",
+     download_url="https://github.com/eth0-re/ShellcodeFormatter/archive/refs/tags/v1.0.0.tar.gz",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
