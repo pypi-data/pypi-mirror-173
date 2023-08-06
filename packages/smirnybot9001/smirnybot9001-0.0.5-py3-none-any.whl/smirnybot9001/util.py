079689a19471bb0754cfbf670cf78647ab4eb0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:20 +0000

    Add package smirnybot9001/0.0.5/smirnybot9001-0.0.5-py3-none-any.whl

---
+++ packages/smirnybot9001/smirnybot9001-0.0.5-py3-none-any.whl/smirnybot9001/util.py
@@ -0,0 +1,24 @@
+import re
+import requests
+from fake_user_agent import user_agent
+
+MAX_SET_NR_LENGTH = 128
+VALID_SET_CHARS = r'[\w\d\-]'
+
+
+def is_identifier(candidate):
+    try:
+        candidate = str(candidate)
+    except ValueError:
+        return False
+    if len(candidate) > MAX_SET_NR_LENGTH:
+        return False
+    pattern = f"^{VALID_SET_CHARS}+$"
+    m = re.search(pattern, candidate, re.ASCII)
+    if m is None:
+        return False
+    return True
+
+
+def get_with_user_agent(url):
+    return requests.get(url, headers={'User-Agent': user_agent()})
