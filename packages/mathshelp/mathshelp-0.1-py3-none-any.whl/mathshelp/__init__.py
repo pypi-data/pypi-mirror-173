4645e744059c419396c25b2be6dcb2fca43f792d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:25 +0000

    Add package mathshelp/0.1/mathshelp-0.1-py3-none-any.whl

---
+++ packages/mathshelp/mathshelp-0.1-py3-none-any.whl/mathshelp/__init__.py
@@ -0,0 +1,12 @@
+import time
+pi = 3.1415926536
+e = 2.7182818284
+def areaofcircle_d(d):
+    print(d*3.1415926536)
+def areaofcircle_r(r):
+    print(2*r*3.1415926536)
+version='0.0.1'
+def delay(t):
+    time.sleep(t)
+def makeadatalog(name):
+    name=open(name,"w+")
