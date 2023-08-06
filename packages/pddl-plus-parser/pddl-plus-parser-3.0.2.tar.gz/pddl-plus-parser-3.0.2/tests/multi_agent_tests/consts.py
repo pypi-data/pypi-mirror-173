0f7de3d364f09ba5ea7cec15db0014d2229d522d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:42 +0000

    Add package pddl-plus-parser/3.0.2/pddl-plus-parser-3.0.2.tar.gz

---
+++ packages/pddl-plus-parser/pddl-plus-parser-3.0.2.tar.gz/pddl-plus-parser-3.0.2/tests/multi_agent_tests/consts.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+CWD = os.getcwd()
+SOKOBAN_DOMAIN_FILE_PATH = Path(CWD, "sokoban_domain.pddl")
+SOKOBAN_UNPARSED_PLAN_PATH = Path(CWD, "sokoban_plan.txt")
+SOKOBAN_PROBLEM_PATH = Path(CWD, "sokoban_problem.pddl")
+
+WOODWORKING_DOMAIN_FILE_PATH = Path(CWD, "woodworking_domain.pddl")
+WOODWORKING_UNPARSED_PLAN_PATH = Path(CWD, "woodworking_plan.txt")
+WOODWORKING_PARSED_PLAN_PATH = Path(CWD, "woodworking_plan.solution")
+WOODWORKING_SHORT_PARSED_PLAN_PATH = Path(CWD, "woodworking_short_plan.solution")
+
+COMBINED_PROBLEM_PATH = Path(CWD, "combined_problem.pddl")
+COMBINED_DOMAIN_PATH = Path(CWD, "combined_domain.pddl")
+
+MULTI_AGENT_DATA_DIRECTORY = Path(CWD, "multi_agent_problem")
+
+WOODWORKING_AGENT_NAMES = ["glazer0", "grinder0", "highspeed-saw0", "immersion-varnisher0", "planer0", "saw0",
+                           "spray-varnisher0"]
+
+
+DEPOT_MA_DOMAIN_PATH = Path(CWD, "Depots.pddl")
+DEPOT_MA_PROBLEM_PATH = Path(CWD, "pfile1_depot.pddl")
+DEPOT_MA_SOLUTION_PATH = Path(CWD, "pfile1_depots.solution")
