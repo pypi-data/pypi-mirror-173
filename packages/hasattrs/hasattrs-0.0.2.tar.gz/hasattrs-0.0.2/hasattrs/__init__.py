f7f66ed8656b943a87ce2541255feed07d28d1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:24 +0000

    Add package hasattrs/0.0.2/hasattrs-0.0.2.tar.gz

---
+++ packages/hasattrs/hasattrs-0.0.2.tar.gz/hasattrs-0.0.2/hasattrs/__init__.py
@@ -0,0 +1,13 @@
+from hasattrs.checks import has_attrs, has_container_attrs, has_hashable_attrs, has_iterable_attrs
+from hasattrs.checks import has_iterator_attrs, has_reversible_attrs, has_generator_attrs
+from hasattrs.checks import has_sized_attrs, has_callable_attrs, has_collection_attrs
+from hasattrs.checks import has_sequence_attrs, has_mutable_sequence_attrs
+from hasattrs.checks import has_byte_string_attrs, has_set_attrs, has_mutable_set_attrs
+from hasattrs.checks import has_mapping_attrs, has_mutable_mapping_attrs
+from hasattrs.checks import has_mapping_view_attrs, has_item_view_attrs, has_keys_view_attrs
+from hasattrs.checks import has_values_view_attrs, has_awaitable_attrs, has_coroutine_attrs
+from hasattrs.checks import has_async_iterable_attrs, has_async_iterator_attrs
+from hasattrs.checks import has_async_generator_attrs, has_abc_attrs
+
+
+__version__ = '0.0.2'
---
+++ packages/hasattrs/hasattrs-0.0.2.tar.gz/hasattrs-0.0.2/hasattrs/checks.py
@@ -0,0 +1,117 @@
+from typing import Any, Iterable, Type, Union
+
+from hasattrs.attributes import CONTAINER, HASHABLE, ITERABLE, ITERATOR, REVERSABLE
+from hasattrs.attributes import GENERATOR, SIZED, CALLABLE, COLLECTION, SEQUENCE
+from hasattrs.attributes import MUTABLE_SEQUENCE, BYTE_STRING, SET, MUTABLE_SET
+from hasattrs.attributes import MAPPING, MUTABLE_MAPPING, MAPPING_VIEW
+from hasattrs.attributes import ITEMS_VIEW, KEYS_VIEW, VALUES_VIEW, AWAITABLE, COROUTINE
+from hasattrs.attributes import ASYNC_ITERABLE, ASYNC_ITERATOR, ASYNC_GENERATOR
+from hasattrs.types import ABC, Typing, abc_attrs
+
+
+def has_attrs(obj: Any, attrs: Iterable[str]) -> bool:
+    return all(hasattr(obj, a) for a in attrs)
+
+
+def has_abc_attrs(obj: Any, abc: Union[Type[ABC], Type[Typing], str]) -> bool:
+    return has_attrs(obj, abc_attrs[abc])
+
+
+def has_container_attrs(obj: Any) -> bool:
+    return has_attrs(obj, CONTAINER)
+
+
+def has_hashable_attrs(obj: Any) -> bool:
+    return has_attrs(obj, HASHABLE)
+
+
+def has_iterable_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ITERABLE)
+
+
+def has_iterator_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ITERATOR)
+
+
+def has_reversible_attrs(obj: Any) -> bool:
+    return has_attrs(obj, REVERSABLE)
+
+
+def has_generator_attrs(obj: Any) -> bool:
+    return has_attrs(obj, GENERATOR)
+
+
+def has_sized_attrs(obj: Any) -> bool:
+    return has_attrs(obj, SIZED)
+
+
+def has_callable_attrs(obj: Any) -> bool:
+    return has_attrs(obj, CALLABLE)
+
+
+def has_collection_attrs(obj: Any) -> bool:
+    return has_attrs(obj, COLLECTION)
+
+
+def has_sequence_attrs(obj: Any) -> bool:
+    return has_attrs(obj, SEQUENCE)
+
+
+def has_mutable_sequence_attrs(obj: Any) -> bool:
+    return has_attrs(obj, MUTABLE_SEQUENCE)
+
+
+def has_byte_string_attrs(obj: Any) -> bool:
+    return has_attrs(obj, BYTE_STRING)
+
+
+def has_set_attrs(obj: Any) -> bool:
+    return has_attrs(obj, SET)
+
+
+def has_mutable_set_attrs(obj: Any) -> bool:
+    return has_attrs(obj, MUTABLE_SET)
+
+
+def has_mapping_attrs(obj: Any) -> bool:
+    return has_attrs(obj, MAPPING)
+
+
+def has_mutable_mapping_attrs(obj: Any) -> bool:
+    return has_attrs(obj, MUTABLE_MAPPING)
+
+
+def has_mapping_view_attrs(obj: Any) -> bool:
+    return has_attrs(obj, MAPPING_VIEW)
+
+
+def has_item_view_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ITEMS_VIEW)
+
+
+def has_keys_view_attrs(obj: Any) -> bool:
+    return has_attrs(obj, KEYS_VIEW)
+
+
+def has_values_view_attrs(obj: Any) -> bool:
+    return has_attrs(obj, VALUES_VIEW)
+
+
+def has_awaitable_attrs(obj: Any) -> bool:
+    return has_attrs(obj, AWAITABLE)
+
+
+def has_coroutine_attrs(obj: Any) -> bool:
+    return has_attrs(obj, COROUTINE)
+
+
+def has_async_iterable_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ASYNC_ITERABLE)
+
+
+def has_async_iterator_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ASYNC_ITERATOR)
+
+
+def has_async_generator_attrs(obj: Any) -> bool:
+    return has_attrs(obj, ASYNC_GENERATOR)
