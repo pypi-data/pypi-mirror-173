037c3f7129d501a97bbd310e5c0cde58db36325f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:54 +0000

    Add package uchejudennodim/0.1/uchejudennodim-0.1.tar.gz

---
+++ packages/uchejudennodim/uchejudennodim-0.1.tar.gz/uchejudennodim-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='uchejudennodim',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['uchejudennodim'],
+      zip_safe=False)
