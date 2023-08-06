3927bb1d9131a673405669863ff564455500bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:07 +0000

    Add package navigator-api/2.3.20/navigator-api-2.3.20.tar.gz

---
+++ packages/navigator-api/navigator-api-2.3.20.tar.gz/navigator-api-2.3.20/apps/retail/__init__.py
@@ -0,0 +1,12 @@
+"""
+Program Init.
+
+retail Program.
+"""
+from navigator.handlers.types import AppConfig
+
+class retail(AppConfig):
+    __version__ = '0.0.1'
+    app_description = """
+    API for retail
+    """
