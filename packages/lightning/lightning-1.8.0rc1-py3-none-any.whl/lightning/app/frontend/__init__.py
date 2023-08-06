52675fe8ea1da5f197634586330ab24340018736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:28 +0000

    Add package lightning/1.8.0rc1/lightning-1.8.0rc1-py3-none-any.whl

---
+++ packages/lightning/lightning-1.8.0rc1-py3-none-any.whl/lightning/app/frontend/__init__.py
@@ -0,0 +1,6 @@
+from lightning.app.frontend.frontend import Frontend
+from lightning.app.frontend.panel import PanelFrontend
+from lightning.app.frontend.stream_lit import StreamlitFrontend
+from lightning.app.frontend.web import StaticWebFrontend
+
+__all__ = ["Frontend", "PanelFrontend", "StaticWebFrontend", "StreamlitFrontend"]
